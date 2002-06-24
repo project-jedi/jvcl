7b112f3680f9177c9685a6bbcfdb8cce1b818b15
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2002-06-24 10:34:21 +0000

    *** empty log message ***

---
+++ tests/restructured/examples/JvDialogs/JvDialogs.dpr
@@ -0,0 +1,13 @@
+program JvDialogs;
+
+uses
+  Forms,
+  fDialogs in 'fDialogs.pas' {Form1};
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
