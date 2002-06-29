9cc6a4e61a76f8a5ca4e78c29d5bbbaeb49bcf8b
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2002-06-29 16:23:44 +0000

    Demo that demonstrates the new capabilities of the JvSystemPopUp

---
+++ tests/restructured/examples/JvSystemPopUp2/SystemPopupTest.dpr
@@ -0,0 +1,13 @@
+program SystemPopupTest;
+
+uses
+  Forms,
+  SystemPopupTestDlg in 'SystemPopupTestDlg.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
