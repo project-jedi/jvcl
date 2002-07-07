b4953625007539f2f14be9eb8f727577237eb436
beckmi <beckmi@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2002-07-07 19:55:11 +0000

    *** empty log message ***

---
+++ tests/RALib/interpreter/Languages/_new/res.bat
@@ -0,0 +1,7 @@
+brcc32.exe rafd.rc
+copy RACtlConst.pas ..\..\Lib\RACtlConst.pas
+copy RADBConst.pas ..\..\Lib\RADBConst.pas
+copy JvInterpreterConst.pas ..\..\Lib\JvInterpreterConst.pas
+copy rafd.res ..\..\Lib\rafd.res
+copy packconst.pas ..\..\DPacks\packconst.pas
+copy ..\all\rafd2.res ..\..\Lib\rafd2.res
