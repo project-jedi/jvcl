a35cf3e566e0e345cb35d1a47f93567eac7b0c0b
sbuysse <sbuysse@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2002-08-07 21:13:10 +0000

    Added Automatic popupmenu Added Save/Load Positions

--- tests/archive/jvcl/source/JvControlBar.pas
+++ tests/archive/jvcl/source/JvControlBar.pas
@@ -33,9 +33,11 @@ interface
 
 
 uses
-  Windows, Messages, SysUtils, Classes, Graphics, Extctrls, Controls, Forms, JVCLVer;
+  Windows, Messages, SysUtils, Classes, Graphics, Extctrls, Controls, Forms,
+  Menus, JVCLVer;
 
 type
+  TPopupNames = (pnHint, pnName);
   TJvControlBar = class(TControlBar)
   private
     FColor: TColor;
@@ -45,14 +47,27 @@ type
     FOnParentColorChanged: TNotifyEvent;
     Fover: Boolean;
     FAboutJVCL: TJVCLAboutInfo;
+    FPopupControl: Boolean;
+    FPopup: TPopupMenu;
+    FPopupNames: TPopupNames;
   protected
     procedure CMMouseEnter(var Msg: TMessage); message CM_MOUSEENTER;
     procedure CMMouseLeave(var Msg: TMessage); message CM_MOUSELEAVE;
     procedure CMParentColorChanged(var Msg: TMessage); message CM_PARENTCOLORCHANGED;
+    procedure MouseUp(Button: TMouseButton; Shift: TShiftState;
+      X, Y: Integer); override;
+    procedure PopupMenuClick(Sender: TObject);
   public
     constructor Create(AOwner: TComponent); override;
+    destructor Destroy;override;
+    function SavePositions: string;
+    procedure LoadPositions(const Value: string);
   published
     property AboutJVCL: TJVCLAboutInfo read FAboutJVCL write FAboutJVCL stored False;
+
+    property PopupControl:Boolean read FPopupControl write FPopupControl default true;
+    property PopupNames:TPopupNames read FPopupNames write FPopupNames default pnHint;
+
     property HintColor: TColor read FColor write FColor default clInfoBk;
     property OnMouseEnter: TNotifyEvent read FOnMouseEnter write FOnMouseEnter;
     property OnMouseLeave: TNotifyEvent read FOnMouseLeave write FOnMouseLeave;
@@ -77,6 +92,8 @@ begin
   inherited Create(AOwner);
   FColor := clInfoBk;
   FOver := False;
+  FPopupControl := true;
+  FPopupNames := pnHint;
   ControlStyle := ControlStyle + [csAcceptsControls];
 end;
 
@@ -105,6 +122,115 @@ begin
   end;
   if Assigned(FOnMouseLeave) then
     FOnMouseLeave(Self);
+end;
+
+{**************************************************}
+
+procedure TJvControlBar.MouseUp(Button: TMouseButton; Shift: TShiftState;
+  X, Y: Integer);
+var
+ i: Integer;
+ it: TMenuItem;
+begin
+  inherited;
+  if (PopupControl) and (Button = mbRight) then
+  begin
+    if FPopup<>nil then
+      FPopup.Items.Clear
+    else
+      FPopup := TPopupMenu.Create(self);
+    for i:=0 to ControlCount-1 do
+    begin
+      it := TMenuItem.Create(FPopup);
+      if PopupNames=pnHint then
+        it.Caption := Controls[i].Hint
+      else
+        it.Caption := Controls[i].Name;
+      it.AutoCheck := true;
+      it.Tag := i;
+      it.OnClick := PopupMenuClick;
+      it.Checked := Controls[i].Visible;
+      FPopup.Items.Add(it);
+    end;
+    with ClientToScreen(Point(X,Y)) do
+      FPopup.Popup(X,Y);
+  end;
+end;
+
+{**************************************************}
+
+destructor TJvControlBar.Destroy;
+begin
+  if FPopup<>nil then
+    FPopup.Free;
+  inherited;
+end;
+
+{**************************************************}
+
+procedure TJvControlBar.PopupMenuClick(Sender: TObject);
+begin
+  with Sender as TMenuItem do
+    if (Tag>=0) and (Tag<ControlCount) then
+      Controls[Tag].Visible := Checked;
+end;
+
+{**************************************************}
+
+procedure TJvControlBar.LoadPositions(const Value: string);
+var
+  st,st2: string;
+  i,j: Integer;
+begin
+  for i:=0 to ControlCount-1 do
+    Controls[i].Visible := false;
+  st := Value;
+  j := 0;
+  while (Length(st)>1) and (j<ControlCount) do
+  begin
+    i := Pos(';',st);
+    if i=0 then
+    begin
+      st2 := st;
+      st := '';
+    end
+    else
+    begin
+      st2 := Copy(st,1,i-1);
+      st := Copy(st,i+1,Length(st));
+    end;
+
+    i := pos(',',st2);
+    if i<>0 then
+    begin
+      Controls[j].Visible := pos('true',st2)=1;
+      st2 := Copy(st2,i+1,Length(st2));
+      i := pos(',',st2);
+      if i<>0 then
+      begin
+        Controls[j].Top := StrToIntDef( Copy(st2,i+1,Length(st2)), Controls[j].Top);
+        Controls[j].Left := StrToIntDef( Copy(st2,1,i-1), Controls[j].Left);
+      end;
+    end;
+    inc(j);
+  end;
+end;
+
+{**************************************************}
+
+function TJvControlBar.SavePositions: string;
+var
+ i: Integer;
+begin
+  result := '';
+  for i:=0 to ControlCount-1 do
+  begin
+    if Controls[i].Visible then
+      result := result+'true,'
+    else
+      result := result+'false,';
+    result := result + IntToStr(Controls[i].Left) + ',' + IntToStr(Controls[i].Top)+';';
+  end;
 end;
 
 end.
