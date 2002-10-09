ccb090d916a952ba26a28cee4739413ae0e71de2
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2002-10-09 23:19:15 +0000

    Actions for many of the Jv Dialogs (more to come)

---
+++ tests/archive/jvcl/source/JvDialogActns.pas
@@ -0,0 +1,425 @@
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/MPL-1.1.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: JvStdActions.PAS, released on 2002-10-06.
+
+The Initial Developer of the Original Code is Peter Thörnqvist.
+All Rights Reserved.
+
+Contributor(s):
+
+Last Modified: 2002-10-06
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
+located at http://jvcl.sourceforge.net
+
+Known Issues:
+-----------------------------------------------------------------------------}
+{$I JVCL.INC}
+
+unit JvDialogActns;
+
+{$IFDEF COMPILER6}
+{$WARN SYMBOL_PLATFORM OFF}
+{$WARN UNIT_PLATFORM OFF}
+{$ENDIF}
+
+interface
+uses
+  Classes, SysUtils, ActnList, StdActns, JvBaseDlg, JvBrowseFolder, JvSelectDirectory,
+  JvConnectNetwork, JvWinDialogs, JvDialogs, JvPageSetupTitled, JvPageSetup;
+
+type
+  TJvCommonDialogClass = class of TJvCommonDialog;
+  TJvCommonDialogPClass = class of TJvCommonDialogP;
+
+  TJvCommonDialogAction = class(TCustomAction)
+  private
+    FExecuteResult: Boolean;
+    FOnAccept: TNotifyEvent;
+    FOnCancel: TNotifyEvent;
+    FBeforeExecute: TNotifyEvent;
+  protected
+    FDialog: TJvCommonDialog;
+    function GetDialogClass: TJvCommonDialogClass; virtual;
+  public
+    constructor Create(AOwner: TComponent); override;
+    function HandlesTarget(Target: TObject): Boolean; override;
+    procedure ExecuteTarget(Target: TObject); override;
+    property ExecuteResult: Boolean read FExecuteResult;
+    property BeforeExecute: TNotifyEvent read FBeforeExecute write FBeforeExecute;
+  published
+    property Caption;
+    property Enabled;
+    property HelpContext;
+    property Hint;
+    property ImageIndex;
+    property ShortCut;
+    property SecondaryShortCuts;
+    property Visible;
+    property OnAccept: TNotifyEvent read FOnAccept write FOnAccept;
+    property OnCancel: TNotifyEvent read FOnCancel write FOnCancel;
+  end;
+
+  TJvCommonDialogPAction = class(TCustomAction)
+  private
+    FBeforeExecute: TNotifyEvent;
+    FAfterExecute: TNotifyEvent;
+  protected
+    FDialog: TJvCommonDialogP;
+    function GetDialogClass: TJvCommonDialogPClass; virtual;
+  public
+    constructor Create(AOwner: TComponent); override;
+    function HandlesTarget(Target: TObject): Boolean; override;
+    procedure ExecuteTarget(Target: TObject); override;
+  published
+    property Caption;
+    property Enabled;
+    property HelpContext;
+    property Hint;
+    property ImageIndex;
+    property ShortCut;
+    property SecondaryShortCuts;
+    property Visible;
+    property BeforeExecute: TNotifyEvent read FBeforeExecute write FBeforeExecute;
+    property AfterExecute: TNotifyEvent read FAfterExecute write FAfterExecute;
+  end;
+
+  TJvBrowseFolderAction = class(TJvCommonDialogAction)
+  private
+    function GetDialog: TJvBrowseFolder;
+  protected
+    function GetDialogClass: TJvCommonDialogClass; override;
+  published
+    property Dialog: TJvBrowseFolder read GetDialog;
+  end;
+
+  TJvSelectDirectoryAction = class(TJvCommonDialogAction)
+  private
+    function GetDialog: TJvSelectDirectory;
+  protected
+    function GetDialogClass: TJvCommonDialogClass; override;
+  published
+    property Dialog: TJvSelectDirectory read GetDialog;
+  end;
+
+
+  TJvConnectNetworkAction = class(TJvCommonDialogAction)
+  private
+    function GetDialog: TJvNetworkConnect;
+  protected
+    function GetDialogClass: TJvCommonDialogClass; override;
+  published
+    property Dialog: TJvNetworkConnect read GetDialog;
+  end;
+
+  TJvFloppyFormatAction = class(TJvCommonDialogPAction)
+  private
+    function GetDialog: TJvFormatDialog;
+  protected
+    function GetDialogClass: TJvCommonDialogPClass; override;
+  published
+    property Dialog: TJvFormatDialog read GetDialog;
+  end;
+
+  TJvOrganizeFavoritesAction = class(TJvCommonDialogAction)
+  private
+    function GetDialog: TJvOrganizeFavoritesDialog;
+  protected
+    function GetDialogClass: TJvCommonDialogClass; override;
+  published
+    property Dialog: TJvOrganizeFavoritesDialog read GetDialog;
+  end;
+
+  TJvControlPanelAction = class(TJvCommonDialogPAction)
+  private
+    function GetDialog: TJvControlPanelDialog;
+  protected
+    function GetDialogClass: TJvCommonDialogPClass; override;
+  published
+    property Dialog: TJvControlPanelDialog read GetDialog;
+  end;
+
+  TJvOpenFileAction = class(TCommonDialogAction)
+  private
+    function GetDialog: TJvOpenDialog;
+  protected
+    function GetDialogClass: TCommonDialogClass; override;
+  published
+    property Caption;
+    property Dialog: TJvOpenDialog read GetDialog;
+    property Enabled;
+    property HelpContext;
+    property Hint;
+    property ImageIndex;
+    property ShortCut;
+    property SecondaryShortCuts;
+    property Visible;
+    property OnAccept;
+    property OnCancel;
+  end;
+
+  TJvSaveFileAction = class(TJvOpenFileAction)
+  private
+    function GetDialog: TJvSaveDialog;
+  protected
+    function GetDialogClass: TCommonDialogClass;override;
+  published
+    property Dialog: TJvSaveDialog read GetDialog;
+  end;
+
+  TJvPageSetupAction = class(TJvCommonDialogAction)
+  private
+    function GetDialog: TJvPageSetupDialog;
+  protected
+    function GetDialogClass: TJvCommonDialogClass; override;
+  published
+    property Dialog: TJvPageSetupDialog read GetDialog;
+  end;
+
+  TJvPageSetupTitledAction = class(TJvCommonDialogAction)
+  private
+    function GetDialog: TJvPageSetupTitledDialog;
+  protected
+    function GetDialogClass: TJvCommonDialogClass; override;
+  published
+    property Dialog: TJvPageSetupTitledDialog read GetDialog;
+  end;
+
+
+
+procedure Register;
+
+implementation
+uses
+  JvActnRes;
+
+procedure Register;
+begin
+  RegisterActions('Jv Dialog',
+    [TJvBrowseFolderAction,TJvSelectDirectoryAction,TJvConnectNetworkAction,TJvFloppyFormatAction,
+    TJvOrganizeFavoritesAction,TJvControlPanelAction,TJvOpenFileAction,TJvSaveFileAction,
+    TJvPageSetupAction,TJvPageSetupTitledAction],
+    // TJvStandardActions is a datamodule with default settings for our actions
+    TJvStandardActions);
+end;
+
+{ TJvCommonDialogAction }
+
+constructor TJvCommonDialogAction.Create(AOwner: TComponent);
+var
+  DialogClass: TJvCommonDialogClass;
+begin
+  inherited Create(AOwner);
+  DialogClass := GetDialogClass;
+  if Assigned(DialogClass) then
+  begin
+    FDialog := DialogClass.Create(Self);
+    FDialog.Name := Copy(DialogClass.ClassName, 2, Length(DialogClass.ClassName));
+    FDialog.SetSubComponent(True);
+  end;
+  DisableIfNoHandler := False;
+  Enabled := True;
+end;
+
+procedure TJvCommonDialogAction.ExecuteTarget(Target: TObject);
+begin
+  FExecuteResult := False;
+  if Assigned(FDialog) then
+  begin
+    if Assigned(FBeforeExecute) then
+      FBeforeExecute(Self);
+    FExecuteResult := FDialog.Execute;
+    if FExecuteResult then
+    begin
+      if Assigned(FOnAccept) then
+        FOnAccept(Self)
+    end
+    else
+      if Assigned(FOnCancel) then
+        FOnCancel(Self);
+  end;
+end;
+
+function TJvCommonDialogAction.GetDialogClass: TJvCommonDialogClass;
+begin
+  Result := nil;
+end;
+
+function TJvCommonDialogAction.HandlesTarget(
+  Target: TObject): Boolean;
+begin
+  Result := true;
+end;
+
+{ TJvCommonDialogPAction }
+
+constructor TJvCommonDialogPAction.Create(AOwner: TComponent);
+var
+  DialogClass: TJvCommonDialogPClass;
+begin
+  inherited Create(AOwner);
+  DialogClass := GetDialogClass;
+  if Assigned(DialogClass) then
+  begin
+    FDialog := DialogClass.Create(Self);
+    FDialog.Name := Copy(DialogClass.ClassName, 2, Length(DialogClass.ClassName));
+    {$IFDEF COMPILER6_UP}
+    FDialog.SetSubComponent(True);
+    {$ENDIF}
+  end;
+  DisableIfNoHandler := False;
+  Enabled := True;
+end;
+
+procedure TJvCommonDialogPAction.ExecuteTarget(Target: TObject);
+begin
+  if Assigned(FDialog) then
+  begin
+    if Assigned(FBeforeExecute) then
+      FBeforeExecute(Self);
+    FDialog.Execute;
+    if Assigned(FAfterExecute) then
+      FAfterExecute(Self);
+  end;
+end;
+
+function TJvCommonDialogPAction.GetDialogClass: TJvCommonDialogPClass;
+begin
+  Result := nil;
+end;
+
+function TJvCommonDialogPAction.HandlesTarget(
+  Target: TObject): Boolean;
+begin
+  Result := true;
+end;
+
+{ TJvBrowseFolderAction }
+
+function TJvBrowseFolderAction.GetDialog: TJvBrowseFolder;
+begin
+  Result := TJvBrowseFolder(FDialog);
+end;
+
+function TJvBrowseFolderAction.GetDialogClass: TJvCommonDialogClass;
+begin
+  Result := TJvBrowseFolder;
+end;
+
+  { TJvSelectDirectoryAction }
+
+function TJvSelectDirectoryAction.GetDialog: TJvSelectDirectory;
+begin
+  Result := TJvSelectDirectory(FDialog);
+end;
+
+function TJvSelectDirectoryAction.GetDialogClass: TJvCommonDialogClass;
+begin
+  Result := TJvSelectDirectory;
+end;
+
+{ TJvConnectNetworkAction }
+
+function TJvConnectNetworkAction.GetDialog: TJvNetworkConnect;
+begin
+  Result := TJvNetworkConnect(FDialog);
+end;
+
+function TJvConnectNetworkAction.GetDialogClass: TJvCommonDialogClass;
+begin
+  Result := TJvNetworkConnect;
+end;
+
+{ TJvFloppyFormatAction }
+
+function TJvFloppyFormatAction.GetDialog: TJvFormatDialog;
+begin
+  Result := TJvFormatDialog(FDialog);
+end;
+
+function TJvFloppyFormatAction.GetDialogClass: TJvCommonDialogPClass;
+begin
+  Result := TJvFormatDialog;
+end;
+
+{ TJvOrganizeFavoritesAction }
+
+function TJvOrganizeFavoritesAction.GetDialog: TJvOrganizeFavoritesDialog;
+begin
+  Result := TJvOrganizeFavoritesDialog(FDialog);
+end;
+
+function TJvOrganizeFavoritesAction.GetDialogClass: TJvCommonDialogClass;
+begin
+  Result := TJvOrganizeFavoritesDialog;
+end;
+
+{ TJvControlPanelAction }
+
+function TJvControlPanelAction.GetDialog: TJvControlPanelDialog;
+begin
+  Result := TJvControlPanelDialog(FDialog);
+end;
+
+function TJvControlPanelAction.GetDialogClass: TJvCommonDialogPClass;
+begin
+  Result := TJvControlPanelDialog;
+end;
+
+{ TJvOpenFileAction }
+
+function TJvOpenFileAction.GetDialog: TJvOpenDialog;
+begin
+  Result := TJvOpenDialog(FDialog);
+end;
+
+function TJvOpenFileAction.GetDialogClass: TCommonDialogClass;
+begin
+  Result := TJvOpenDialog;
+end;
+
+{ TJvSaveFileAction }
+
+function TJvSaveFileAction.GetDialog: TJvSaveDialog;
+begin
+  Result := TJvSaveDialog(FDialog);
+end;
+
+function TJvSaveFileAction.GetDialogClass: TCommonDialogClass;
+begin
+  Result := TJvSaveDialog;
+end;
+
+{ TJvPageSetupAction }
+
+function TJvPageSetupAction.GetDialog: TJvPageSetupDialog;
+begin
+  Result := TJvPageSetupDialog(FDialog);
+end;
+
+function TJvPageSetupAction.GetDialogClass: TJvCommonDialogClass;
+begin
+  Result := TJvPageSetupDialog;
+end;
+
+{ TJvPageSetupTitledAction }
+
+function TJvPageSetupTitledAction.GetDialog: TJvPageSetupTitledDialog;
+begin
+  Result := TJvPageSetupTitledDialog(FDialog);
+end;
+
+function TJvPageSetupTitledAction.GetDialogClass: TJvCommonDialogClass;
+begin
+  Result := TJvPageSetupTitledDialog;
+end;
+
+end.
