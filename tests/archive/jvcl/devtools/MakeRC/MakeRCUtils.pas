ad1e5a90e8803792632da8a8095deabd5a767b6f
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2002-11-02 23:32:47 +0000

    *** empty log message ***

--- tests/archive/jvcl/devtools/MakeRC/MakeRCUtils.pas
+++ tests/archive/jvcl/devtools/MakeRC/MakeRCUtils.pas
@@ -15,14 +15,17 @@ begin
   writeln('MakeRC: creates an RC file from the BMP files in a specific folder');
   writeln('');
   writeln('Usage:');
-  writeln(ExtractFileName(ParamStr(0)), ' <filemask> <filename>');
-  writeln('');
+  writeln(ExtractFileName(ParamStr(0)), ' <filemask(s)> <filename>');
   writeln(#9'<filemask>'#9'files to search for. Defaults to <curdir>\*.bmp');
   writeln(#9'<filename>'#9'name of rc to create. Defaults to <curdir>\new.rc');
+  writeln('or:');
+  writeln('/a <infile> <infile> <infile>');
+  writeln('Output file will be saved as new.rc');
+  writeln('');
   readln;
 end;
 
-procedure MakeRCFile(const FileMask, Filename: string);
+procedure MakeRCFile(const FileMask, Filename: string; Append: boolean);
 var
   fd: TWin32FindData;
   H: THandle;
@@ -34,6 +37,8 @@ begin
   begin
     S := TStringlist.Create;
     try
+      if Append and FileExists(Filename) then
+        S.LoadFromFile(Filename);
       repeat
         S.Add(Format('%s'#9'BITMAP'#9'"%s"', [UpperCase(ChangeFileExt(fd.cFileName, '')), UpperCase(fd.cFileName)]));
         writeln('Adding ', string(fd.cFileName), '...');
@@ -55,34 +60,41 @@ end;
 
 procedure Run;
 var
-  FileMask, Filename: string;
+  FileMask, Filename: string; i: integer; Append: boolean;
 begin
   try
     writeln('MakeRC: creates an RC file from the bitmap files in a specific folder');
+    Append := GetCmdSwitchValue('a', ['-', '/'], FileMask, true);
     if GetCmdSwitchValue('h', ['-', '/'], FileMask, true) or
       GetCmdSwitchValue('?', ['-', '/'], FileMask, true) then
     begin
       ShowHelp;
       Exit;
     end;
-    case ParamCount of
-      0:
-        begin
-          FileMask := ExpandUNCFilename('*.bmp');
-          Filename := ExpandUNCFilename('new.rc');
-        end;
-      1:
+    if Append then
+    begin
+      for i := 2 to ParamCount do
+        MakeRCFile(ParamStr(i), ExpandUNCFilename('new.rc'), true);
+    end
+    else
+      case ParamCount of
+        0:
+          begin
+            FileMask := ExpandUNCFilename('*.bmp');
+            Filename := ExpandUNCFilename('new.rc');
+          end;
+        1:
+          begin
+            FileMask := ExpandUNCFilename(ParamStr(1));
+            Filename := ExpandUNCFilename('new.rc');
+          end;
+      else
         begin
           FileMask := ExpandUNCFilename(ParamStr(1));
           Filename := ExpandUNCFilename('new.rc');
         end;
-    else
-      begin
-        FileMask := ParamStr(1);
-        Filename := ExpandUNCFilename(ParamStr(2));
       end;
-    end;
-    MakeRCFile(FileMask, Filename);
+    MakeRCFile(FileMask, Filename, false);
   except
     on E: Exception do
       writeln('ERROR: ', E.Message);
