d292aef7dd3ede38daf3aff80ad47bea92d2fdb0
jedi_mbe <jedi_mbe@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2002-11-16 13:57:07 +0000

    Added GenImages tool:
    Reads all BMPs from jvcl/images, sets the color at lower left most pixel in the entire image to gray and writes it as BMPS and PNG to dev/images/comp. Uses TPNGImage which can be found here: http://pngdelphi.sourceforge.net/

---
+++ help/tools/GenImages/GenImages.dpr
@@ -0,0 +1,13 @@
+program GenImages;
+{$APPTYPE CONSOLE}
+uses
+  SysUtils,
+  pngzlib in 'pngzlib.pas',
+  pngimage in 'pngimage.pas',
+  pnglang in 'pnglang.pas',
+  UpdBckgrnd in 'UpdBckgrnd.pas',
+  genimage_main in 'genimage_main.pas';
+
+begin
+  Execute;
+end.
