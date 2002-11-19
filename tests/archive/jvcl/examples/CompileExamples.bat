97efede3b85d4f8458722a5b7cdad5643f91b296
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2002-11-19 20:29:01 +0000

    *** empty log message ***

--- tests/archive/jvcl/examples/CompileExamples.bat
+++ tests/archive/jvcl/examples/CompileExamples.bat
@@ -1,8 +1,9 @@
 @echo off
-REM Change the line to use the make program of your choice or from a specific Delphi/BCB version
-REM f ex C:\Program Files\Borland\Delphi5\bin\make.exe to use the Delphi 5 make program etc
-REM Defaults to: make.exe (whatever that is on your system), always build, silent, keep temp files, cache dependency info
-SET MYMAKE=make.exe -B -K -a -c -s -f
+REM Change the line below to use the make program of your choice or from a specific Delphi/BCB version
+REM (f ex C:\Program Files\Borland\Delphi5\bin\make.exe to use the Delphi 5 make)
+REM Defaults to: make.exe (whatever that is on your system), always build
+SET MYMAKE=make.exe -B -f
 REM Make all examples:
+cls
 %MYMAKE% makefile.mak
 
