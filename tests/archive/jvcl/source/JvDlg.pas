e539465d0560dc0edc3d2534e459003fa41b78fa
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2002-11-20 08:03:46 +0000

    Mantis #463

--- tests/archive/jvcl/source/JvDlg.pas
+++ tests/archive/jvcl/source/JvDlg.pas
@@ -11,12 +11,13 @@ the specific language governing rights and limitations under the License.
 The Original Code is: JvDlg.PAS, released on 2002-07-04.
 
 The Initial Developers of the Original Code are: Andrei Prygounkov <a.prygounkov@gmx.de>
-Copyright (c) 1999, 2002 Andrei Prygounkov   
+Copyright (c) 1999, 2002 Andrei Prygounkov
 All Rights Reserved.
 
-Contributor(s): 
+Contributor(s):
+Zinvob
 
-Last Modified: 2002-07-04
+Last Modified: 2002-11-19
 
 You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
 located at http://jvcl.sourceforge.net
@@ -42,37 +43,38 @@ type
 
   TJvProgressForm = class(TComponent)
   private
-    Form : TForm;
-    FProgressBar : TProgressBar;
-    Label1 : TLabel;
+    Form: TForm;
+    FProgressBar: TProgressBar;
+    Label1: TLabel;
 
-    FCaption : TCaption;
-    FInfoLabel : TCaption;
-    FOnShow : TNotifyEvent;
-    FCancel : boolean;
+    FCaption: TCaption;
+    FInfoLabel: TCaption;
+    FOnShow: TNotifyEvent;
+    FCancel: boolean;
     FProgressMin,
-    FProgressMax,
-    FProgressStep,
-    FProgressPosition : integer;
-    EE : Exception;
-    
-    procedure SetCaption(ACaption : TCaption);
-    procedure SetInfoLabel(ACaption : TCaption);
-    procedure FormOnShow(Sender : TObject);
-    procedure FormOnCancel(Sender : TObject);
-    procedure SetProgress(index : integer; AValue : integer);
+      FProgressMax,
+      FProgressStep,
+      FProgressPosition: integer;
+    EE: Exception;
+
+    procedure SetCaption(ACaption: TCaption);
+    procedure SetInfoLabel(ACaption: TCaption);
+    procedure FormOnShow(Sender: TObject);
+    procedure FormOnCancel(Sender: TObject);
+    procedure SetProgress(index: integer; AValue: integer);
   public
+    destructor Destroy; override;
     procedure Execute;
     procedure ProgressStepIt;
-    property Cancel : boolean read FCancel;
+    property Cancel: boolean read FCancel;
   published
-    property Caption : TCaption read FCaption write SetCaption;
-    property InfoLabel : TCaption read FInfoLabel write SetInfoLabel;
-    property ProgressMin : integer index 0 read FProgressMin write SetProgress;
-    property ProgressMax : integer index 1 read FProgressMax write SetProgress;
-    property ProgressStep : integer index 2 read FProgressStep write SetProgress;
-    property ProgressPosition : integer index 3 read FProgressPosition write SetProgress;
-    property OnShow : TNotifyEvent read FOnShow write FOnShow;
+    property Caption: TCaption read FCaption write SetCaption;
+    property InfoLabel: TCaption read FInfoLabel write SetInfoLabel;
+    property ProgressMin: integer index 0 read FProgressMin write SetProgress;
+    property ProgressMax: integer index 1 read FProgressMax write SetProgress;
+    property ProgressStep: integer index 2 read FProgressStep write SetProgress;
+    property ProgressPosition: integer index 3 read FProgressPosition write SetProgress;
+    property OnShow: TNotifyEvent read FOnShow write FOnShow;
   end;
 
 implementation
@@ -80,7 +82,8 @@ implementation
 uses JvCtlConst;
 
 {$IFNDEF BCB3}
-function ChangeTopException(E : TObject): TObject;
+
+function ChangeTopException(E: TObject): TObject;
 type
   PRaiseFrame = ^TRaiseFrame;
   TRaiseFrame = record
@@ -100,7 +103,7 @@ begin
     Result := PRaiseFrame(RaiseList)^.ExceptObject;
     PRaiseFrame(RaiseList)^.ExceptObject := E
   end else
-   Result := nil;
+    Result := nil;
 {$ENDIF COMPLIB_VCL}
 {$IFDEF LINUX}
   // XXX: changing exception in stack frame is not supported on Kylix
@@ -115,30 +118,34 @@ type
 
   TJvProgressFormForm = class(TForm)
   private
-    procedure WMUser1(var Message : TMessage); message wm_User+1;
+    procedure WMUser1(var Message: TMessage); message wm_User + 1;
   end;
 
 procedure TJvProgressForm.Execute;
 begin
- {$IFDEF BCB}
-  Form := TJvProgressFormForm.CreateNew(Self, 1);
- {$ELSE}
-  Form := TJvProgressFormForm.CreateNew(Self);
- {$ENDIF BCB}
+{$IFDEF BCB}
+  if not Assigned(Form) then Form := TJvProgressFormForm.CreateNew(Owner, 1);
+{$ELSE}
+  if not Assigned(Form) then Form := TJvProgressFormForm.CreateNew(Owner);
+{$ENDIF}
   try
     Form.Caption := Caption;
     with Form do
-      begin
-        ClientWidth := 307;
-        ClientHeight := 98;
-        BorderStyle := bsDialog;
-        Position := poScreenCenter;
-        FProgressBar := TProgressBar.Create(Form);
-      end;
+    begin
+      ClientWidth := 307;
+      ClientHeight := 98;
+      BorderStyle := bsDialog;
+      Position := poScreenCenter;
+      FProgressBar := TProgressBar.Create(Form);
+    end;
     with FProgressBar do begin
       Parent := Form;
+      Min := FProgressMin;
+      Max := FProgressMax;
       SetBounds(8, 38, 292, 18);
-      Step := 1;
+      if FProgressStep = 0 then FProgressStep := 1;
+      Step := FProgressStep;
+      Position := FProgressPosition;
     end;
     Label1 := TLabel.Create(Form);
     with Label1 do begin
@@ -153,77 +160,77 @@ begin
       SetBounds(116, 67, 75, 23);
       OnClick := FormOnCancel;
     end;
-    Form.OnShow := FormOnShow;
     FCancel := false;
     if Assigned(FOnShow) then
     begin
+      Form.OnShow := FormOnShow;
       EE := nil;
       Form.ShowModal;
       if EE <> nil then raise EE;
-    end;
+    end
+    else Form.Show;
   finally
-    Form.Free;
-    Form := nil;
+    if Assigned(FOnShow) then FreeAndNil(Form);
   end;
 end;
 
-procedure TJvProgressForm.FormOnShow(Sender : TObject);
+procedure TJvProgressForm.FormOnShow(Sender: TObject);
 begin
   PostMessage(Form.Handle, wm_User + 1, 0, 0);
 end;
 
-procedure TJvProgressForm.FormOnCancel(Sender : TObject);
+procedure TJvProgressForm.FormOnCancel(Sender: TObject);
 begin
   FCancel := true;
 end;
 
-procedure TJvProgressFormForm.WMUser1(var Message : TMessage);
+procedure TJvProgressFormForm.WMUser1(var Message: TMessage);
 begin
   Application.ProcessMessages;
   try
     try
       (Owner as TJvProgressForm).FOnShow(Self);
     except
-      on E : Exception do
+      on E: Exception do
       begin
-       {$IFNDEF BCB3}
+{$IFNDEF BCB3}
         (Owner as TJvProgressForm).EE := E;
         ChangeTopException(nil);
-       {$ENDIF BCB3}
-       {$IFDEF BCB3}
+{$ENDIF BCB3}
+{$IFDEF BCB3}
         (Owner as TJvProgressForm).EE := Exception.Create(E.Message);
-       {$ENDIF BCB3}
+{$ENDIF BCB3}
       end;
     end;
   finally
     ModalResult := mrOk;
-  end;  
+  end;
 end;
 
-procedure TJvProgressForm.SetCaption(ACaption : TCaption);
+procedure TJvProgressForm.SetCaption(ACaption: TCaption);
 begin
   FCaption := ACaption;
   if Form <> nil then Form.Caption := FCaption;
 end;
 
-procedure TJvProgressForm.SetInfoLabel(ACaption : TCaption);
+procedure TJvProgressForm.SetInfoLabel(ACaption: TCaption);
 begin
   FInfoLabel := ACaption;
   if Form <> nil then Label1.Caption := ACaption;
 end;
 
-procedure TJvProgressForm.SetProgress(index : integer; AValue : integer);
+procedure TJvProgressForm.SetProgress(index: integer; AValue: integer);
 begin
   case index of
-    0 : FProgressMin := AValue;
-    1 : FProgressMax := AValue;
-    2 : FProgressStep := AValue;
-    3 : FProgressPosition := AValue;
+    0: FProgressMin := AValue;
+    1: FProgressMax := AValue;
+    2: FProgressStep := AValue;
+    3: FProgressPosition := AValue;
   end;
   if Form <> nil then
   begin
-    FProgressBar.Min  := FProgressMin ;
-    FProgressBar.Max  := FProgressMax ;
+    FProgressBar.Min := FProgressMin;
+    FProgressBar.Max := FProgressMax;
     FProgressBar.Step := FProgressStep;
     FProgressBar.Position := FProgressPosition;
   end;
@@ -235,6 +242,11 @@ begin
     FProgressBar.StepIt;
 end;
 
-end.
+destructor TJvProgressForm.Destroy;
+begin
+  if Assigned(Form) then Form.Free;
+  inherited;
+end;
 
+end.
 
