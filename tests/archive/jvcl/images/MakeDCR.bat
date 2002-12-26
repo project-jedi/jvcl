b284e1f31d2fbc94ad043b733e068bcb55428dc2
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2002-12-26 23:09:35 +0000

    New and altered palette images from Rober Marquardt + new rc scripts for recently added components

--- tests/archive/jvcl/images/MakeDCR.bat
+++ tests/archive/jvcl/images/MakeDCR.bat
@@ -1,2 +1,5 @@
 brc32 JVCLReg.rc -r -fo..\Resources\JVCLReg.dcr
-
+brc32 RACtlReg.rc -r -fo..\Resources\RACtl.dcr
+brc32 JvDatePickerReg.rc -r -fo..\Resources\JvDatePickerReg.dcr
+brc32 RADBReg.rc -r -fo..\Resources\RADB.dcr
+brc32 RAIReg.rc -r -fo..\Resources\RAI.dcr
