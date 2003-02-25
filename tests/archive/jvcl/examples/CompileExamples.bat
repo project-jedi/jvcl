008e6423b6f473691ca3fdfa488a837d9a78a8cb
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-02-25 17:52:54 +0000

    Committed by mistake :(

--- tests/archive/jvcl/examples/CompileExamples.bat
+++ tests/archive/jvcl/examples/CompileExamples.bat
@@ -2,8 +2,8 @@
 REM Change the line below to use the make program of your choice or from a specific Delphi/BCB version
 REM (f ex C:\Program Files\Borland\Delphi5\bin\make.exe to use the Delphi 5 make)
 REM Defaults to: make.exe (whatever that is on your system), always build
-SET MYMAKE=D:\Borland\Delphi5\bin\make.exe -B -f
+SET JVCLMAKE=make.exe -B -f
 REM Make all examples:
 cls
-%MYMAKE% makefile.mak
+%JVCLMAKE% makefile.mak
 
