2e2f04875c153ebbd2e75f8f184f6c4928e6127a
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-02-27 10:09:04 +0000

    Added "check modify" and  "recurse" options

--- tests/archive/jvcl/devtools/JvAdjustLineBreaks/crlfutils.pas
+++ tests/archive/jvcl/devtools/JvAdjustLineBreaks/crlfutils.pas
@@ -10,51 +10,64 @@ uses
 
 procedure ShowHelp;
 begin
-  writeln('JEDI CR(LF) utility: converts LF to CRLF or CRLF to LF');
   writeln('');
+  writeln('JEDI CR(LF) version 0.1: LF->CRLF and CRLF->LF converter.');
+  writeln('===================================================');
   writeln('Usage:');
-  writeln('crlf [/l|/w] <filemask> [/l|/w] <filemask> (etc)');
+  writeln('crlf [options] <filemask> [options] <filemask> (etc)');
   writeln('');
-  writeln('where');
-  writeln('  /l - convert to Linux line breaks (LF)');
-  writeln('  /w - convert to Windows line breaks (CRLF)');
+  writeln('where [options] can be any combination of:');
+  writeln('  /s - recurse into sub-folders from this point on');
+  writeln('  /c - compare before write: only write if file has changed (default)');
+  writeln('  /u - do NOT compare before write: always write');
+  writeln('  /l - convert to Linux line breaks (LF) (default on Linux)');
+  writeln('  /w - convert to Windows line breaks (CRLF) (default on Windows)');
+  writeln('');
+  writeln('<filemask> accepts wildcards (* and ?).');
   writeln('');
   writeln('Example:');
-  writeln('crlf /l *.pas /w *.dfm');
-  writeln('Converts the pas files to LF and the dfm files to CRLF');
+  writeln('========');
+  writeln('crlf /u /l *.pas /c /w /s *.dfm');
+  writeln('Converts the pas files to LF (always writes) and the dfm files to CRLF (writes if modified). Recurses into sub-folders when searching for dfm''s.');
   writeln('');
   writeln('Example:');
+  writeln('========');
   writeln('crlf *.pas *.dfm');
-  writeln('Converts the pas and dfm files to LF on Linux and CRLF on Windows (system default)');
+  writeln('Converts the pas and dfm files to LF on Linux and CRLF on Windows (system default). Always checks before write (no /u option).');
   writeln('');
   writeln('');
-  writeln('NOTE: if you compile in Delphi 5 or earlier,');
-  writeln('      CRLF->LF is NOT supported!');
+  writeln('NOTE: if you compiled using Delphi 5 or earlier, CRLF->LF is NOT supported!');
 end;
 
-procedure ConvertFile(const Filename:string;ToWindows:boolean);
+procedure ConvertFile(const Filename:string;ToWindows,CompareBeforeWrite:boolean);
 {$IFDEF COMPILER6_UP}
 const
   cStyle:array[boolean] of TTextLineBreakStyle = (tlbsLF,tlbsCRLF);
 {$ENDIF COMPILER6_UP}
-var S:TFileStream;tmp:string;
+var
+  F:TFileStream;
+  tmp,tmp2:string;
 begin
-  S := TFileStream.Create(Filename,fmOpenReadWrite or fmShareExclusive );
+  F := TFileStream.Create(Filename,fmOpenReadWrite or fmShareExclusive );
   try
-    SetLength(tmp,S.Size);
-    if S.Size > 0 then
+    SetLength(tmp,F.Size);
+    if F.Size > 0 then
     begin
-      S.Read(tmp[1],S.Size);
+      F.Read(tmp[1],F.Size);
+      if CompareBeforeWrite then
+        tmp2 := tmp;
       tmp := AdjustLineBreaks(tmp{$IFDEF COMPILER6_UP},cStyle[ToWindows]{$ENDIF});
-      S.Size := 0; 
-      S.Write(tmp[1],Length(tmp));
+      if CompareBeforeWrite and (tmp = tmp2) then
+        Exit;
+      F.Size := 0;
+      F.Write(tmp[1],Length(tmp));
     end;
   finally
-    S.Free;
+    F.Free;
   end;
 end;
 
-function ConvertFiles(const FileMask:string;ToWindows:boolean):integer;
+function ConvertFiles(const FileMask:string;ToWindows,CompareBeforeWrite,Recurse:boolean):integer;
 var
   SearchHandle:DWORD;
   FindData:TWin32FindData;
@@ -72,11 +85,13 @@ begin
           writeln('ERROR: ',FindData.cFileName,' is read-only!')
         else
         begin
-          writeln('Converting ',FindData.cFileName,'...');
-          ConvertFile(APath + FindData.cFileName,ToWindows);
+          writeln(FindData.cFileName);
+          ConvertFile(APath + FindData.cFileName,ToWindows,CompareBeforeWrite);
           Inc(Result);
         end;
-      end;
+      end
+      else if Recurse and (FindData.cFileName[0] <> '.') then
+        ConvertFiles(IncludeTrailingPathdelimiter(APath + FindData.cFileName) + ExtractFilename(Filemask),ToWindows,CompareBeforeWrite,true);
     until not FindNextFile(SearchHandle,FindData);
   finally
     Windows.FindClose(SearchHandle);
@@ -84,20 +99,27 @@ begin
 end;
 
 procedure Run;
+const
+  cCurrentOS:array[boolean] of PChar = ('(CRLF->LF)','(LF->CRLF)');
 var
-  ToWindows:boolean;
+  ToWindows,CompareBeforeWrite,Recurse:boolean;
   i,Count:integer;
 begin
-  // cmd line: -l *.pas *.dfm *.txt -w *.xfm
+  // cmd line: -l *.pas *.dfm *.txt -c -w *.xfm
   // where
   // -l - convert CRLF to LF (to linux)
   // -w - convert LF to CRLF (to windows)
+  // -c - check content: only write if file has changed (default)
+  // -u - never check content: always write
+
   Count := 0;
   if ParamCount = 0 then
   begin
     ShowHelp;
     Exit;
   end;
+  CompareBeforeWrite := true;
+  Recurse := false;
   // set depending on target
   ToWindows := true;
   {$IFDEF LINUX}
@@ -105,25 +127,44 @@ begin
   {$ENDIF}
   for i := 1 to ParamCount do
   begin
-    if AnsiSameText(ParamStr(i),'/l') or AnsiSameText(ParamStr(i),'-l') then
+    if SameText(ParamStr(i),'/l') or SameText(ParamStr(i),'-l') then
     begin
       ToWindows := false;
-      Continue;
-    end;
-    if AnsiSameText(ParamStr(i),'/w') or AnsiSameText(ParamStr(i),'-w') then
-    begin
-      ToWindows := true;
+      writeln('Converting ', cCurrentOS[ToWindows],':');
       Continue;
     end
-    else if AnsiSameText(ParamStr(i),'/?') or AnsiSameText(ParamStr(i),'-?') or
-       AnsiSameText(ParamStr(i),'/h') or AnsiSameText(ParamStr(i),'-h')then
+    else if SameText(ParamStr(i),'/w') or SameText(ParamStr(i),'-w') then
+    begin
+      ToWindows := true;
+      writeln('Converting ', cCurrentOS[ToWindows],':');
+      Continue;
+    end
+    else if SameText(ParamStr(i),'/?') or SameText(ParamStr(i),'-?') or
+       SameText(ParamStr(i),'/h') or SameText(ParamStr(i),'-h')then
     begin
       ShowHelp;
       Exit;
-    end;
-    Inc(Count,ConvertFiles(ExpandUNCFilename(ParamStr(i)),ToWindows));
+    end
+    else if SameText(ParamStr(i),'/c') or SameText(ParamStr(i),'-c') then
+    begin
+      CompareBeforeWrite := true;
+      Continue;
+    end
+    else if SameText(ParamStr(i),'/u') or SameText(ParamStr(i),'-u') then
+    begin
+      CompareBeforeWrite := false;
+      Continue;
+    end
+    else if SameText(ParamStr(i),'/s') or SameText(ParamStr(i),'-s') then
+    begin
+      Recurse := true;
+      Continue;
+    end
+    else
+      Inc(Count,ConvertFiles(ExpandUNCFilename(ParamStr(i)),ToWindows,CompareBeforeWrite,Recurse));
   end;
-  writeln(Count,' files converted.')
+  writeln('');
+  writeln('Done: ', Count,' files converted.');
 end;
 
 end.
