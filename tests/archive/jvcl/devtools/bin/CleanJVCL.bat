ae7ee2914d589e0cb5c601eb4e3d442472698fd5
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-03-06 17:02:26 +0000

    Updated with new tools to clean JVCL for release

--- tests/archive/jvcl/devtools/bin/CleanJVCL.bat
+++ tests/archive/jvcl/devtools/bin/CleanJVCL.bat
@@ -1,11 +1,30 @@
 @echo off
-echo. Copy this batch (and the jconvert and crlf programs from \devtools) to the folder *above JVCL* before running it
+echo. Run this batch to clean up the JVCL before release or if you just want to get it in "mint" condition
 echo. This batch does the following:
-echo. 1. Deletes all files created from compiling (dcu's, dsk's etc)
-echo. 2. Converts all dfm's to text format (if necessary)
-echo. 3. Converts all files from Linux format to Windows format (if necessary)
-echo. Hit Ctrl+C now to quit
+echo. 1. Strips unwanted properties from dfm's (and converts them to text)
+echo. 2. Converts all remaining dfm's to text format (if necessary)
+echo. 3. Converts all files from Linux LF format to Windows CRLF format (if necessary)
+echo. 4. Deletes all files created from compiling (dcu's, dsk's exe's etc)
+echo. 
+echo. Hit Ctrl+C NOW if you want to quit, any other key to run the batch
 pause
-del /s *.dcu *.ddp *.dsk
-jconvert -i -t -s *.dfm
-crlf -s -q *.pas *.dfm *.dpk *.dpr
+cd ..
+rem Build the tools we need:
+make
+cd bin
+rem Fix CRLF corruption:
+crlf -s -q ..\..\..\*.pas ..\..\..\*.dfm ..\..\..\*.dpk ..\..\..\*.dpr
+echo.
+rem Remove unwanted properties (make D5 compatible):
+dc.exe -i -s -fskiplistd5.txt ..\..\..\*.dfm
+echo.
+rem Convert remaining DFM's to text:
+jconvert -i -t -s ..\..\..\*.dfm
+echo.
+cd ..
+cd ..
+rem Delete garbage:
+del /s /q *.dcu *.ddp *.dsk *.exe >NUL
+rem Go back were we started:
+cd devtools\bin
+
