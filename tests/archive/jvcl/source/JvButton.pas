16e3fa79f561ad1e53d91036bc3703903d469870
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-04-01 16:27:25 +0000

    Added ForceSameSize to TJvCustomGraphicButton Fixed DropDownMenu bugs

--- tests/archive/jvcl/source/JvButton.pas
+++ tests/archive/jvcl/source/JvButton.pas
@@ -40,7 +40,7 @@ const
 type
   TCmForceSize = record
     Msg: Cardinal;
-    Sender: TWinControl;
+    Sender: TControl;
     NewSize: TSmallPoint;
     Result: Longint;
   end;
@@ -59,6 +59,7 @@ type
     FDropDownMenu: TPopupMenu;
     FDown: boolean;
     FOnParentColorChanged: TNotifyEvent;
+    FForceSameSize: boolean;
     procedure SetCaption(const Value: TCaption);
     procedure SetFlat(const Value: boolean);
     procedure SetDown(const Value: boolean);
@@ -67,6 +68,8 @@ type
     procedure CMDialogChar(var Msg: TCMDialogChar); message CM_DIALOGCHAR;
     procedure CMEnabledChanged(var Msg: TMessage); message CM_ENABLEDCHANGED;
     procedure CMParentColorChanged(var Msg: TMessage); message CM_PARENTCOLORCHANGED;
+    procedure SetForceSameSize(const Value: boolean);
+    procedure CMForceSize(var Msg: TCMForceSize); message CM_FORCESIZE;
   protected
     procedure Notification(AComponent: TComponent; Operation: TOperation); override;
     procedure MouseDown(Button: TMouseButton; Shift: TShiftState; X, Y: Integer); override;
@@ -79,6 +82,7 @@ type
     procedure PaintFrame(Canvas: TCanvas); virtual;
     function InsideBtn(X, Y: Integer): Boolean; virtual;
     property MouseStates:TJvButtonMouseStates read FStates write FStates default [];
+    property ForceSameSize:boolean read FForceSameSize write SetForceSameSize default false;
     property Pattern:TBitmap read FPattern;
     property Caption:TCaption read FCaption write SetCaption;
     property Flat:boolean read FFlat write SetFlat default True;
@@ -89,6 +93,8 @@ type
     property OnMouseExit: TNotifyEvent read FOnMouseExit write FOnMouseExit;
     property OnParentColorChange: TNotifyEvent read FOnParentColorChanged write FOnParentColorChanged;
   public
+    procedure SetBounds(ALeft: Integer; ATop: Integer; AWidth: Integer;
+      AHeight: Integer); override;
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
   end;
@@ -102,7 +108,7 @@ type
     FSaved: TColor;
     FOnCtl3DChanged: TNotifyEvent;
     FOnParentColorChanged: TNotifyEvent;
-    FDropMenu: TPopupMenu;
+    FDropDownMenu: TPopupMenu;
     FHotTrack: Boolean;
     FHotFont: TFont;
     FFontSave: TFont;
@@ -122,7 +128,8 @@ type
     procedure DoMouseLeave; dynamic;
     procedure CreateParams(var Params: TCreateParams); override;
     function GetRealCaption: string;dynamic;
-
+    procedure Notification(AComponent: TComponent; Operation: TOperation);
+      override;
   public
     procedure Click; override;
     constructor Create(AOwner: TComponent); override;
@@ -131,8 +138,8 @@ type
       AHeight: Integer); override;
   protected
     property WordWrap:boolean read FWordWrap write SetWordWrap default true;
-    property ForceSameSize:boolean read FForceSameSize write SetForceSameSize;
-    property DropDownMenu: TPopupMenu read FDropMenu write FDropMenu;
+    property ForceSameSize:boolean read FForceSameSize write SetForceSameSize default false;
+    property DropDownMenu: TPopupMenu read FDropDownMenu write FDropDownMenu;
     property HotTrack: Boolean read FHotTrack write FHotTrack default False;
     property HotTrackFont: TFont read FHotFont write SetHotFont;
     property HintColor: TColor read FColor write FColor default clInfoBk;
@@ -180,6 +187,7 @@ begin
   FPattern := CreateBrushPattern;
   FBuffer := TBitmap.Create;
   FFlat := True;
+  FForceSameSize := false;
 end;
 
 destructor TJvCustomGraphicButton.Destroy;
@@ -296,8 +304,9 @@ begin
   begin
     { calc where to put menu }
     Tmp := ClientToScreen(Point(0, Height));
+    FDropDownMenu.PopupComponent := self; 
+
     FDropDownMenu.Popup(Tmp.X, Tmp.Y);
-    // (rom) needs to be checked
     { wait 'til menu is done }
     while PeekMessage(Msg, 0, WM_MOUSEFIRST, WM_MOUSELAST, PM_REMOVE) do
       {nothing};
@@ -395,7 +404,7 @@ begin
 //  ControlStyle := ControlStyle + [csAcceptsControls];
   FOver := False;
   FWordWrap := true;
-
+  FForceSameSize := false;
 end;
 
 destructor TJvCustomButton.Destroy;
@@ -408,9 +417,10 @@ end;
 procedure TJvCustomButton.Click;
 begin
   inherited Click;
-  if FDropMenu <> nil then
+  if FDropDownMenu <> nil then
   begin
-    FDropMenu.Popup(GetClientOrigin.x, GetClientOrigin.y + Height);
+    FDropDownMenu.PopupComponent := self;
+    FDropDownMenu.Popup(GetClientOrigin.x, GetClientOrigin.y + Height);
     Perform(CM_MOUSELEAVE, 0, 0);
   end;
 end;
@@ -542,6 +552,54 @@ end;
 
 
 procedure TJvCustomButton.CMForceSize(var Msg: TCMForceSize);
+begin
+  with Msg do
+    if Sender <> Self then
+      with NewSize do
+        inherited SetBounds(Left, Top, X, Y);
+end;
+
+procedure TJvCustomButton.Notification(AComponent: TComponent;
+  Operation: TOperation);
+begin
+  inherited;
+  if (Operation = opRemove) and (AComponent = FDropDownMenu) then
+    DropDownMenu := nil;
+end;
+
+procedure TJvCustomGraphicButton.SetForceSameSize(const Value: boolean);
+begin
+  if FForceSameSize <> Value then
+  begin
+    FForceSameSize := Value;
+    if FForceSameSize then
+      SetBounds(Left,Top,Width,Height);
+  end;
+end;
+
+procedure TJvCustomGraphicButton.SetBounds(ALeft, ATop, AWidth,
+  AHeight: Integer);
+var
+  Form: TCustomForm;
+  Msg: TCMForceSize;
+begin
+  inherited;
+  if ForceSameSize then
+  begin
+    Form := GetParentForm(Self);
+    if Assigned(Form) then
+    begin
+      // (p3) what is this rect doing here?
+      Msg.Msg := CM_FORCESIZE;
+      Msg.Sender := self;
+      Msg.NewSize.x := AWidth;
+      Msg.NewSize.y := AHeight;
+      Form.Broadcast(Msg);
+    end;
+  end;
+end;
+
+procedure TJvCustomGraphicButton.CMForceSize(var Msg: TCMForceSize);
 begin
   with Msg do
     if Sender <> Self then
