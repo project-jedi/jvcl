e13febd5a842af92be800612beef24a709302d20
jedi_mbe <jedi_mbe@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-04-14 07:35:41 +0000

    Filling of tree in code

--- tests/filler/examples/Test1/MainTest1.pas
+++ tests/filler/examples/Test1/MainTest1.pas
@@ -5,7 +5,7 @@ interface
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
   JvFillFontList, StdCtrls, JvListBox, JvFillerControls, JvLabel,
-  JvComponent, JvFillBasicImpl, JvFillStringList;
+  JvComponent, JvFillBasicImpl, JvFillStringList, JvxCtrls;
 
 type
   TForm1 = class(TForm)
@@ -35,6 +35,7 @@ implementation
 {$R *.DFM}
 
 uses
+  JvFillIntf,
   JvFillerEditor;
 
 procedure TForm1.btnEditStringsClick(Sender: TObject);
@@ -48,8 +49,48 @@ begin
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
+var
+  FilItemsIntf: IFillerItems;
+  FilManIntf: IFillerItemManagment;
 begin
   JvTreeFiller1 := TJvTreeFiller.Create(Self);
+  if not JvTreeFiller1.GetInterface(IFillerItemManagment, FilManIntf) then
+    raise Exception.Create('No managment interface');
+  with FilManIntf do
+//  begin
+    (New as IFillerItemText).Caption := 'Item 1';
+  with FilManIntf do
+    (New as IFillerItemText).Caption := 'Item 2';
+  with FilManIntf do
+    (New as IFillerItemText).Caption := 'Item 3';
+  with FilManIntf do
+    (New as IFillerItemText).Caption := 'Item 4';
+//  end;
+  if not JvTreeFiller1.GetInterface(IFillerItems, FilItemsIntf) then
+    raise Exception.Create('No items interface');
+  with FilItemsIntf do
+  begin
+    with (getItem(0) as IFillerItems) as IFillerItemManagment do
+//    begin
+      (New as IFillerItemText).Caption := 'Item 1.1';
+    with (getItem(0) as IFillerItems) as IFillerItemManagment do
+      (New as IFillerItemText).Caption := 'Item 1.2';
+    with (getItem(0) as IFillerItems) as IFillerItemManagment do
+      (New as IFillerItemText).Caption := 'Item 1.3';
+//    end;
+    with (getItem(2) as IFillerItems) as IFillerItemManagment do
+//    begin
+      (New as IFillerItemText).Caption := 'Item 2.1';
+    with (getItem(2) as IFillerItems) as IFillerItemManagment do
+      (New as IFillerItemText).Caption := 'Item 2.2';
+    with (getItem(2) as IFillerItems) as IFillerItemManagment do
+      (New as IFillerItemText).Caption := 'Item 2.3';
+    with (getItem(2) as IFillerItems) as IFillerItemManagment do
+      (New as IFillerItemText).Caption := 'Item 2.4';
+    with (getItem(2) as IFillerItems) as IFillerItemManagment do
+      (New as IFillerItemText).Caption := 'Item 2.5';
+//    end;
+  end;
 end;
 
 procedure TForm1.FormDestroy(Sender: TObject);
