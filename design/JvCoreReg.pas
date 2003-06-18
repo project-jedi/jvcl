0c1a510337e6c09a9871ab52bca0b29451726034
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-06-18 16:15:38 +0000

    *** empty log message ***

--- design/JvCoreReg.pas
+++ design/JvCoreReg.pas
@@ -6,14 +6,19 @@ procedure Register;
 
 implementation
 uses
-  Classes, ActnList, JvJVCLAbout, JvActions, JvActnRes, JvxDConst;
+  // About JVCL
+  Classes, ActnList, DesignIntf,
+  JvxDConst,
+  JvActions, JvActnRes, JvJVCLAbout, JvJVCLAboutProperty, JVCLVer;
+  // JvWndProcHook
 
 {.$R ..\resources\JvCoreReg.dcr}
 
 procedure Register;
 begin
-  RegisterComponents('Jv Standard',[TJvJVCLAboutComponent]);
-  RegisterActions('JVCL',[TJvSendMail, TJvWebAction],TJvStandardActions);
+  RegisterComponents('Jv Core',[TJvJVCLAboutComponent {,TJvWindowHook}]);
+  RegisterPropertyEditor(TypeInfo(TJVCLAboutInfo), nil, 'AboutJVCL', TJVCLAboutDialogProperty);
+  RegisterActions(srJVCLActions, [TJvSendMail, TJvWebAction], TJvStandardActions);
 end;
 
 end.
