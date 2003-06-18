02fda5a1504c6591a4b58d4ee3f632f8912d1392
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-06-18 13:58:44 +0000

    *** empty log message ***

--- jvcl/design/JvCoreReg.pas
+++ jvcl/design/JvCoreReg.pas
@@ -2,6 +2,18 @@ unit JvCoreReg;
 
 interface
 
+procedure Register;
+
 implementation
+uses
+  Classes, ActnList, JvJVCLAbout, JvActions, JvActnRes, JvxDConst;
+
+{.$R ..\resources\JvCoreReg.dcr}
+
+procedure Register;
+begin
+  RegisterComponents('Jv Standard',[TJvJVCLAboutComponent]);
+  RegisterActions('JVCL',[TJvSendMail, TJvWebAction],TJvStandardActions);
+end;
 
 end.
