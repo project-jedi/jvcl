c0998136855d0fc494d0120697d4a11872d4e8a9
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-09-11 13:07:34 +0000

    now compiling, but not linking refer to newsgroups for details

--- jvcl/packages/bcb6/JvCoreC6D.cpp
+++ jvcl/packages/bcb6/JvCoreC6D.cpp
@@ -3,6 +3,7 @@
 #include <basepch.h>
 #pragma hdrstop
 USEFORMNS("..\..\design\JvStringsForm.pas", Jvstringsform, JvStrEditDlg);
+USEFORMNS("..\..\design\JvProviderTreeListFrame.pas", Jvprovidertreelistframe, fmeJvProviderTreeList); /* TFrame: File Type */
 USEFORMNS("..\..\design\JvBaseDsgnForm.pas", Jvbasedsgnform, JvBaseDesign);
 USEFORMNS("..\..\design\JvBaseDsgnFrame.pas", Jvbasedsgnframe, fmeJvBaseDesign); /* TFrame: File Type */
 USEFORMNS("..\..\design\JvBaseDsgnToolbarFrame.pas", Jvbasedsgntoolbarframe, fmeJvBaseToolbarDesign); /* TFrame: File Type */
@@ -10,7 +11,6 @@ USEFORMNS("..\..\design\JvDataConsumerContextSelectForm.pas", Jvdataconsumercont
 USEFORMNS("..\..\design\JvDataConsumerItemSelectForm.pas", Jvdataconsumeritemselectform, frmJvDataConsumerItemSelect);
 USEFORMNS("..\..\design\JvDataProviderDesignerForm.pas", Jvdataproviderdesignerform, frmDataProviderDesigner);
 USEFORMNS("..\..\design\JvDateTimeForm.pas", Jvdatetimeform, frmSelectDateTimeDlg);
-USEFORMNS("..\..\design\JvProviderTreeListFrame.pas", Jvprovidertreelistframe, fmeJvProviderTreeList); /* TFrame: File Type */
 USEFORMNS("..\..\design\JvStdToolbarDsgnFrame.pas", Jvstdtoolbardsgnframe, fmeJvStdToolbarDesign); /* TFrame: File Type */
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
