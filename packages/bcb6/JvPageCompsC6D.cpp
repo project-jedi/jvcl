2d88521bd5ea512eb3c4ce79432e6a3fb0b1e5c2
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-09-12 14:21:45 +0000

    Compiling, linking, installing

---
+++ packages/bcb6/JvPageCompsC6D.cpp
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------------
+
+#include <basepch.h>
+#pragma hdrstop
+USEFORMNS("..\..\design\JvTreeItemsEditorForm.pas", Jvtreeitemseditorform, frmTreeViewItems);
+USEFORMNS("..\..\design\JvPageLinkEditorForm.pas", Jvpagelinkeditorform, frmJvTreeViewLinksEditor);
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+//---------------------------------------------------------------------------
+
+//   Package source.
+//---------------------------------------------------------------------------
+
+#pragma argsused
+int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void*)
+{
+        return 1;
+}
+//---------------------------------------------------------------------------
