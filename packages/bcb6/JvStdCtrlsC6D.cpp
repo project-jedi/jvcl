ab2ed703e2aa41c5cfd196dd13a56bda0142a1ea
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-09-13 08:17:25 +0000

    Compiling, Linking, Installing

---
+++ packages/bcb6/JvStdCtrlsC6D.cpp
@@ -0,0 +1,18 @@
+//---------------------------------------------------------------------------
+
+#include <basepch.h>
+#pragma hdrstop
+USEFORMNS("..\..\design\JvCheckedItemsForm.pas", Jvcheckeditemsform, JvCheckItemsEditor);
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+//---------------------------------------------------------------------------
+
+//   Package source.
+//---------------------------------------------------------------------------
+
+#pragma argsused
+int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void*)
+{
+        return 1;
+}
+//---------------------------------------------------------------------------
