83e563f62725772339ee03a5d5ce0b325df2a169
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-09-13 14:19:21 +0000

    Compiling and linking

---
+++ jvcl/packages/bcb6/JvMMC6R.cpp
@@ -0,0 +1,18 @@
+//---------------------------------------------------------------------------
+
+#include <basepch.h>
+#pragma hdrstop
+USEFORMNS("..\..\run\JvVirtualKeySelectionFrame.pas", Jvvirtualkeyselectionframe, JvVirtualKeySelectionFrame); /* TFrame: File Type */
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+//---------------------------------------------------------------------------
+
+//   Package source.
+//---------------------------------------------------------------------------
+
+#pragma argsused
+int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void*)
+{
+        return 1;
+}
+//---------------------------------------------------------------------------
