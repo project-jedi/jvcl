19faca1fd6f5fb24c1e6a66cee7feb431bc308a4
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-09-13 08:46:07 +0000

    Compiling, linking,Installing

---
+++ packages/bcb6/JvCtrlsC6D.cpp
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------------
+
+#include <basepch.h>
+#pragma hdrstop
+USEFORMNS("..\..\design\JvSpeedbarForm.pas", Jvspeedbarform, JvSpeedbarEditor);
+USEFORMNS("..\..\design\JvHTHintForm.pas", Jvhthintform, JvHintEditor);
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+//---------------------------------------------------------------------------
+
+//   Package source.
+//---------------------------------------------------------------------------
+
+#pragma argsused
+int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void*)
+{
+        return 1;
+}
+//---------------------------------------------------------------------------
