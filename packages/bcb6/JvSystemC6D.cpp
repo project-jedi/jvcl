e7badee7ca94e54bf4bf2fc5268807990f16c072
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-09-13 07:44:21 +0000

    Compiling, but not linking : Acces Violation

---
+++ packages/bcb6/JvSystemC6D.cpp
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------------
+
+#include <basepch.h>
+#pragma hdrstop
+USEFORMNS("..\..\design\JvFormPropertiesForm.pas", Jvformpropertiesform, JvFormPropsDlg);
+USEFORMNS("..\..\design\JvMinMaxForm.pas", Jvminmaxform, MinMaxInfoEditDialog);
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+//---------------------------------------------------------------------------
+
+//   Package source.
+//---------------------------------------------------------------------------
+
+#pragma argsused
+int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void*)
+{
+        return 1;
+}
+//---------------------------------------------------------------------------
