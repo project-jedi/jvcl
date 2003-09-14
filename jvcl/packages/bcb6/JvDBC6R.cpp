c54230a0716ff0b6fd242f078d0321f63ea7ff63
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-09-14 03:57:53 +0000

    Compiling and linking

---
+++ jvcl/packages/bcb6/JvDBC6R.cpp
@@ -0,0 +1,18 @@
+//---------------------------------------------------------------------------
+
+#include <basepch.h>
+#pragma hdrstop
+USEFORMNS("..\..\run\JvDBQueryParamsForm.pas", Jvdbqueryparamsform, JvQueryParamsDialog);
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+//---------------------------------------------------------------------------
+
+//   Package source.
+//---------------------------------------------------------------------------
+
+#pragma argsused
+int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void*)
+{
+        return 1;
+}
+//---------------------------------------------------------------------------
