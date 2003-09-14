961bf80a4892e97797e98af883bc7e6f298887c8
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-09-14 03:58:05 +0000

    Compiling, Linking, and installing

---
+++ jvcl/packages/bcb6/JvDBC6D.cpp
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------------
+
+#include <basepch.h>
+#pragma hdrstop
+USEFORMNS("..\..\design\JvSelectDataSetForm.pas", Jvselectdatasetform, JvSelectDataSetForm);
+USEFORMNS("..\..\design\JvCsvDataForm.pas", Jvcsvdataform, JvCsvDefStrDialog);
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+//---------------------------------------------------------------------------
+
+//   Package source.
+//---------------------------------------------------------------------------
+
+#pragma argsused
+int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void*)
+{
+        return 1;
+}
+//---------------------------------------------------------------------------
