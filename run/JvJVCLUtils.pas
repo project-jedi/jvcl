eeb996b67bd00987bf5f5898abb72b0a38a474d1
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-09-24 13:44:38 +0000

    Added MPL1.1 Header

--- run/JvJVCLUtils.pas
+++ run/JvJVCLUtils.pas
@@ -1,3 +1,27 @@
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/MPL-1.1.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: JvJVCLUtils.PAS, released on 2002-09-24.
+
+The Initial Developers of the Original Code are: Fedor Koshevnikov, Igor Pavluk and Serge Korolev
+Copyright (c) 1997, 1998 Fedor Koshevnikov, Igor Pavluk and Serge Korolev
+Copyright (c) 2001,2002 SGB Software
+All Rights Reserved.
+
+Last Modified: 2002-09-24
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
+located at http://jvcl.sourceforge.net
+
+Known Issues:
+-----------------------------------------------------------------------------}
 {$I JVCL.INC}
 {$I WINDOWSONLY.INC}
 unit JvJVCLUtils;
@@ -16,8 +40,8 @@ uses
 {$IFNDEF COMPILER6_UP}
 type
   EOSError = class(EWin32Error);
-  {$ENDIF}
-  
+{$ENDIF}
+
 // Transform an icon to a bitmap
 function IconToBitmap(Ico: HICON): TBitmap;
 // Transform an icon to a bitmap using an image list
@@ -7405,4 +7429,5 @@ initialization
 finalization
   ReleaseBitmap;
 { end from JvVCLUtils }
+
 end.
