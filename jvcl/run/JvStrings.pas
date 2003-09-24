0213a9673160f30b1747024f7863db6a4efc3036
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-09-24 11:14:21 +0000

    removed function result warning (update)

--- jvcl/run/JvStrings.pas
+++ jvcl/run/JvStrings.pas
@@ -31,7 +31,10 @@ unit JvStrings;
 interface
 
 uses
-  Windows, Messages, SysUtils, Classes, Graphics, dialogs;
+{$IFDEF MSWINDOWS}
+  Windows,
+{$ENDIF}
+  SysUtils, Classes, Graphics, dialogs;
 
 {regular expressions}
 
@@ -121,7 +124,9 @@ function PosTextLast(const FindString, SourceString: string): integer;
 {finds the last occurance}
 
 function NameValuesToXML(aText: string): string;
+{$IFDEF MSWINDOWS}
 procedure LoadResourceFile(aFile: string; ms: TMemoryStream);
+{$ENDIF}
 procedure DirFiles(aDir, amask: string; aFileList: TStringlist);
 procedure RecurseDirFiles(myDir: string; var aFileList: TStringlist);
 procedure RecurseDirProgs(myDir: string; var aFileList: TStringlist);
@@ -944,6 +949,7 @@ begin
   FindClose(sr);
 end;
 
+{$IFDEF MSWINDOWS}
 procedure LoadResourceFile(aFile: string; ms: TMemoryStream);
 var
   HResInfo: HRSRC;
@@ -967,6 +973,7 @@ begin
   UnlockResource(HGlobal);
   FreeResource(HGlobal);
 end;
+{$ENDIF}
 
 procedure GetNames(aText: string; aList: TStringList);
 var
@@ -1306,11 +1313,12 @@ var
   i: integer;
   InBuf: array[0..3] of byte;
   OutBuf: array[0..2] of byte;
+  RetValue: string;
 begin
   if ((Length(S) mod 4) <> 0) or (S = '') then
     raise Exception.Create('Base64: Incorrect string format');
 
-  SetLength(Result, ((Length(S) div 4) - 1) * 3);
+  SetLength(RetValue, ((Length(S) div 4) - 1) * 3);
   for i := 1 to ((Length(S) div 4) - 1) do
   begin
     Move(S[(i - 1) * 4 + 1], InBuf, 4);
@@ -1357,7 +1365,7 @@ begin
     OutBuf[0] := (InBuf[0] shl 2) or ((InBuf[1] shr 4) and $03);
     OutBuf[1] := (InBuf[1] shl 4) or ((InBuf[2] shr 2) and $0F);
     OutBuf[2] := (InBuf[2] shl 6) or (InBuf[3] and $3F);
-    Move(OutBuf, Result[(i - 1) * 3 + 1], 3);
+    Move(OutBuf, RetValue[(i - 1) * 3 + 1], 3);
   end;
   if S <> '' then
   begin
@@ -1385,7 +1393,7 @@ begin
       else
         InBuf[1] := 63;
       OutBuf[0] := (InBuf[0] shl 2) or ((InBuf[1] shr 4) and $03);
-      Result := Result + char(OutBuf[0]);
+      RetValue := RetValue + char(OutBuf[0]);
     end
     else if InBuf[3] = 61 then
     begin
@@ -1421,7 +1429,7 @@ begin
         InBuf[2] := 63;
       OutBuf[0] := (InBuf[0] shl 2) or ((InBuf[1] shr 4) and $03);
       OutBuf[1] := (InBuf[1] shl 4) or ((InBuf[2] shr 2) and $0F);
-      Result := Result + char(OutBuf[0]) + char(OutBuf[1]);
+      RetValue := RetValue + char(OutBuf[0]) + char(OutBuf[1]);
     end
     else
     begin
@@ -1468,11 +1476,10 @@ begin
       OutBuf[0] := (InBuf[0] shl 2) or ((InBuf[1] shr 4) and $03);
       OutBuf[1] := (InBuf[1] shl 4) or ((InBuf[2] shr 2) and $0F);
       OutBuf[2] := (InBuf[2] shl 6) or (InBuf[3] and $3F);
-      Result := Result + Char(OutBuf[0]) + Char(OutBuf[1]) + Char(OutBuf[2]);
+      RetValue := RetValue + Char(OutBuf[0]) + Char(OutBuf[1]) + Char(OutBuf[2]);
     end;
-  end
-  else
-    Result := '';
+  end;
+  Result := RetValue;
 end;
 
 {*******************************************************
