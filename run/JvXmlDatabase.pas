5dfa4de50bcd110046baa825e7ae1b6aa0b6826b
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-09-25 10:13:40 +0000

    . Added MPL header

--- run/JvXmlDatabase.pas
+++ run/JvXmlDatabase.pas
@@ -1,11 +1,32 @@
-{$I JVCL.INC}
-{
-  Known limitations:
-    -You have to place the columns you test in the where clause in the select clause too
-    -Where conditions *MUST* be enclosed between parenthesis as ... WHERE (Col = 5) AND (Col2 < Col3) ...
-    -Update statements are limited to simple operations like ... SET Col1 = Col1 + 1, Col2 = 4 ...
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/MPL-1.1.html
 
-}
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: JvXMLDatabase.PAS, released on 2003-06-22.
+
+The Initial Developer of the Original Code is Sébastien Buysse [sbuysse@buypin.com]
+Portions created by Sébastien Buysse are Copyright (C) 2003 Sébastien Buysse.
+All Rights Reserved.
+
+Contributor(s):
+
+Last Modified: 2003-06-22
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
+located at http://jvcl.sourceforge.net
+
+Known Issues:
+  -You have to place the columns you test in the where clause in the select clause too
+  -Where conditions *MUST* be enclosed between parenthesis as ... WHERE (Col = 5) AND (Col2 < Col3) ...
+  -Update statements are limited to simple operations like ... SET Col1 = Col1 + 1, Col2 = 4 ...
+-----------------------------------------------------------------------------}
+{$I JVCL.INC}
 
 unit JvXMLDatabase;
 
