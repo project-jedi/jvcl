dae96b91653a818856c3d50b106d3ccd649b67d6
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-09-27 09:04:15 +0000

    New packages for HMI components

---
+++ jvcl/packages/bcb6/JvHMIC6R.cpp
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------------
+
+#include <basepch.h>
+#pragma hdrstop
+USEFORMNS("..\..\run\JvSLDMappingEditorDialog.pas", Jvsldmappingeditordialog, frmSLDMappingEditorDialog);
+USEFORMNS("..\..\run\JvSegmentedLEDDisplayMapperFrame.pas", Jvsegmentedleddisplaymapperframe, fmeJvSegmentedLEDDisplayMapper); /* TFrame: File Type */
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+//---------------------------------------------------------------------------
+
+//   Package source.
+//---------------------------------------------------------------------------
+
+#pragma argsused
+int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void*)
+{
+        return 1;
+}
+//---------------------------------------------------------------------------
