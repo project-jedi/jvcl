e17331df1089de32664d9bcc24864b23b4b3cda3
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-09-29 16:56:41 +0000

    Updated for JVCL 3

--- jvcl/devtools/bin/MakeDofs.bat
+++ jvcl/devtools/bin/MakeDofs.bat
@@ -1,4 +1,4 @@
-MakeDOF "..\..\packages\*50.dpk" d5packsdof.tmpl
-MakeDOF "..\..\packages\*60.dpk"  d6packsdof.tmpl
-MakeDOF "..\..\packages\*70.dpk"  d7packsdof.tmpl
-MakeDOF "..\..\examples\*.dpr"   examplesdof.tmpl
+MakeDOF "..\..\packages\d5\*.dpk" d5packsdof.tmpl
+MakeDOF "..\..\packages\d6\*.dpk" d6packsdof.tmpl
+MakeDOF "..\..\packages\d7\*.dpk" d7packsdof.tmpl
+rem MakeDOF "..\..\examples\*.dpr" examplesdof.tmpl
