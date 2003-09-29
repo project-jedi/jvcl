db33b068cc2586f1256396f7317716e3da06ac8f
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-09-29 19:20:43 +0000

    Initial release

---
+++ jvcl/packages/MakeD5.bat
@@ -0,0 +1,32 @@
+@echo off
+
+SET PACKAGE=D5 Packages
+SET DIR=D5
+
+REM ------- Generic --------
+
+cd ..\devtools
+if NOT EXIST bin\MakeDOF.exe  make MakeDOF.exe
+if NOT EXIST bin\MakeCFG.exe  make MakeCFG.exe
+if NOT EXIST bin\Bpg2Make.exe  make Bpg2Make.exe
+cd bin
+
+REM echo.
+REM echo Creating .dof files
+REM call makedofs.bat
+
+echo.
+echo Createing .cfg files
+call makecfgs.bat
+
+echo.
+cd ..\..\packages
+
+..\devtools\bin\Bpg2Make.exe "%PACKAGE%.bpg"
+
+make -f "%PACKAGE%.mak"
+
+echo.
+echo Cleaning
+del "%PACKAGE%.mak"
+del %DIR%\*.cfg
