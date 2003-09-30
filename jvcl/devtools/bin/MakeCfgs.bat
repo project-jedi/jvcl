f6dde92c8e5d7871b9156a2b4e2175b09acf4c21
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-09-30 22:17:06 +0000

    Added Delphi 6 PE support

--- jvcl/devtools/bin/MakeCfgs.bat
+++ jvcl/devtools/bin/MakeCfgs.bat
@@ -1,4 +1,5 @@
 MakeCFG "..\..\packages\d5\*.dpk" d5packscfg.tmpl
 MakeCFG "..\..\packages\d6\*.dpk" d6packscfg.tmpl
+MakeCFG "..\..\packages\d6per\*.dpk" d6packscfg.tmpl
 MakeCFG "..\..\packages\d7\*.dpk" d7packscfg.tmpl
 rem MakeCFG "..\..\examples\*.dpr" examplescfg.tmpl
