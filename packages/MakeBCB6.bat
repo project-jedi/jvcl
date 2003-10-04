092b4a8b5708ef7de08e162b586eeb9f2036d19c
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-10-04 09:22:54 +0000

    Doesn't work yet because of linking issues

---
+++ packages/MakeBCB6.bat
@@ -0,0 +1,44 @@
+@echo off
+
+rem NB! Change this path to reflect the location of make.exe:
+SET ROOT=C:\Prog\CBuilder6
+SET MAKE=%ROOT%\bin\make.exe
+
+
+
+
+SET PACKAGE=BCB6 Packages
+SET DIR=BCB6
+
+REM ------- Generic --------
+
+if NOT EXIST "%MAKE%" SET MAKE=make
+
+cd ..\devtools
+if NOT EXIST bin\MakeDOF.exe  %MAKE% MakeDOF.exe
+if NOT EXIST bin\MakeCFG.exe  %MAKE% MakeCFG.exe
+if NOT EXIST bin\Bpg2Make.exe %MAKE% Bpg2Make.exe
+cd bin
+
+REM echo.
+REM echo Creating .dof files
+REM call makedofs.bat
+
+rem echo.
+rem echo Creating .cfg files
+rem MakeCFG "..\..\packages\%DIR%\*.bpk" %DIR%packscfg.tmpl
+
+
+echo.
+cd ..\..\packages
+
+..\devtools\bin\Bpg2Make.exe "%PACKAGE%.bpg"
+
+%MAKE% -f "%PACKAGE%.mak" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+
+
+echo.
+echo Cleaning
+rem del "%PACKAGE%.mak"
+rem del %DIR%\*.cfg
