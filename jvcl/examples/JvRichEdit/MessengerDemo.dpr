9bf6fb1ee5dd866c542795b159be541c4ab98bfa
remkobonte <remkobonte@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-10-16 21:52:07 +0000

    New TJvRichEdit demo (mostly pictures, insert formatted text)

---
+++ jvcl/examples/JvRichEdit/MessengerDemo.dpr
@@ -0,0 +1,13 @@
+program MessengerDemo;
+
+uses
+  Forms,
+  MessengerMainFormU in 'MessengerMainFormU.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
