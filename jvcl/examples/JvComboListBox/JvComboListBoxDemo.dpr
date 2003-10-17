7e0260f1f280c84ef0d0a4522aaa737368291cf0
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-10-17 13:07:23 +0000

    - some more funny stuff

--- jvcl/examples/JvComboListBox/JvComboListBoxDemo.dpr
+++ jvcl/examples/JvComboListBox/JvComboListBoxDemo.dpr
@@ -2,14 +2,14 @@ program JvComboListBoxDemo;
 
 uses
   Forms,
-  MainFrm in 'MainFrm.pas' {Form1},
+  MainFrm in 'MainFrm.pas' {frmMain},
   DropFrm in 'DropFrm.pas' {frmDrop};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TfrmMain, frmMain);
   Application.Run;
 end.
 
