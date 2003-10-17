41684f6dc032b11b61d232e0add355ad01f6755c
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-10-17 10:33:40 +0000

    faster StringToHtml/HtmlToString StringToHtml/HtmlToString had done the opposite of its names.

--- run/JvStrToHtml.pas
+++ run/JvStrToHtml.pas
@@ -15,8 +15,9 @@ Portions created by Sébastien Buysse are Copyright (C) 2001 Sébastien Buysse.
 All Rights Reserved.
 
 Contributor(s): Michael Beck [mbeck@bigfoot.com].
+                Andreas Hausladen [Andreas.Hausladen@gmx.de]
 
-Last Modified: 2000-02-28
+Last Modified: 2003-10-17
 
 You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
 located at http://jvcl.sourceforge.net
@@ -43,15 +44,15 @@ type
     procedure SetValue(const Value: string);
   public
     constructor Create(AOwner: TComponent); override;
-    function TextToHtml(Text: string): string;
-    function HtmlToText(Text: string): string;
+    function TextToHtml(const Text: string): string;
+    function HtmlToText(const Text: string): string;
   published
     property Text: string read FValue write SetValue;
     property Html: string read FHtml write SetHtml;
   end;
 
-function StringToHtml(Value: string): string;
-function HtmlToString(Value: string): string;
+function StringToHtml(const Value: string): string;
+function HtmlToString(const Value: string): string;
 function CharToHtml(Ch: Char): string;
 
 implementation
@@ -152,7 +153,7 @@ begin
   FHtml := '';
 end;
 
-function TJvStrToHtml.HtmlToText(Text: string): string;
+function TJvStrToHtml.HtmlToText(const Text: string): string;
 begin
   Result := HtmlToString(Text);
 end;
@@ -167,28 +168,103 @@ begin
   FHtml := TextToHtml(Value);
 end;
 
-function TJvStrToHtml.TextToHtml(Text: string): string;
+function TJvStrToHtml.TextToHtml(const Text: string): string;
 begin
   Result := StringToHtml(Text);
 end;
 
-function StringToHtml(Value: string): string;
+function StringToHtml(const Value: string): string;
 var
-  I: Integer;
+  I, J: Integer;
+  Len, AddLen, HtmlLen: Integer;
+  P: PChar;
+  Ch: Char;
 begin
-  Result := Value;
-  for I := Low(Conversions) to High(Conversions) do
-    Result := StringReplace(Result, Conversions[I].Html, Conversions[I].Ch,
-      [rfReplaceAll, rfIgnoreCase]);
+  Len := Length(Value);
+ // number of chars to add
+  AddLen := 0;
+  for I := 1 to Len do
+    for J := Low(Conversions) to High(Conversions) do
+      if Value[I] = Conversions[J].Ch then
+      begin
+        Inc(AddLen, StrLen(Conversions[J].Html) - 1);
+        Break;
+      end;
+
+  if AddLen = 0 then
+    Result := Value
+  else
+  begin
+    SetLength(Result, Len + AddLen);
+    P := Pointer(Result);
+    for I := 1 to Len do
+    begin
+      Ch := Value[I];
+      for J := Low(Conversions) to High(Conversions) do
+        if Ch = Conversions[J].Ch then
+        begin
+          HtmlLen := StrLen(Conversions[J].Html);
+          Move(Conversions[J].Html[0], P[0], HtmlLen); // Conversions[].Html is a PChar
+          Inc(P, HtmlLen);
+          Ch := #0;
+          Break;
+        end;
+      if Ch <> #0 then
+      begin
+        P[0] := Ch;
+        Inc(P);
+      end;
+    end;
+  end;
 end;
 
-function HtmlToString(Value: string): string;
+function HtmlToString(const Value: string): string;
 var
-  I: Integer;
+  I, Index, Len: Integer;
+  Start, J: Integer;
+  Ch: Char;
+  ReplStr: string;
 begin
-  Result := '';
-  for I := 1 to Length(Value) do
-    Result := Result + CharToHtml(Value[I]);
+  Len := Length(Value);
+  SetLength(Result, Len); // worst case
+  Index := 0;
+  I := 1;
+  while I <= Len do
+  begin
+    Ch := Value[I];
+   // html entitiy
+    if Ch = '&' then
+    begin
+      Start := I;
+      Inc(I);
+      while (I <= Len) and (Value[I] <> ';') and (I < Start + 20) do
+        Inc(I);
+      if Value[I] <> ';' then
+        I := Start
+      else
+      begin
+        Ch := #0;
+        ReplStr := LowerCase(Copy(Value, Start, I - Start + 1));
+        for J := Low(Conversions) to High(Conversions) do
+          if Conversions[J].Html = ReplStr then
+          begin
+            Ch := Conversions[J].Ch;
+            Break;
+          end;
+        if Ch = #0 then
+        begin
+          I := Start;
+          Ch := Value[I];
+        end;
+      end;
+    end;
+
+    Inc(I);
+    Inc(Index);
+    Result[Index] := Ch;
+  end;
+  if Index <> Len then
+    SetLength(Result, Index);
 end;
 
 function CharToHtml(Ch: Char): string;
