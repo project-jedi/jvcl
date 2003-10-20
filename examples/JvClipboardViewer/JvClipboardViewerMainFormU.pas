2cd98a79e06c1a08d732d5673b373f5e9512e4a3
ivazquez <ivazquez@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-10-20 20:11:23 +0000

    JvClipboardViewer example update

--- examples/JvClipboardViewer/JvClipboardViewerMainFormU.pas
+++ examples/JvClipboardViewer/JvClipboardViewerMainFormU.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
-  ExtCtrls, StdCtrls, JvClipView, JvComponent;
+  ExtCtrls, StdCtrls, JvComponent, JvClipboardViewer;
 
 type
   TJvClipboardViewerMainForm = class(TForm)
