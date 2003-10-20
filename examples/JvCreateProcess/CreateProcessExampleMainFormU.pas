41982d7b54dc61b44192657419aaaecddf86b09b
ivazquez <ivazquez@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-10-20 20:15:18 +0000

    JvCreateProcess example update

--- examples/JvCreateProcess/CreateProcessExampleMainFormU.pas
+++ examples/JvCreateProcess/CreateProcessExampleMainFormU.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
-  StdCtrls, ComCtrls, ExtCtrls, JvSysComp, JvComponent;
+  StdCtrls, ComCtrls, ExtCtrls, JvSysComp, JvComponent, JvCreateProcess;
 
 type
   TCreateProcessExampleMainForm = class(TForm)
