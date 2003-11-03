ef3e0d23641d26d2e7769d5ef526cf888c8fb23a
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-11-03 08:35:37 +0000

    Added AutoArrange example

--- examples/JvPanel/JvPanelDemoFrm.pas
+++ examples/JvPanel/JvPanelDemoFrm.pas
@@ -16,7 +16,25 @@ type
     JvFilenameEdit1: TJvFilenameEdit;
     Label1: TLabel;
     JvFormStorage1: TJvFormStorage;
+    CheckBox2: TCheckBox;
+    Panel1: TPanel;
+    Label2: TLabel;
+    LeftMaskEdit: TMaskEdit;
+    Panel2: TPanel;
+    Label3: TLabel;
+    TopMaskEdit: TMaskEdit;
+    Panel3: TPanel;
+    Label4: TLabel;
+    HorizontalMaskEdit: TMaskEdit;
+    Panel4: TPanel;
+    Label5: TLabel;
+    VerticalMaskEdit: TMaskEdit;
     procedure CheckBox1Click(Sender: TObject);
+    procedure CheckBox2Click(Sender: TObject);
+    procedure LeftMaskEditChange(Sender: TObject);
+    procedure TopMaskEditChange(Sender: TObject);
+    procedure HorizontalMaskEditChange(Sender: TObject);
+    procedure VerticalMaskEditChange(Sender: TObject);
   private
     { Private declarations }
   public
@@ -37,6 +55,31 @@ begin
     Edit1.Top := 8
   else
     Edit1.Top := JvPanel1.Height - 35;
+end;
+
+procedure TForm1.CheckBox2Click(Sender: TObject);
+begin
+  JvPanel1.ArrangeSettings.AutoArrange := CheckBox2.Checked;
+end;
+
+procedure TForm1.LeftMaskEditChange(Sender: TObject);
+begin
+  JvPanel1.ArrangeSettings.BorderLeft := strtoint(LeftMaskEdit.Text);
+end;
+
+procedure TForm1.TopMaskEditChange(Sender: TObject);
+begin
+  JvPanel1.ArrangeSettings.BorderTop:= strtoint(TopMaskEdit.Text);
+end;
+
+procedure TForm1.HorizontalMaskEditChange(Sender: TObject);
+begin
+  JvPanel1.ArrangeSettings.DistanceHorizontal := strtoint(HorizontalMaskEdit.Text);
+end;
+
+procedure TForm1.VerticalMaskEditChange(Sender: TObject);
+begin
+  JvPanel1.ArrangeSettings.DistanceVertical := strtoint(VerticalMaskEdit.Text);
 end;
 
 end.
