efaa028c7a8077e0bc4ac38fc5c4f735f220bbd5
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-11-13 12:55:19 +0000

    splitted in command line and GUI edition. Should be ready to use in production

---
+++ devtools/PackagesGenerator/pgEdit.dpr
@@ -0,0 +1,24 @@
+program pgEdit;
+
+uses
+  Forms,
+  SysUtils,
+  MainForm in 'MainForm.pas' {frmMain},
+  FileUtils in 'FileUtils.pas',
+  TargetDialog in 'TargetDialog.pas' {frmTargets},
+  GenerateUtils in 'GenerateUtils.pas',
+  KnownTagsForm in 'KnownTagsForm.pas' {frmKnownTags},
+  FormTypeDialog in 'FormTypeDialog.pas' {frmFormType}
+  ;
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.CreateForm(TfrmTargets, frmTargets);
+  Application.CreateForm(TfrmKnownTags, frmKnownTags);
+  Application.CreateForm(TfrmFormType, frmFormType);
+  Application.Run;
+end.
