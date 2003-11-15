b6f6d8f24174acd24ad07b76767793e096d14800
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-11-15 10:06:55 +0000

    First versions of the creation templates.

---
+++ jvcl/packages/bcb6/template.cpp
@@ -0,0 +1,48 @@
+//---------------------------------------------------------------------------
+/*
+-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/MPL-1.1.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: %NAME%.bpk, released on 2003-11-08.
+
+The Initial Developer of the Original Code is Olivier Sannier [obones@meloo.com]
+Portions created by Olivier Sannier are Copyright (C) 2003 Olivier Sannier.
+All Rights Reserved.
+
+Contributor(s): -
+
+Last Modified: 2003-11-08
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
+located at http://jvcl.sourceforge.net
+
+Known Issues:
+-----------------------------------------------------------------------------
+*/
+
+#include <basepch.h>
+#pragma hdrstop
+<%%% START FORMS %%%>
+USEFORMNS("%FILENAME%", %Unitname%, %FORMNAME%);
+<%%% END FORMS %%%>
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+//---------------------------------------------------------------------------
+
+//   Package source.
+//---------------------------------------------------------------------------
+
+#pragma argsused
+int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void*)
+{
+        return 1;
+}
+//---------------------------------------------------------------------------
+
