471a8d9d44cccc40bd1af6acbf4b1f67cd8aeacf
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-12-11 17:31:45 +0000

    clean dcu\*.obj

--- jvcl/clean.bat
+++ jvcl/clean.bat
@@ -1,6 +1,7 @@
 @echo off
 echo erasing...
 del /S *.dcu *.ddp *.dsk *.~* *.cfg *.drc %1 %2 %3 %4 %5 %6 %7 %8 %9 >NUL
+del dcu\*.obj >NUL
 if NOT "%1" == "all" goto LEAVE
 del lib5\*.dcp lib5\*.bpl >NUL
 del lib6\*.dcp lib6\*.bpl >NUL
