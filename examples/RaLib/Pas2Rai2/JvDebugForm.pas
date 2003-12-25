439b8c9f3c6bf0d16d1d4651a1b7882008e758e8
marquardt <marquardt@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-12-25 15:12:56 +0000

    made compiling again  needs more work

--- examples/RaLib/Pas2Rai2/JvDebugForm.pas
+++ examples/RaLib/Pas2Rai2/JvDebugForm.pas
@@ -4,17 +4,14 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
-  StdCtrls, JvRegAuto, ExtCtrls, JvEditor, JvHLEditor;
+  StdCtrls, ExtCtrls, JvEditor, JvHLEditor;
 
 type
   TJvDebugLog  = class(TForm)
     memDebug: TJvHLEditor;
     Panel1: TPanel;
     cbDebug: TCheckBox;
-  private
-    { Private declarations }
   public
-    { Public declarations }
   end;
 
 var
