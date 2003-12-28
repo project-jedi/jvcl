b479dfdf635973a5aa2c8f21032b50956d4e0439
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2003-12-28 13:09:35 +0000

    missing JvFunctions

--- jvcl/devtools/JConvert/JConvertUtils.pas
+++ jvcl/devtools/JConvert/JConvertUtils.pas
@@ -6,7 +6,7 @@ procedure Run;
 
 implementation
 uses
-  Windows, Classes, SysUtils, JvSearchFiles, JvFunctions, JTools;
+  Windows, Classes, SysUtils, JvSearchFiles, JvJVCLUtils, JTools;
 
 procedure ShowHelp;
 begin
