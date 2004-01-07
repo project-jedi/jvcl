da359fdfcba4334d37ef893df3bfd3e80c48814f
marquardt <marquardt@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-01-07 07:59:52 +0000

    style cleaning

--- jvcl/examples/JvChartDemo/JvChartDemoFm.pas
+++ jvcl/examples/JvChartDemo/JvChartDemoFm.pas
@@ -1,12 +1,32 @@
-{******************************************************************}
-{  Chart Demo program for JvChart (formerly AABsoftGraph)          }
-{                                                                  }
-{  BAR/LINE Charting Component for JEDI                            }
-{                                                                  }
-{  Original                                                        }
-{  (C) 1996 AABsoft and Mårten Henrichson                          }
-{  Modified 2003 Warren Postma                                     }
-{******************************************************************}
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/MPL-1.1.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: JvChartDemoFm.Pas, released on 2002-10-04.
+
+The Initial Developer of the Original Code is AABsoft and Mårten Henrichson.
+(C) 1996 AABsoft and Mårten Henrichson.
+All Rights Reserved.
+
+Contributor(s): -
+
+Last Modified: 2004-01-07
+  Modified 2003 Warren Postma
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
+located at http://jvcl.sourceforge.net
+
+Description:
+  BAR/LINE Charting Component for JEDI
+
+Known Issues:
+-----------------------------------------------------------------------------}
 
 unit JvChartDemoFm;
 
