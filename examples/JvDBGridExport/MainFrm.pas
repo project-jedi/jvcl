638ca3a0e311d63bccbbf0e75aff05b2f8c22ba7
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-01-16 00:51:26 +0000

    - Minor changes - D5 compatibility

--- examples/JvDBGridExport/MainFrm.pas
+++ examples/JvDBGridExport/MainFrm.pas
@@ -5,7 +5,7 @@ interface
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
   Menus, Db, Grids, DBGrids, ComCtrls, 
-  JvComponent, JvDBGridExport, JvCsvData,
+  JvComponent, JvDBGridExport, JvCsvData, JvXPBar,
   JvBaseDlg, JvProgressDialog, JvDBGrid;
 
 type
