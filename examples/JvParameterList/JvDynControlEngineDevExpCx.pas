0166ae8856e4ede35511913975d7d794d5ba2b2a
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-01-21 11:32:59 +0000

    - comment updated

--- examples/JvParameterList/JvDynControlEngineDevExpCx.pas
+++ examples/JvParameterList/JvDynControlEngineDevExpCx.pas
@@ -8,14 +8,12 @@ Software distributed under the License is distributed on an "AS IS" basis,
 WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
 the specific language governing rights and limitations under the License.
 
-The Original Code is: JvPlacemnt.PAS, released on 2002-07-04.
+The Original Code is: JvDynControlEngineDevExpCx.PAS, released on 2003-12-28.
 
-The Initial Developers of the Original Code are: Fedor Koshevnikov, Igor Pavluk and Serge Korolev
-Copyright (c) 1997, 1998 Fedor Koshevnikov, Igor Pavluk and Serge Korolev
-Copyright (c) 2001,2002 SGB Software
-All Rights Reserved.
+The Initial Developers of the Original Code is Jens Fudickar
+Copyright (c) 2001,2002 Jens Fudickar. All Rights Reserved.
 
-Last Modified: 2002-07-04
+Last Modified: 2003-12-28
 
 You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
 located at http://jvcl.sourceforge.net
