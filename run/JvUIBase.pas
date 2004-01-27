af67548091c0e1fd3545bfbab3aeaade25d52912
progdigy <progdigy@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-01-27 23:32:41 +0000

    no message

--- run/JvUIBase.pas
+++ run/JvUIBase.pas
@@ -4,14 +4,12 @@
 { Project JEDI Code Library (JCL)                                              }
 {                                                                              }
 { The contents of this file are subject to the Mozilla Public License Version  }
-{ 1.0 (the "License"); you may not use this file except in compliance with the }
+{ 1.1 (the "License"); you may not use this file except in compliance with the }
 { License. You may obtain a copy of the License at http://www.mozilla.org/MPL/ }
 {                                                                              }
 { Software distributed under the License is distributed on an "AS IS" basis,   }
 { WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for }
 { the specific language governing rights and limitations under the License.    }
-{                                                                              }
-{ The Original Code is JvUIBase.pas.                                           }
 {                                                                              }
 { The Initial Developer of the Original Code is documented in the accompanying }
 { help file JCL.chm. Portions created by these individuals are Copyright (C)   }
@@ -37,12 +35,11 @@
 
 {$IFDEF USE_IBASE_H}
 (*$HPPEMIT '#include<ibase.h>'*)
-{$ENDIF USE_IBASE_H}
+{$ENDIF}
 
 unit JvUIBase;
 
 interface
-
 uses
   {$IFDEF MSWINDOWS}
   Windows,
