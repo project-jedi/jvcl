74f0cb3acf4aeb05826db757046124c9099711ab
marquardt <marquardt@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-01-29 07:46:27 +0000

    installer improvement, JvAviCapDemo minor change

--- examples/CompileExamples.bat
+++ examples/CompileExamples.bat
@@ -1,11 +1,12 @@
 :start
 @echo off
 cls
-echo. Change the JVCLMAKE line in this batch file to use the make program of your choice 
-echo. or from a specific Delphi/BCB version (f ex C:\Program Files\Borland\Delphi5\bin\make.exe 
+echo. Change the JVCLMAKE line in this batch file to use the make program of
+echo. your choice or from a specific Delphi/BCB version (for example
+echo. C:\Program Files\Borland\Delphi5\bin\make.exe
 echo. to use the Delphi 5 make).
-echo. Defaults to: make.exe (whatever that is on your system), always build
+echo. Defaults to: make.exe (whatever that is on your system), always build.
 pause
 SET JVCLMAKE=make.exe -B -f
-echo. Making all examples (get some coffeee, this takes a while!):
+echo. Making all examples (get some coffee, this takes a while!):
 %JVCLMAKE% makefile.mak
