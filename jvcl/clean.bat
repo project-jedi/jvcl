735a0b51380eee27fe677716a48af396d378c183
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-02-01 23:53:28 +0000

    Added lines for C5 and C6

--- jvcl/clean.bat
+++ jvcl/clean.bat
@@ -6,5 +6,7 @@ if NOT "%1" == "all" goto LEAVE
 del lib\d5\*.dcp lib\d5\*.bpl >NUL
 del lib\d6\*.dcp lib\d6\*.bpl >NUL
 del lib\d7\*.dcp lib\d7\*.bpl >NUL
-# (rom) needs delete lines for c5, c6 and k3
+del lib\c5\obj\*.obj lib\c5\*.dcu >NUL
+del lib\c6\obj\*.obj lib\c6\*.dcu >NUL
+# (obones) needs delete lines for k3
 :LEAVE
