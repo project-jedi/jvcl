bf8a6b4b8c25d1ca66faf84b2cc2485141f923aa
marquardt <marquardt@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-02-01 09:51:22 +0000

    structure change of lib directories

--- clean.bat
+++ clean.bat
@@ -3,7 +3,8 @@ echo erasing...
 del /S *.dcu *.ddp *.dsk *.~* *.cfg *.drc %1 %2 %3 %4 %5 %6 %7 %8 %9 >NUL
 del dcu\*.obj >NUL
 if NOT "%1" == "all" goto LEAVE
-del lib5\*.dcp lib5\*.bpl >NUL
-del lib6\*.dcp lib6\*.bpl >NUL
-del lib7\*.dcp lib7\*.bpl >NUL
+del lib\d5\*.dcp lib\d5\*.bpl >NUL
+del lib\d6\*.dcp lib\d6\*.bpl >NUL
+del lib\d7\*.dcp lib\d7\*.bpl >NUL
+# (rom) needs delete lines for c5, c6 and k3
 :LEAVE
