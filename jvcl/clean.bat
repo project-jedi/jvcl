38c62830a66dfa6358f30f0792dead3b353511d8
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-02-04 00:35:07 +0000

    Corrected wrong comment and wrong delete commands

--- jvcl/clean.bat
+++ jvcl/clean.bat
@@ -6,7 +6,7 @@ if NOT "%1" == "all" goto LEAVE
 del lib\d5\*.dcp lib\d5\*.bpl >NUL
 del lib\d6\*.dcp lib\d6\*.bpl >NUL
 del lib\d7\*.dcp lib\d7\*.bpl >NUL
-del lib\c5\obj\*.obj lib\c5\*.dcu >NUL
-del lib\c6\obj\*.obj lib\c6\*.dcu >NUL
-# (obones) needs delete lines for k3
+del lib\c5\obj\*.obj lib\c5\obj\*.dcu >NUL
+del lib\c6\obj\*.obj lib\c6\obj\*.dcu >NUL
+: needs delete lines for k3
 :LEAVE
