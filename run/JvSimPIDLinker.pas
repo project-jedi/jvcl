710f5b54063299e67b40c9f0a6392775fcdc2465
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-02-06 18:27:16 +0000

    - missing Notification calls added - JvSimPIDLinker: reworked implementation of multiple similar properties

--- run/JvSimPIDLinker.pas
+++ run/JvSimPIDLinker.pas
@@ -37,63 +37,84 @@ uses
 type
   TJvSimPIDLinker = class(TComponent)
   private
-    FIn1: TJvSimPID;
-    FOut1: TJvSimPID;
-    FIn2: TJvSimPID;
-    FOut2: TJvSimPID;
-    FIn3: TJvSimPID;
-    FOut3: TJvSimPID;
-    FIn4: TJvSimPID;
-    FOut4: TJvSimPID;
-    FIn5: TJvSimPID;
-    FOut5: TJvSimPID;
-    FIn6: TJvSimPID;
-    FOut6: TJvSimPID;
-    FIn7: TJvSimPID;
-    FOut7: TJvSimPID;
-    FIn8: TJvSimPID;
-    FOut8: TJvSimPID;
+    FPIDS: array of TJvSimPID;
+    function GetPID(const Index: Integer): TJvSimPID;
+    procedure SetPID(const Index: Integer; const Value: TJvSimPID);
+  protected
+    procedure Notification(AComponent: TComponent; Operation: TOperation);
+      override;
+    procedure InitPids;
   public
     procedure Execute;
+    constructor Create(AOwner: TComponent); override;
   published
-    property In1: TJvSimPID read FIn1 write FIn1;
-    property Out1: TJvSimPID read FOut1 write FOut1;
-    property In2: TJvSimPID read FIn2 write FIn2;
-    property Out2: TJvSimPID read FOut2 write FOut2;
-    property In3: TJvSimPID read FIn3 write FIn3;
-    property Out3: TJvSimPID read FOut3 write FOut3;
-    property In4: TJvSimPID read FIn4 write FIn4;
-    property Out4: TJvSimPID read FOut4 write FOut4;
-    property In5: TJvSimPID read FIn5 write FIn5;
-    property Out5: TJvSimPID read FOut5 write FOut5;
-    property In6: TJvSimPID read FIn6 write FIn6;
-    property Out6: TJvSimPID read FOut6 write FOut6;
-    property In7: TJvSimPID read FIn7 write FIn7;
-    property Out7: TJvSimPID read FOut7 write FOut7;
-    property In8: TJvSimPID read FIn8 write FIn8;
-    property Out8: TJvSimPID read FOut8 write FOut8;
+    property In1: TJvSimPID index 0 read GetPID write SetPID;
+    property Out1: TJvSimPID index 1 read GetPID write SetPID;
+    property In2: TJvSimPID index 2 read GetPID write SetPID;
+    property Out2: TJvSimPID index 3 read GetPID write SetPID;
+    property In3: TJvSimPID index 4 read GetPID write SetPID;
+    property Out3: TJvSimPID index 5 read GetPID write SetPID;
+    property In4: TJvSimPID index 6 read GetPID write SetPID;
+    property Out4: TJvSimPID index 7 read GetPID write SetPID;
+    property In5: TJvSimPID index 8 read GetPID write SetPID;
+    property Out5: TJvSimPID index 9 read GetPID write SetPID;
+    property In6: TJvSimPID index 10 read GetPID write SetPID;
+    property Out6: TJvSimPID index 11 read GetPID write SetPID;
+    property In7: TJvSimPID index 12 read GetPID write SetPID;
+    property Out7: TJvSimPID index 13 read GetPID write SetPID;
+    property In8: TJvSimPID index 14 read GetPID write SetPID;
+    property Out8: TJvSimPID index 15 read GetPID write SetPID;
   end;
 
 implementation
 
-procedure TJvSimPIDLinker.Execute;
+constructor TJvSimPIDLinker.Create(AOwner: TComponent);
 begin
-  if Assigned(FIn1) and Assigned(FOut1) then
-    FOut1.MV := FIn1.CV;
-  if Assigned(FIn2) and Assigned(FOut2) then
-    FOut2.MV := FIn2.CV;
-  if Assigned(FIn3) and Assigned(FOut3) then
-    FOut3.MV := FIn3.CV;
-  if Assigned(FIn4) and Assigned(FOut4) then
-    FOut4.MV := FIn4.CV;
-  if Assigned(FIn5) and Assigned(FOut5) then
-    FOut5.MV := FIn5.CV;
-  if Assigned(FIn6) and Assigned(FOut6) then
-    FOut6.MV := FIn6.CV;
-  if Assigned(FIn7) and Assigned(FOut7) then
-    FOut7.MV := FIn7.CV;
-  if Assigned(FIn8) and Assigned(FOut8) then
-    FOut8.MV := FIn8.CV;
+  inherited;
+  InitPids;
+end;
+
+procedure TJvSimPIDLinker.Execute;
+var
+  i: integer;
+begin
+  for i := 0 to Length(FPIDS) - 2 do
+    if (FPIDS[i] <> nil) and (FPIDS[i + 1] <> nil) then
+      FPIDS[i].MV := FPIDS[i + 1].CV;
+end;
+
+function TJvSimPIDLinker.GetPID(const Index: Integer): TJvSimPID;
+begin
+  Result := FPIDS[Index];
+end;
+
+procedure TJvSimPIDLinker.InitPids;
+const
+  cCount = 16;
+var
+  i: integer;
+begin
+  SetLength(FPIDS, cCount);
+  for i := 0 to cCount - 1 do
+    FPIDS[i] := nil;
+end;
+
+procedure TJvSimPIDLinker.Notification(AComponent: TComponent;
+  Operation: TOperation);
+var
+  i: integer;
+begin
+  inherited Notification(AComponent, Operation);
+  if Operation = opRemove then
+    for i := 0 to Length(FPIDS) - 1 do
+      if FPIDS[i] = AComponent then
+        FPIDS[i] := nil;
+end;
+
+procedure TJvSimPIDLinker.SetPID(const Index: Integer;
+  const Value: TJvSimPID);
+begin
+  FPIDS[Index] := Value;
 end;
 
 end.
