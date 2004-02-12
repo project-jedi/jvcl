0b0ee94eee52529d4b8b1b40f8bd39c39916a10f
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-02-12 11:37:20 +0000

    - fixed params

--- jvcl/devtools/bin/pg2want.bat
+++ jvcl/devtools/bin/pg2want.bat
@@ -1,6 +1,6 @@
 if NOT EXIST pg2want.exe goto make
 :build
-pg2want.exe ..\..\packages\xml\*.xml separate.xml fixed.xml ..\..\ ./jvcl/**/ >pg2want.log
+pg2want.exe ..\..\packages\xml\*.xml separate.xml fixed.xml ./jvcl/ >pg2want.log
 goto end
 :make
 cd..
