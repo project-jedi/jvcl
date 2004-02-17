7d54f2896383747e435e32cbb65894e4f6173224
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-02-17 12:12:20 +0000

    New demo for BCB

---
+++ examples/JvCaptionButton/BCB/MainForm.h
@@ -0,0 +1,84 @@
+//---------------------------------------------------------------------------
+
+#ifndef MainFormH
+#define MainFormH
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include "JvCaptionButton.hpp"
+#include "JvComponent.hpp"
+#include <ComCtrls.hpp>
+#include <ExtCtrls.hpp>
+#include <ImgList.hpp>
+//---------------------------------------------------------------------------
+class TfrmMain : public TForm
+{
+__published:	// IDE-managed Components
+  TUpDown *udPosition;
+  TTrackBar *tbBtnWidth;
+  TMemo *meEvents;
+  TLabel *lblPos;
+  TLabel *Label7;
+  TLabel *Label6;
+  TLabel *Label5;
+  TLabel *Label4;
+  TLabel *Label3;
+  TLabel *Label2;
+  TLabel *Label1;
+  TJvCaptionButton *JvCaptionButton1;
+  TImageList *ImageList1;
+  TGroupBox *gbButtons;
+  TCheckBox *chkSysMenu;
+  TCheckBox *chkMax;
+  TCheckBox *chkMin;
+  TCheckBox *chkHelp;
+  TEdit *edHint;
+  TEdit *edCaption;
+  TCheckBox *chkVisible;
+  TCheckBox *chkToggle;
+  TCheckBox *chkShowImage;
+  TCheckBox *chkShowHints;
+  TCheckBox *chkLogEvents;
+  TCheckBox *chkEnabled;
+  TCheckBox *chkDown;
+  TComboBox *cbStandard;
+  TComboBox *cbBorderStyle;
+  TButton *btnDelete;
+  TButton *btnAdd;
+  TBevel *Bevel1;
+  void __fastcall JvCaptionButton1Click(TObject *Sender);
+  void __fastcall JvCaptionButton1MouseDown(TObject *Sender,
+          TMouseButton Button, TShiftState Shift, int X, int Y);
+  void __fastcall JvCaptionButton1MouseUp(TObject *Sender,
+          TMouseButton Button, TShiftState Shift, int X, int Y);
+  void __fastcall JvCaptionButton1MouseMove(TObject *Sender,
+          TShiftState Shift, int X, int Y);
+  void __fastcall chkVisibleClick(TObject *Sender);
+  void __fastcall chkToggleClick(TObject *Sender);
+  void __fastcall cbStandardChange(TObject *Sender);
+  void __fastcall FormCreate(TObject *Sender);
+  void __fastcall udPositionClick(TObject *Sender, TUDBtnType Button);
+  void __fastcall chkDownClick(TObject *Sender);
+  void __fastcall edCaptionChange(TObject *Sender);
+  void __fastcall chkSysMenuClick(TObject *Sender);
+  void __fastcall chkMaxClick(TObject *Sender);
+  void __fastcall chkMinClick(TObject *Sender);
+  void __fastcall chkHelpClick(TObject *Sender);
+  void __fastcall cbBorderStyleChange(TObject *Sender);
+  void __fastcall chkShowHintsClick(TObject *Sender);
+  void __fastcall edHintClick(TObject *Sender);
+  void __fastcall chkEnabledClick(TObject *Sender);
+  void __fastcall tbBtnWidthChange(TObject *Sender);
+  void __fastcall btnAddClick(TObject *Sender);
+  void __fastcall btnDeleteClick(TObject *Sender);
+  void __fastcall chkShowImageClick(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+  __fastcall TfrmMain(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TfrmMain *frmMain;
+//---------------------------------------------------------------------------
+#endif
