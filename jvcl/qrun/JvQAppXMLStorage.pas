036a0eaf5e94e7c2ef3c307b7a471bd7b7ab87c1
asnepvangers <asnepvangers@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-02-21 20:13:38 +0000

    VisualCLX units Note: generated files but some are modified Currently not in CVS because of codefreeze

---
+++ jvcl/qrun/JvQAppXMLStorage.pas
@@ -0,0 +1,610 @@
+{**************************************************************************************************}
+{  WARNING:  JEDI preprocessor generated unit. Manual modifications will be lost on next release.  }
+{**************************************************************************************************}
+
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/MPL-1.1.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: JvAppXMLStorage.pas, released on 2003-12-06.
+
+The Initial Developer of the Original Code is Olivier Sannier
+Portions created by Olivier Sannier are Copyright (C) 2003 Olivier Sannier
+All Rights Reserved.
+
+Contributor(s):
+  Marcel Bestebroer
+
+Last Modified: 2004-01-18
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
+located at http://jvcl.sourceforge.net
+
+Known Issues:
+-----------------------------------------------------------------------------}
+
+{$I jvcl.inc}
+
+unit JvQAppXMLStorage;
+
+interface
+
+uses
+  {$IFDEF MSWINDOWS}
+  Windows,
+  {$ENDIF MSWINDOWS}
+  {$IFDEF LINUX}
+  Libc,
+  {$ENDIF LINUX}
+  SysUtils, Classes, IniFiles,
+  JvQAppStorage, JvQSimpleXml;
+
+type
+  // This is the base class for an in memory XML file storage
+  // There is at the moment only one derived class that simply
+  // allows to flush into a disk file.
+  // But there may be a new descendent that stores into a
+  // database field, if anyone is willing to write such
+  // a class (nothing much is involved, use the AsString property).
+  TJvCustomAppXMLStorage = class(TJvCustomAppMemoryFileStorage)
+  protected
+    FXml: TJvSimpleXml;
+    FCurrentNode: TJvSimpleXmlElem;
+    function GetAsString: string; override;
+    procedure SetAsString(const Value: string); override;
+
+    function GetRootNodeName: string;
+    procedure SetRootNodeName(const Value: string);
+    // Returns the last node in path, if it exists.
+    // Returns nil in all other cases
+    // If StartNode is nil, then FCurrentNode is used as a
+    // starting point for Path
+    function GetNodeFromPath(Path: string; StartNode: TJvSimpleXmlElem = nil): TJvSimpleXmlElem;
+    // Reads the \ separated Key string and sets FCurrentNode to
+    // the last one, having created all the required XML nodes
+    // including the last one
+    procedure CreateAndSetNode(Key: string);
+    procedure EnumFolders(const Path: string; const Strings: TStrings;
+      const ReportListAsValue: Boolean = True); override;
+    procedure EnumValues(const Path: string; const Strings: TStrings;
+      const ReportListAsValue: Boolean = True); override;
+    function IsFolderInt(Path: string; ListIsValue: Boolean = True): Boolean; override;
+    procedure SplitKeyPath(const Path: string; out Key, ValueName: string); override;
+    function PathExistsInt(const Path: string): boolean; override;
+    function ValueStoredInt(const Path: string): Boolean; override;
+    procedure DeleteValueInt(const Path: string); override;
+    procedure DeleteSubTreeInt(const Path: string); override;
+    function DoReadBoolean(const Path: string; Default: Boolean): Boolean; override;
+    procedure DoWriteBoolean(const Path: string; Value: Boolean); override;
+    function DoReadInteger(const Path: string; Default: Integer): Integer; override;
+    procedure DoWriteInteger(const Path: string; Value: Integer); override;
+    function DoReadFloat(const Path: string; Default: Extended): Extended; override;
+    procedure DoWriteFloat(const Path: string; Value: Extended); override;
+    function DoReadString(const Path: string; Default: string): string; override;
+    procedure DoWriteString(const Path: string; Value: string); override;
+    function DoReadBinary(const Path: string; var Buf; BufSize: Integer): Integer; override;
+    procedure DoWriteBinary(const Path: string; const Buf; BufSize: Integer); override;
+
+    property Xml: TJvSimpleXml read FXml;
+    property RootNodeName: string read GetRootNodeName write SetRootNodeName;
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+    
+  end;
+
+  // This class handles the flushing into a disk file
+  // and publishes a few properties for them to be
+  // used by the user in the IDE
+  TJvAppXMLFileStorage = class (TJvCustomAppXMLStorage)
+  public
+    property Xml;
+
+    procedure Flush; override;
+    procedure Reload; override;
+    
+    property AsString;
+  published
+    property FileName;
+    property Location;
+    property RootNodeName;
+  end;
+  
+implementation
+
+uses
+  TypInfo,
+  JclStrings,
+  JvQTypes, JvQConsts, JvQResources;
+
+const
+  cNullDigit = '0';
+  cCount = 'Count';
+  cEmptyPath = 'EmptyPath';
+
+function BinStrToBuf(Value: string; var Buf; BufSize: Integer): Integer;
+var
+  P: PChar;
+begin
+  if Odd(Length(Value)) then
+    Value := cNullDigit + Value;
+  if (Length(Value) div 2) < BufSize then
+    BufSize := Length(Value) div 2;
+  Result := 0;
+  P := PChar(Value);
+  while (BufSize > 0) do
+  begin
+    PChar(Buf)[Result] := Chr(StrToInt('$' + P[0] + P[1]));
+    Inc(Result);
+    Dec(BufSize);
+    Inc(P, 2);
+  end;
+end;
+
+function BufToBinStr(const Buf; BufSize: Integer): string;
+var
+  P: PChar;
+  S: string;
+begin
+  SetLength(Result, BufSize * 2);
+  P := PChar(Result);
+  Inc(P, (BufSize - 1) * 2); // Point to end of string ^
+  while BufSize > 0 do
+  begin
+    S := IntToHex(Ord(PChar(Buf)[BufSize]), 2);
+    P[0] := S[1];
+    P[1] := S[2];
+    Dec(P, 2);
+    Dec(BufSize);
+  end;
+end;
+
+//=== TJvAppXMLStorage =======================================================
+
+constructor TJvCustomAppXMLStorage.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+  FXml := TJvSimpleXml.Create(nil);
+  RootNodeName := 'Configuration';
+  FCurrentNode := FXml.Root;
+end;
+
+destructor TJvCustomAppXMLStorage.Destroy;
+begin
+  inherited Destroy;
+  // delete after the inherited call, see comment in
+  // the base class, TJvCustomMemoryFileAppStorage
+  FXml.Free;
+end;
+
+procedure TJvCustomAppXMLStorage.SetRootNodeName(const Value: string);
+begin
+  if Value = '' then
+    raise EPropertyError.Create(RsENodeCannotBeEmpty)
+  else
+  begin
+    StringReplace(Value, ' ', '_', [rfReplaceAll]);
+    FXml.Root.Name := Value;
+  end;
+end;
+
+procedure TJvCustomAppXMLStorage.SplitKeyPath(const Path: string; out Key, ValueName: string);
+begin
+  inherited SplitKeyPath(Path, Key, ValueName);
+  if Key = '' then
+    Key := Path;
+end;
+
+function TJvCustomAppXMLStorage.ValueStoredInt(const Path: string): Boolean;
+var
+  Section: string;
+  Key: string;
+  Node: TJvSimpleXmlElem;
+begin
+  SplitKeyPath(Path, Section, Key);
+  Result := False;
+  Node := GetNodeFromPath(Section);
+  if Assigned(Node) then
+    Result := Assigned(Node.Items.ItemNamed[Key]);
+end;
+
+procedure TJvCustomAppXMLStorage.DeleteValueInt(const Path: string);
+var
+  Node: TJvSimpleXmlElem;
+  Section: string;
+  Key: string;
+begin
+  if ValueStored(Path) then
+  begin
+    SplitKeyPath(Path, Section, Key);
+    Node := GetNodeFromPath(Section);
+    if Assigned(Node) then
+      Node.Items.Delete(Key);
+  end;
+end;
+
+procedure TJvCustomAppXMLStorage.DeleteSubTreeInt(const Path: string);
+var
+  TopNode: string;
+  Node: TJvSimpleXmlElem;
+  Parent: TJvSimpleXmlElem;
+  Name: string;
+begin
+  TopNode := GetAbsPath(Path);
+  if TopNode = '' then
+    TopNode := Path;
+  Node := GetNodeFromPath(TopNode);
+  if Assigned(Node) then
+  begin
+    Name := Node.Name;
+    Parent := Node.Parent;
+    if Assigned(Parent) then
+      Parent.Items.Delete(Name);
+  end;
+end;
+
+function TJvCustomAppXMLStorage.DoReadInteger(const Path: string; Default: Integer): Integer;
+var
+  ParentPath: string;
+  ValueName: string;
+  Node: TJvSimpleXmlElem;
+begin
+  SplitKeyPath(Path, ParentPath, ValueName);
+
+  Node := GetNodeFromPath(ParentPath);
+
+  if Assigned(Node) and Assigned(Node.Items.ItemNamed[ValueName]) then
+  begin
+    try
+      Result := Node.Items.ItemNamed[ValueName].IntValue;
+    except
+      if StorageOptions.DefaultIfReadConvertError then
+        Result := Default
+      else
+        raise;
+    end;
+  end
+  else
+  if StorageOptions.DefaultIfValueNotExists then
+    Result := Default
+  else
+    raise EJVCLException.CreateFmt(RsEPathDoesntExists, [Path]);
+end;
+
+procedure TJvCustomAppXMLStorage.DoWriteInteger(const Path: string; Value: Integer);
+var
+  ParentPath: string;
+  ValueName: string;
+begin
+  SplitKeyPath(Path, ParentPath, ValueName);
+  CreateAndSetNode(ParentPath);
+  FXml.Options := [sxoAutoCreate, sxoAutoIndent];
+  FCurrentNode.Items.ItemNamed[ValueName].IntValue := Value;
+  FXml.Options := [sxoAutoIndent];
+end;
+
+function TJvCustomAppXMLStorage.DoReadFloat(const Path: string; Default: Extended): Extended;
+var
+  ParentPath: string;
+  ValueName: string;
+  StrValue: string;
+  Node: TJvSimpleXmlElem;
+begin
+  SplitKeyPath(Path, ParentPath, ValueName);
+
+  Node := GetNodeFromPath(ParentPath);
+
+  if Assigned(Node) and Assigned(Node.Items.ItemNamed[ValueName]) then
+  begin
+    try
+      StrValue := Node.Items.ItemNamed[ValueName].Value;
+      Result := StrToFloat(StrValue);
+    except
+      if StorageOptions.DefaultIfReadConvertError then
+        Result := Default
+      else
+        raise;
+    end;
+  end
+  else
+  if StorageOptions.DefaultIfValueNotExists then
+    Result := Default
+  else
+    raise EJVCLException.CreateFmt(RsEPathDoesntExists, [Path]);
+end;
+
+procedure TJvCustomAppXMLStorage.DoWriteFloat(const Path: string; Value: Extended);
+var
+  ParentPath: string;
+  ValueName: string;
+begin
+  SplitKeyPath(Path, ParentPath, ValueName);
+  CreateAndSetNode(ParentPath);
+  FXml.Options := [sxoAutoCreate, sxoAutoIndent];
+  FCurrentNode.Items.ItemNamed[ValueName].Value := FloatToStr(Value);
+  FXml.Options := [sxoAutoIndent];
+end;
+
+function TJvCustomAppXMLStorage.DoReadString(const Path: string; Default: string): string;
+var
+  ParentPath: string;
+  ValueName: string;
+  Node: TJvSimpleXmlElem;
+begin
+  SplitKeyPath(Path, ParentPath, ValueName);
+
+  Node := GetNodeFromPath(ParentPath);
+
+  if Assigned(Node) and Assigned(Node.Items.ItemNamed[ValueName]) then
+  begin
+    try
+      Result := Node.Items.ItemNamed[ValueName].Value;
+    except
+      if StorageOptions.DefaultIfReadConvertError then
+        Result := Default
+      else
+        raise;
+    end;
+  end
+  else
+  if StorageOptions.DefaultIfValueNotExists then
+    Result := Default
+  else
+    raise EJVCLException.CreateFmt(RsEPathDoesntExists, [Path]);
+end;
+
+procedure TJvCustomAppXMLStorage.DoWriteString(const Path: string; Value: string);
+var
+  ParentPath: string;
+  ValueName: string;
+begin
+  SplitKeyPath(Path, ParentPath, ValueName);
+  CreateAndSetNode(ParentPath);
+  FXml.Options := [sxoAutoCreate, sxoAutoIndent];
+  FCurrentNode.Items.ItemNamed[ValueName].Value := Value;
+  FXml.Options := [sxoAutoIndent];
+end;
+
+function TJvCustomAppXMLStorage.DoReadBinary(const Path: string; var Buf; BufSize: Integer): Integer;
+var
+  Value: string;
+begin
+  Value := DoReadString(Path, '');
+  Result := BinStrToBuf(Value, Buf, BufSize);
+end;
+
+procedure TJvCustomAppXMLStorage.DoWriteBinary(const Path: string; const Buf; BufSize: Integer);
+begin
+  DoWriteString(Path, BufToBinStr(Buf, BufSize));
+end;
+
+procedure TJvCustomAppXMLStorage.EnumFolders(const Path: string;
+  const Strings: TStrings; const ReportListAsValue: Boolean);
+var
+  RefPath: string;
+  I: Integer;
+  Node: TJvSimpleXmlElem;
+begin
+  RefPath := GetAbsPath(Path);
+  if RefPath = '' then
+    RefPath := cEmptyPath;
+
+  Node := GetNodeFromPath(RefPath, FXml.Root);
+
+  if Node <> nil then
+  begin
+    Strings.BeginUpdate;
+    try
+      Strings.Clear;
+      for I := 0 to Node.Items.Count - 1 do
+        Strings.Add(Node.Items[I].Name);
+    finally
+      Strings.EndUpdate;
+    end;
+  end
+  else
+    raise EJVCLException.CreateFmt(RsEPathDoesntExists, [RefPath]);
+end;
+
+procedure TJvCustomAppXMLStorage.EnumValues(const Path: string;
+  const Strings: TStrings; const ReportListAsValue: Boolean);
+var
+  PathIsList: Boolean;
+  RefPath: string;
+  I: Integer;
+  Node: TJvSimpleXmlElem;
+  Name: string;
+begin
+  PathIsList := ReportListAsValue and ListStored(Path);
+  RefPath := GetAbsPath(Path);
+  if RefPath = '' then
+    RefPath := cEmptyPath;
+
+  Node := GetNodeFromPath(RefPath, FXml.Root);
+
+  if Node <> nil then
+  begin
+    Strings.BeginUpdate;
+    try
+      Strings.Clear;
+      for I := 0 to Node.Items.Count - 1 do
+      begin
+        Name := Node.Items[I].Name;
+        if (not PathIsList or (not AnsiSameText(cCount, Name) and
+          not NameIsListItem(Name))) then
+          Strings.Add(Name);
+      end;
+    finally
+      Strings.EndUpdate;
+    end;
+  end
+  else
+    raise EJVCLException.CreateFmt(RsEPathDoesntExists, [RefPath]);
+end;
+
+function TJvCustomAppXMLStorage.IsFolderInt(Path: string;
+  ListIsValue: Boolean): Boolean;
+var
+  RefPath: string;
+  ValueNames: TStrings;
+  I: Integer;
+  Node: TJvSimpleXmlElem;
+  Name: string;
+begin
+  RefPath := GetAbsPath(Path);
+  if RefPath = '' then
+    RefPath := cEmptyPath;
+
+  Node := GetNodeFromPath(RefPath);
+  Result := False;
+  if Assigned(Node) and ListIsValue and
+    Assigned(Node.Items.ItemNamed[cCount]) then
+  begin
+    ValueNames := TStringList.Create;
+    try
+      I := 0;
+      repeat
+        Name := Node.Items[I].Name;
+        Result := not AnsiSameText(cCount, Name) and
+          not NameIsListItem(Name);
+        Inc(I);
+      until (I = Node.Items.Count) or Result;
+    finally
+      ValueNames.Free;
+    end;
+  end;
+end;
+
+function TJvCustomAppXMLStorage.GetRootNodeName: string;
+begin
+  Result := FXml.Root.Name;
+end;
+
+procedure TJvCustomAppXMLStorage.CreateAndSetNode(Key: string);
+begin
+  FXml.Options := [sxoAutoCreate, sxoAutoIndent];
+  FCurrentNode := GetNodeFromPath(Key, FXml.Root);
+  FXml.Options := [sxoAutoIndent];
+end;
+
+function TJvCustomAppXMLStorage.GetNodeFromPath(Path: string; StartNode: TJvSimpleXmlElem = nil): TJvSimpleXmlElem;
+var
+  NodeList: TStringList;
+  I: Integer;
+  Node: TJvSimpleXmlElem;
+begin
+  NodeList := TStringList.Create;
+  if StartNode <> nil then
+    Node := StartNode
+  else
+    Node := FCurrentNode;
+
+  try
+    try
+      StrToStrings(Path, '\', NodeList, false);
+      for I := 0 to NodeList.Count - 1 do
+      begin
+        if Assigned(Node.Items.ItemNamed[NodeList[i]]) then
+          Node := Node.Items.ItemNamed[NodeList[i]]
+        else
+        begin
+          Result := nil;
+          Exit;
+        end;
+      end;
+    finally
+      NodeList.Free;
+    end;
+  except
+    Node := nil;
+  end;
+  Result := Node;
+end;
+
+function TJvCustomAppXMLStorage.PathExistsInt(const Path: string): boolean;
+var
+  SubKey: string;
+  ValueName: string;
+  Node: TJvSimpleXmlElem;
+begin
+  SplitKeyPath(Path, SubKey, ValueName);
+  Result := False;
+  Node := GetNodeFromPath(Path, FCurrentNode);
+  if Assigned(Node) then
+    Result := Assigned(Node.Items.ItemNamed[ValueName]);
+end;
+
+function TJvCustomAppXMLStorage.DoReadBoolean(const Path: string;
+  Default: Boolean): Boolean;
+var
+  ParentPath: string;
+  ValueName: string;
+  Node: TJvSimpleXmlElem;
+begin
+  SplitKeyPath(Path, ParentPath, ValueName);
+
+  Node := GetNodeFromPath(ParentPath);
+
+  if Assigned(Node) and Assigned(Node.Items.ItemNamed[ValueName]) then
+  begin
+    try
+      Result := Node.Items.ItemNamed[ValueName].BoolValue;
+    except
+      if StorageOptions.DefaultIfReadConvertError then
+        Result := Default
+      else
+        raise;
+    end;
+  end
+  else
+  if StorageOptions.DefaultIfValueNotExists then
+    Result := Default
+  else
+    raise EJVCLException.CreateFmt(RsEPathDoesntExists, [Path]);
+end;
+
+procedure TJvCustomAppXMLStorage.DoWriteBoolean(const Path: string;
+  Value: Boolean);
+var
+  ParentPath: string;
+  ValueName: string;
+begin
+  SplitKeyPath(Path, ParentPath, ValueName);
+  CreateAndSetNode(ParentPath);
+  FXml.Options := [sxoAutoCreate, sxoAutoIndent];
+  FCurrentNode.Items.ItemNamed[ValueName].BoolValue := Value;
+  FXml.Options := [sxoAutoIndent];
+end;
+
+function TJvCustomAppXMLStorage.GetAsString: string;
+begin
+  Result := FXml.SaveToString;
+end;
+
+procedure TJvCustomAppXMLStorage.SetAsString(const Value: string);
+begin
+  FXml.LoadFromString(Value);
+end;
+
+{ TJvAppXMLFileStorage }
+
+procedure TJvAppXMLFileStorage.Flush;
+begin
+  if FullFileName <> '' then
+    FXml.SaveToFile(FullFileName);
+end;
+
+procedure TJvAppXMLFileStorage.Reload;
+begin
+  if FileExists(FullFileName) then
+    FXml.LoadFromFile(FullFileName);
+end;
+
+end.
+
---
+++ jvcl/qrun/JvQArrayButton.pas
@@ -0,0 +1,345 @@
+{**************************************************************************************************}
+{  WARNING:  JEDI preprocessor generated unit. Manual modifications will be lost on next release.  }
+{**************************************************************************************************}
+
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/MPL-1.1.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: JvArrayButton.PAS, released on 2002-06-15.
+
+The Initial Developer of the Original Code is Jan Verhoeven [jan1.verhoeven@wxs.nl]
+Portions created by Jan Verhoeven are Copyright (C) 2002 Jan Verhoeven.
+All Rights Reserved.
+
+Contributor(s): Robert Love [rlove@slcdug.org].
+
+Last Modified: 2003-10-25
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
+located at http://jvcl.sourceforge.net
+
+Known Issues:
+-----------------------------------------------------------------------------}
+
+{$I jvcl.inc}
+
+unit JvQArrayButton;
+
+interface
+
+uses
+  
+  
+  Types, QGraphics, QControls, QForms, QExtCtrls, QButtons, QWindows,
+  
+  SysUtils, Classes, JvQComponent;
+
+type
+  TArrayButtonClicked = procedure(ACol, ARow: Integer) of object;
+
+  TJvArrayButton = class(TJvGraphicControl)
+  private
+    FPtDown: TPoint;
+    FPushDown: boolean;
+    FColor: TColor;
+    FRows: Integer;
+    FCols: Integer;
+    FOnArrayButtonClicked: TArrayButtonClicked;
+    FCaptions: TStringList;
+    FColors: TStringList;
+    FHints: TStringList;
+    
+    procedure SetCols(const Value: Integer);
+    procedure SetRows(const Value: Integer);
+    procedure SetCaptions(const Value: TStringList);
+    procedure SetColors(const Value: TStringList);
+    procedure MouseToCell(const X, Y: Integer; var ACol, ARow: Integer);
+    function CellRect(ACol, ARow: Integer): TRect;
+    procedure SetHints(const Value: TStringList);
+  protected
+    procedure FontChanged; override;
+    procedure MouseDown(Button: TMouseButton; Shift: TShiftState; X, Y: Integer); override;
+    procedure MouseUp(Button: TMouseButton; Shift: TShiftState; X, Y: Integer); override;
+    
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+    procedure Paint; override;
+    procedure DoShowHint(var HintStr:  WideString ;
+      var CanShow: Boolean; var HintInfo: THintInfo);
+
+    {this procedure can be used in response to a Application.OnShowHint event
+     button hints are stored in the hints property from array top-left to array bottom right
+     in your application create a seperate onshowhint event Handler
+     within that Handler test HintInfo.HintControl is this object. If it is dispatch to this objects doShowHint.
+     In the formcreate event handler include:
+       Application.OnShowHint := DrawHint;
+
+     procedure TDrawF.DrawHint(var HintStr: string; var CanShow: Boolean;
+       var HintInfo: THintInfo);
+     begin
+       if HintInfo.HintControl = JvArrayButton1 then
+          JvArrayButton1.DoShowHint(HintStr, CanShow, HintInfo);
+     end;
+
+     I could have set the Application.OnShowHint handler directly in this component,
+     but if you have more components that do this then only the last one would work
+     }
+  published
+    property Align;
+    property Rows: Integer read FRows write SetRows;
+    property Cols: Integer read FCols write SetCols;
+    property Font;
+    property Captions: TStringList read FCaptions write SetCaptions;
+    property Height default 35;
+    {A List of button captions from the top-left to the bottom-right button}
+    property Hints: TStringList read FHints write SetHints;
+    {A List of button hints from the top-left to the bottom-right button}
+    property Colors: TStringList read FColors write SetColors;
+    {A List of button Colors from the top-left to the bottom-right button
+     values must standard Delphi Color names like clRed, clBlue or hex Color strings like $0000ff for red.
+     please note the hex order in Delphi is BGR i.s.o. the RGB order you may know from HTML hex Color triplets}
+    property Hint;
+    property ShowHint default True;
+    
+    property Width default 35;
+    property OnArrayButtonClicked: TArrayButtonClicked read FOnArrayButtonClicked write FOnArrayButtonClicked;
+    {provides you with the Column and Row of the clicked button
+    the topleft button has Column=0 and Row=0}
+  end;
+
+implementation
+
+uses
+  JvQThemes;
+
+constructor TJvArrayButton.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+  Width := 35;
+  Height := 35;
+  FColor := clSilver;
+  FPushDown := False;
+  FCols := 1;
+  FRows := 1;
+  ShowHint := True;
+  FCaptions := TStringList.Create;
+  FHints := TStringList.Create;
+  FColors := TStringList.Create;
+  
+end;
+
+destructor TJvArrayButton.Destroy;
+begin
+  FCaptions.Free;
+  FHints.Free;
+  FColors.Free;
+  inherited Destroy;
+end;
+
+procedure TJvArrayButton.MouseToCell(const X, Y: Integer; var ACol, ARow: Integer);
+var
+  dh, dw: Integer;
+begin
+  dh := (Height - 2) div FRows;
+  dw := (Width - 2) div FCols;
+  ACol := X div dw;
+  ARow := Y div dh;
+end;
+
+procedure TJvArrayButton.MouseDown(Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+var
+  Col, Row: Integer;
+begin
+  if Button = mbLeft then
+  begin
+    FPushDown := True;
+    MouseToCell(X, Y, Col, Row);
+    FptDown := Point(Col, Row);
+    Invalidate;
+  end;
+end;
+
+procedure TJvArrayButton.MouseUp(Button: TMouseButton; Shift: TShiftState;
+  X, Y: Integer);
+begin
+  if Button = mbLeft then
+  begin
+    FPushDown := False;
+    Invalidate;
+    if Assigned(FOnArraybuttonClicked) then
+      OnArrayButtonClicked(FptDown.X, FptDown.Y);
+  end
+end;
+
+
+
+procedure TJvArrayButton.Paint;
+var
+  R: TRect;
+  Col, Row: Integer;
+  dh, dw: Integer;
+  X0, Y0: Integer;
+  Cap: string;
+  BackColor: TColor;
+  Index: Integer;
+
+  procedure DrawBackground(AColor: TColor);
+  begin
+    Canvas.Brush.Color := AColor;
+    DrawThemedBackground(Self, Canvas, R);
+  end;
+
+  procedure DrawUp;
+  begin
+    
+    begin
+      DrawBackground(BackColor);
+      Frame3D(Self.Canvas, R, clBtnHighlight, clBlack, 1);
+    end;
+    if Cap <> '' then
+      DrawText(Canvas.Handle, PChar(Cap), -1, R, DT_CENTER or DT_VCENTER or DT_SINGLELINE);
+  end;
+
+  procedure DrawDown;
+  begin
+    
+    begin
+      DrawBackground(BackColor);
+      Frame3D(Self.Canvas, R, clblack, clBtnHighlight, 1);
+    end;
+    if Cap <> '' then
+      DrawText(Canvas.Handle, PChar(Cap), -1, R, DT_CENTER or DT_VCENTER or DT_SINGLELINE);
+  end;
+
+begin
+  dh := (Height - 2) div FRows;
+  dw := (Width - 2) div FCols;
+  for Row := 0 to FRows - 1 do
+  begin
+    Y0 := 1 + Row * dh;
+    for Col := 0 to FCols - 1 do
+    begin
+      X0 := 1 + Col * dw;
+      R := Rect(X0, Y0, X0 + dw, Y0 + dh);
+      Index := Row * FCols + Col;
+      if Index < FCaptions.Count then
+        Cap := FCaptions[Index]
+      else
+        Cap := '';
+      if Index < FColors.Count then
+        try
+          BackColor := StringToColor(FColors[Index]);
+        except
+          BackColor := clSilver;
+        end
+      else
+        BackColor := clSilver;
+      if (csDesigning in ComponentState) then
+        DrawUp
+      else
+      if (FptDown.X = Col) and (FptDown.Y = Row) then
+      begin
+        if FPushDown then
+          DrawDown
+        else
+          DrawUp;
+      end
+      else
+        DrawUp;
+    end;
+  end;
+end;
+
+procedure TJvArrayButton.SetCols(const Value: Integer);
+begin
+  if FCols <> Value then
+    if (Value >= 1) and (Value <= 10) then
+    begin
+      FCols := Value;
+      Invalidate;
+    end;
+end;
+
+procedure TJvArrayButton.SetRows(const Value: Integer);
+begin
+  if FRows <> Value then
+    if (Value >= 1) and (Value <= 10) then
+    begin
+      FRows := Value;
+      Invalidate;
+    end;
+end;
+
+
+
+procedure TJvArrayButton.SetCaptions(const Value: TStringList);
+begin
+  FCaptions.Assign(Value);
+  Invalidate;
+end;
+
+procedure TJvArrayButton.FontChanged;
+begin
+  inherited FontChanged;
+  Canvas.Font.Assign(Font);
+  Invalidate;
+end;
+
+procedure TJvArrayButton.SetColors(const Value: TStringList);
+begin
+  FColors.Assign(Value);
+  Invalidate;
+end;
+
+function TJvArrayButton.CellRect(ACol, ARow: Integer): TRect;
+var
+  dh, dw, X0, Y0: Integer;
+begin
+  dh := (Height - 2) div FRows;
+  dw := (Width - 2) div FCols;
+  Y0 := 1 + ARow * dh;
+  X0 := 1 + ACol * dw;
+  //  pt1:=clienttoscreen(point(X0,Y0));
+  //  pt2:=clienttoscreen(point(X0+dw,Y0+dh));
+  //  result:=rect(pt1.X,pt1.Y,pt2.X,pt2.Y);
+  Result := Rect(X0, Y0, X0 + dw, Y0 + dh);
+end;
+
+procedure TJvArrayButton.DoShowHint(var HintStr:  WideString ;
+  var CanShow: Boolean; var HintInfo: THintInfo);
+var
+  ACol, ARow, X, Y: Integer;
+  Index: Integer;
+begin
+  if HintInfo.HintControl = Self then
+  begin
+    X := HintInfo.CursorPos.X;
+    Y := HintInfo.CursorPos.Y;
+    MouseToCell(X, Y, ACol, ARow);
+    if (ACol < 0) or (ARow < 0) then
+      Exit;
+    Index := ARow * FCols + ACol;
+    if Index < FHints.Count then
+      HintStr := FHints[Index]
+    else
+      HintStr := Hint;
+    HintInfo.CursorRect := CellRect(ACol, ARow);
+    CanShow := True;
+  end;
+end;
+
+procedure TJvArrayButton.SetHints(const Value: TStringList);
+begin
+  FHints.Assign(Value);
+end;
+
+end.
