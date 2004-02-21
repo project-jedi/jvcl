a24afda60a9f399a58a212b28cf2a51aec36f817
asnepvangers <asnepvangers@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-02-21 13:56:56 +0000

    VisualCLX  JvQCrypt-D units Note: generated files

---
+++ jvcl/qdesign/JvQCryptReg.pas
@@ -0,0 +1,74 @@
+{**************************************************************************************************}
+{  WARNING:  JEDI preprocessor generated unit. Manual modifications will be lost on next release.  }
+{**************************************************************************************************}
+
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/MPL-1.1.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: JvCryptReg.PAS, released on 2002-05-26.
+
+The Initial Developer of the Original Code is John Doe.
+Portions created by John Doe are Copyright (C) 2003 John Doe.
+All Rights Reserved.
+
+Contributor(s):
+
+Last Modified: 2003-11-09
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
+located at http://jvcl.sourceforge.net
+
+Known Issues:
+-----------------------------------------------------------------------------}
+
+{$I jvcl.inc}
+
+unit JvQCryptReg;
+
+interface
+
+procedure Register;
+
+implementation
+
+uses
+  Classes,
+  {$IFNDEF BCB}
+  JvQZlibMultiple,
+  {$ENDIF BCB}
+  JvQDsgnConsts,
+  JvQVigenereCipher,
+  {$IFDEF MSWINDOWS}
+  JvQCabFile,
+  {$ENDIF MSWINDOWS}
+  JvQCaesarCipher, JvQGenetic,
+  JvQSerialMaker, JvQXorCipher;
+
+{$IFDEF MSWINDOWS}
+{$R ..\Resources\JvCryptReg.dcr}
+{$ENDIF MSWINDOWS}
+{$IFDEF LINUX}
+{$R ../Resources/JvCryptReg.dcr}
+{$ENDIF LINUX}
+
+procedure Register;
+begin
+  RegisterComponents(RsPaletteEncryptCompress, [TJvVigenereCipher,
+    TJvXORCipher, TJvCaesarCipher, TJvGenetic,
+    {$IFDEF MSWINDOWS}
+    TJvCABFile,
+    {$ENDIF MSWINDOWS}
+    {$IFNDEF BCB}
+    TJvZlibMultiple,
+    {$ENDIF BCB}
+    TJvSerialMaker]);
+end;
+
+end.
