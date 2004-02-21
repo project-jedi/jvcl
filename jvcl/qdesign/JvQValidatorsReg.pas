9e27ea0cc8ef69a1069867f8198d65e906dba011
asnepvangers <asnepvangers@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-02-21 19:03:52 +0000

    VisualCLX  JvQValidators-D units Note: generated files

---
+++ jvcl/qdesign/JvQValidatorsReg.pas
@@ -0,0 +1,73 @@
+{**************************************************************************************************}
+{  WARNING:  JEDI preprocessor generated unit. Manual modifications will be lost on next release.  }
+{**************************************************************************************************}
+
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/MPL-1.1.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: JvValidatorsReg.PAS, released on 2003-01-01.
+
+The Initial Developer of the Original Code is Peter Thörnqvist [peter3@peter3.com] .
+Portions created by Peter Thörnqvist are Copyright (C) 2003 Peter Thörnqvist.
+All Rights Reserved.
+
+Contributor(s):
+
+Last Modified: 2003-01-01
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
+located at http://jvcl.sourceforge.net
+
+Known Issues:
+-----------------------------------------------------------------------------}
+
+{$I jvcl.inc}
+
+unit JvQValidatorsReg;
+
+interface
+
+{$IFDEF MSWINDOWS}
+{$R ..\Resources\JvValidatorsReg.dcr}
+{$ENDIF MSWINDOWS}
+{$IFDEF LINUX}
+{$R ../Resources/JvValidatorsReg.dcr}
+{$ENDIF LINUX}
+
+procedure Register;
+
+implementation
+
+uses
+  Classes,
+  
+  DesignEditors, DesignIntf,
+  
+  JvQDsgnConsts,
+  JvQErrorIndicator, JvQValidators, JvQDsgnEditors;
+
+procedure Register;
+begin
+  RegisterComponents(RsPaletteValidators, [TJvValidators,
+    TJvValidationSummary, TJvErrorIndicator]);
+  RegisterNoIcon([TJvRequiredFieldValidator, TJvCompareValidator,
+    TJvRangeValidator, TJvRegularExpressionValidator, TJvCustomValidator]);
+(*)
+  RegisterComponentEditor(TJvValidators, TJvValidatorComponent);
+  RegisterPropertyEditor(TypeInfo(Integer), TJvErrorIndicator,
+    'ImageIndex', TJvDefaultImageIndexProperty);
+//  RegisterPropertyEditor(TypeInfo(string), TJvCustomFormatEdit, 'Characters', TJvCharStringProperty);
+  RegisterPropertyEditor(TypeInfo(string), TJvBaseValidator,
+    'PropertyToValidate', TJvPropertyValidateProperty);
+(*)  
+end;
+
+end.
+
