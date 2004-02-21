7bf48a7dcff105b24a6dd626b8e8fc8c9a875b07
asnepvangers <asnepvangers@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-02-21 21:58:46 +0000

    VisualCLX units Note: generated files but some are modified Currently not in CVS because of codefreeze

---
+++ jvcl/qdesign/JvQCtrlsReg.pas
@@ -0,0 +1,117 @@
+{**************************************************************************************************}
+{  WARNING:  JEDI preprocessor generated unit. Manual modifications will be lost on next release.  }
+{**************************************************************************************************}
+
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/MPL-1.1.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: JvBandsReg.PAS, released on 2002-05-26.
+
+The Initial Developer of the Original Code is John Doe.
+Portions created by John Doe are Copyright (C) 2003 John Doe.
+All Rights Reserved.
+
+Contributor(s):
+
+Last Modified: 2003-11-09
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
+located at http://jvcl.sourceforge.net
+
+Known Issues:
+-----------------------------------------------------------------------------}
+
+{$I jvcl.inc}
+
+unit JvQCtrlsReg;
+
+interface
+
+procedure Register;
+
+implementation
+
+uses
+  Classes,
+  
+  
+  QControls, QImgList, QActnList, QTypes,
+  
+  
+  DesignEditors, DesignIntf,
+
+  JvQBehaviorLabelEditor, JvQHTHintForm, JvQFooterEditor, JvQGroupHeaderEditor,
+
+  JvQDsgnConsts,
+  JvQZoom, JvQBehaviorLabel, JvQArrowButton,
+  JvQaScrollText,
+
+  JvQClock, JvQContentScroller, JvQColorBox,
+  JvQColorButton,
+  JvQDice,
+  JvQFooter, JvQGroupHeader, JvQHint,
+  JvQHtControls,
+  JvQItemsPanel,
+
+  JvQScrollText, JvQSpacer,
+  JvQSwitch,
+  JvQColorForm, JvQDsgnIntf,
+  JvQImageDrawThread, JvQWinampLabel, JvQComponentPanel,
+  JvQButtons, JvQCaptionPanel, JvQMovableBevel
+  ;
+
+{$R ..\Resources\JvCtrlsReg.dcr}
+
+procedure Register;
+begin
+  RegisterComponents(RsPaletteButton, [TJvArrowButton, TJvColorButton,
+    TJvHTButton, TJvSpacer, TJvSwitch]);
+  RegisterComponents(RsPaletteBarPanel, [TJvCaptionPanel,
+    TJvItemsPanel, TJvMovableBevel, TJvFooter, TJvGroupHeader,
+    TJvComponentPanel]);
+  RegisterComponents(RsPaletteLabel, [TJvBehaviorLabel,
+
+    TJvHTLabel, TJvWinampLabel]);
+  RegisterComponents(RsPaletteListComboTree, [TJvHTListBox, TJvHTComboBox]);
+  RegisterComponents(RsPaletteScrollerTracker, [TJvaScrollText,
+    TJvContentScroller, TJvScrollText]);
+//  RegisterComponents(RsPaletteSliderSplitter, [TJvSplitter]);
+  RegisterComponents(RsPaletteVisual, [TJvClock, TJvZoom, TJvDice]);
+  RegisterComponents(RsPaletteNonVisual, [TJvHint {, TJvRegAuto}]);
+
+  RegisterPropertyEditor(TypeInfo(TCaption), TJvHTLabel, 'Caption', TJvHintProperty);
+  RegisterPropertyEditor(TypeInfo(TJvLabelBehaviorName), TJvBehaviorLabel, 'Behavior', TJvLabelBehaviorProperty);
+//  RegisterPropertyEditor(TypeInfo(TCursor), TJvxSplitter, 'Cursor', nil);
+  //RegisterPropertyEditor(TypeInfo(TDateTime), TJvAlarmInfo, 'Date', TJvDateTimeExProperty);
+  //RegisterPropertyEditor(TypeInfo(TDateTime), TJvAlarmInfo, 'Date', TJvDateTimeExProperty);
+//  RegisterPropertyEditor(TypeInfo(TCaption), TJvSpeedItem, 'BtnCaption', TStringProperty);
+
+//  RegisterPropertyEditor(TypeInfo(integer), TJvTransparentButton2, 'ActiveIndex', TJvTBImagesProperty);
+//  RegisterPropertyEditor(TypeInfo(integer), TJvTransparentButton2, 'DisabledIndex', TJvTBImagesProperty);
+//  RegisterPropertyEditor(TypeInfo(integer), TJvTransparentButton2, 'DownIndex', TJvTBImagesProperty);
+//  RegisterPropertyEditor(TypeInfo(integer), TJvTransparentButton2, 'GrayIndex', TJvTBImagesProperty);
+//  RegisterPropertyEditor(TypeInfo(TImageIndex), TJvRollOutImageOptions, '', TJvRollOutOptionsImagesProperty);
+
+//  RegisterComponentEditor(TJvScrollMax, TJvScrollMaxEditor);
+//  RegisterComponentEditor(TJvRollOut, TJvRollOutDefaultEditor);
+  RegisterComponentEditor(TJvGroupHeader, TJvGroupHeaderEditor);
+  RegisterComponentEditor(TJvFooter, TJvFooterEditor);
+//  RegisterComponentEditor(TJvImageListBox, TJvStringsEditor);
+//  RegisterComponentEditor(TJvImageComboBox, TJvStringsEditor);
+//  RegisterComponentEditor(TJvSpeedBar, TJvSpeedbarCompEditor);
+  //  RegisterComponentEditor(TJvRegAuto, TJvRegAutoEditor);
+
+//  RegisterNoIcon([TJvSpeedItem, TJvSpeedbarSection]);
+//  RegisterClass(TJvScrollMaxBand);
+  RegisterClass(TJvFooterBtn);
+//  RegisterActions(RsJVCLActionsCategory, [TJvRollOutAction], nil);
+end;
+
+end.
