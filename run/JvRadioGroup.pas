fddf9b78a18daf0fa819771dae5028706fbf53ba
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-02-29 01:44:43 +0000

    Updated for Peter's extensions

--- run/JvRadioGroup.pas
+++ run/JvRadioGroup.pas
@@ -33,7 +33,7 @@ interface
 uses
   SysUtils, Classes,
   {$IFDEF VCL}
-  Windows, Messages, Graphics, Controls, Forms, StdCtrls, ExtCtrls,
+  Windows, Messages, Graphics, Controls, Forms, StdCtrls, ExtCtrls, ToolWin,
   {$ENDIF VCL}
   {$IFDEF VisualCLX}
   Types, QGraphics, QControls, QForms, QStdCtrls, QExtCtrls,
@@ -41,25 +41,44 @@ uses
   JvThemes, JvExControls, JvExExtCtrls;
 
 type
+  TJvRadioGroupHintEvent = procedure(Sender: TObject; Index: Integer;
+    var AHint: TCaption) of object;
+
   TJvRadioGroup = class(TJvExRadioGroup, IJvDenySubClassing)
   private
     FReadOnly: Boolean;
-    FDrawFrame: Boolean;
-    procedure SetDrawFrame(const Value: Boolean);
+
+    FEdgeBorders: TEdgeBorders;
+    FEdgeInner: TEdgeStyle;
+    FEdgeOuter: TEdgeStyle;
+    FCaptionVisible: Boolean;
+    FOnItemHint: TJvRadioGroupHintEvent;
+    procedure SetEdgeBorders(const Value: TEdgeBorders);
+    procedure SetEdgeInner(const Value: TEdgeStyle);
+    procedure SetEdgeOuter(const Value: TEdgeStyle);
+    procedure SetCaptionVisible(Value: Boolean);
   protected
     procedure Paint; override;
     function CanModify: Boolean; override;
+
+    procedure GetItemHint(Index: Integer; var AHint: TCaption); virtual;
+    function HintShow(var HintInfo: THintInfo): Boolean; override;
   public
     constructor Create(AOwner: TComponent); override;
   published
+    property CaptionVisible: Boolean read FCaptionVisible write SetCaptionVisible;
+    property EdgeBorders: TEdgeBorders read FEdgeBorders write SetEdgeBorders default [ebLeft, ebTop, ebRight, ebBottom];
+    property EdgeInner: TEdgeStyle read FEdgeInner write SetEdgeInner default esRaised;
+    property EdgeOuter: TEdgeStyle read FEdgeOuter write SetEdgeOuter default esLowered;
+
     property HintColor;
     {$IFDEF JVCLThemesEnabledD56}
     property ParentBackground default True;
     {$ENDIF JVCLThemesEnabledD56}
     property ReadOnly: Boolean read FReadOnly write FReadOnly default False;
-    property DrawFrame: Boolean read FDrawFrame write SetDrawFrame default True;
     property OnMouseEnter;
     property OnMouseLeave;
+    property OnItemHint: TJvRadioGroupHintEvent read FOnItemHint write FOnItemHint;
     property OnParentColorChange;
   end;
 
@@ -71,21 +90,31 @@ uses
 constructor TJvRadioGroup.Create(AOwner: TComponent);
 begin
   inherited Create(AOwner);
-  FDrawFrame := True;
+  FEdgeBorders := [ebLeft, ebTop, ebRight, ebBottom];
+  FEdgeInner := esRaised;
+  FEdgeOuter := esLowered;
+  FCaptionVisible := True;
   {$IFDEF JVCLThemesEnabledD56}
   IncludeThemeStyle(Self, [csParentBackground]);
   {$ENDIF JVCLThemesEnabledD56}
 end;
 
 procedure TJvRadioGroup.Paint;
-{$IFDEF JVCLThemesEnabledD56}
+const
+  InnerStyles: array[TEdgeStyle] of Integer = (0, BDR_RAISEDINNER, BDR_SUNKENINNER);
+  OuterStyles: array[TEdgeStyle] of Integer = (0, BDR_RAISEDOUTER, BDR_SUNKENOUTER);
+  Ctl3DStyles: array[Boolean] of Integer = (BF_MONO, 0);
 var
+  H: Integer;
+  R: TRect;
+  Flags: Longint;
+{$IFDEF JVCLThemesEnabledD56}
   Details: TThemedElementDetails;
-  R, CaptionRect: TRect;
+  ClipRect, CaptionRect: TRect;
 {$ENDIF JVCLThemesEnabledD56}
 begin
   {$IFDEF JVCLThemesEnabledD56}
-  if DrawFrame and ThemeServices.ThemesEnabled then
+  if ThemeServices.ThemesEnabled then
   begin
     if Enabled then
       Details := ThemeServices.GetElementDetails(tbGroupBoxNormal)
@@ -93,18 +122,53 @@ begin
       Details := ThemeServices.GetElementDetails(tbGroupBoxDisabled);
     R := ClientRect;
     Inc(R.Top, Canvas.TextHeight('0') div 2);
-    ThemeServices.DrawElement(Canvas.Handle, Details, R);
 
-    CaptionRect := Rect(8, 0, Min(Canvas.TextWidth(Caption) + 8, ClientWidth - 8), Canvas.TextHeight(Caption));
+    if EdgeBorders <> [] then
+    begin
+      if EdgeBorders <> [ebLeft, ebTop, ebRight, ebBottom] then
+      begin
+        ClipRect := R;
+        if not (ebLeft in EdgeBorders) then
+          Inc(ClipRect.Left, 3);
+        if not (ebRight in EdgeBorders) then
+          Dec(ClipRect.Right, 3);
+        if not (ebTop in EdgeBorders) then
+          Inc(ClipRect.Top, 3);
+        if not (ebBottom in EdgeBorders) then
+          Dec(ClipRect.Bottom, 3);
+      end;
+      ThemeServices.DrawElement(Canvas.Handle, Details, R, @ClipRect);
+    end;
+    if CaptionVisible then
+    begin
+      CaptionRect := Rect(8, 0, Min(Canvas.TextWidth(Caption) + 8, ClientWidth - 8), Canvas.TextHeight(Caption));
 
-    Canvas.Brush.Color := Self.Color;
-    DrawThemedBackground(Self, Canvas, CaptionRect);
-    ThemeServices.DrawText(Canvas.Handle, Details, Caption, CaptionRect, DT_LEFT, 0);
-  end
-  else
+      Canvas.Brush.Color := Self.Color;
+      DrawThemedBackground(Self, Canvas, CaptionRect);
+      ThemeServices.DrawText(Canvas.Handle, Details, Caption, CaptionRect, DT_LEFT, 0);
+    end;
+    Exit;
+  end;
   {$ENDIF JVCLThemesEnabledD56}
-  if DrawFrame then
-    inherited Paint;
+  with Canvas do
+  begin
+    Font := Self.Font;
+    H := TextHeight('0');
+    R := Rect(0, H div 2 - 1, Width, Height);  
+    DrawEdge(Handle, R, InnerStyles[FEdgeInner] or OuterStyles[FEdgeOuter],
+      Byte(FEdgeBorders) or Ctl3DStyles[Ctl3D] or BF_ADJUST);
+    if (Text <> '') and CaptionVisible then
+    begin
+      if not UseRightToLeftAlignment then
+        R := Rect(8, 0, 0, H)
+      else
+        R := Rect(R.Right - Canvas.TextWidth(Text) - 8, 0, 0, H);
+      Flags := DrawTextBiDiModeFlags(DT_SINGLELINE);
+      DrawText(Handle, PChar(Text), Length(Text), R, Flags or DT_CALCRECT);
+      Brush.Color := Color;
+      DrawText(Handle, PChar(Text), Length(Text), R, Flags);
+    end;
+  end;
 end;
 
 function TJvRadioGroup.CanModify: Boolean;
@@ -115,12 +179,74 @@ begin
     Result := inherited CanModify;
 end;
 
-procedure TJvRadioGroup.SetDrawFrame(const Value: Boolean);
+procedure TJvRadioGroup.SetEdgeBorders(const Value: TEdgeBorders);
 begin
-  if Value <> FDrawFrame then
+  if FEdgeBorders <> Value then
   begin
-    FDrawFrame := Value;
+    FEdgeBorders := Value;
     Invalidate;
+  end;
+end;
+
+procedure TJvRadioGroup.SetEdgeInner(const Value: TEdgeStyle);
+begin
+  if FEdgeInner <> Value then
+  begin
+    FEdgeInner := Value;
+    Invalidate;
+  end;
+end;
+
+procedure TJvRadioGroup.SetEdgeOuter(const Value: TEdgeStyle);
+begin
+  if FEdgeOuter <> Value then
+  begin
+    FEdgeOuter := Value;
+    Invalidate;
+  end;
+end;
+
+procedure TJvRadioGroup.SetCaptionVisible(Value: Boolean);
+begin
+  if FCaptionVisible <> Value then
+  begin
+    FCaptionVisible := Value;
+    Invalidate;
+  end;
+end;
+
+procedure TJvRadioGroup.GetItemHint(Index: Integer; var AHint: TCaption);
+begin
+  if Assigned(FOnItemHint) then
+    FOnItemHint(Self, Index, AHint);
+end;
+
+function TJvRadioGroup.HintShow(var HintInfo: THintInfo): Boolean;
+var
+  AItemX, AItemY,
+  AHeight, AWidth, VertCount: Integer;
+  ARect: TRect;
+begin
+  Result := False;
+  with HintInfo do
+  begin
+    ARect := ClientRect;
+    HintStr := Hint; // set default
+    if Items.Count > 0 then
+    begin
+      AHeight := ClientHeight div (Items.Count div Columns);
+      AWidth  := ClientWidth div Columns;
+      if (AHeight > 0) then
+      begin
+        VertCount := Items.Count div Columns;
+        AItemX := CursorPos.X div AWidth;
+        AItemY := CursorPos.Y div AHeight;
+        GetItemHint(AItemY + AItemX * VertCount, TCaption(HintStr));
+        ARect := Rect(AItemX * AWidth, AHeight * AItemY,
+          AItemX * AWidth + AWidth, AHeight * AItemY + AHeight);
+      end;
+      CursorRect := ARect;
+    end;
   end;
 end;
 
