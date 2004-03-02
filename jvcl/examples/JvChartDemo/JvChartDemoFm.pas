ce718a6201c2c189c56c7aa1fbc79a7ecfe882f2
wpostma <wpostma@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-03-02 21:08:17 +0000

    Property changes, demo fixed.

--- jvcl/examples/JvChartDemo/JvChartDemoFm.pas
+++ jvcl/examples/JvChartDemo/JvChartDemoFm.pas
@@ -35,50 +35,73 @@ interface
 uses
   Windows, SysUtils, Messages, Classes, Graphics, Controls,
   Forms, Dialogs, ExtCtrls, StdCtrls, Buttons, Spin,
-  JvChart, JvComponent, JvExControls, StatsClasses;
+  JvChart, JvComponent, JvExControls, StatsClasses, Menus;
 
 type
   TJvChartDemoForm = class(TForm)
     Panel1: TPanel;
-    SpeedButton1: TSpeedButton;
-    SpeedButton2: TSpeedButton;
-    SpeedButton3: TSpeedButton;
-    SpeedButton4: TSpeedButton;
-    SpeedButton5: TSpeedButton;
-    SpeedButton6: TSpeedButton;
-    SpeedButton7: TSpeedButton;
-    SpeedButton8: TSpeedButton;
-    SpeedButton9: TSpeedButton;
+    ButtonBarChart: TSpeedButton;
+    ButtonLine: TSpeedButton;
+    ButtonStackedBarAve: TSpeedButton;
+    ButtonStackedBar: TSpeedButton;
+    ButtonBarAve: TSpeedButton;
     ColorDialog1: TColorDialog;
-    SpeedButton10: TSpeedButton;
-    SpeedButton11: TSpeedButton;
+    ButtonPie: TSpeedButton;
     FontDialog1: TFontDialog;
-    SpeedButton12: TSpeedButton;
-    SpeedButton14: TSpeedButton;
-    ButtonNewValues: TButton;
-    CheckBox1: TCheckBox;
+    ButtonMarkers: TSpeedButton;
+    ButtonDeltaAverage: TSpeedButton;
     Chart: TJvChart;
-    procedure ButtonNewValuesClick(Sender: TObject);
-    procedure SpeedButton1Click(Sender: TObject);
+    ButtonLineMarker: TSpeedButton;
+    Label1: TLabel;
+    MainMenu1: TMainMenu;
+    Demo1: TMenuItem;
+    Help1: TMenuItem;
+    About1: TMenuItem;
+    AboutJVCL301: TMenuItem;
+    ShowgapinLineChart1: TMenuItem;
+    N1: TMenuItem;
+    Print1: TMenuItem;
+    Generatenewrandomvalues1: TMenuItem;
+    CopyToClipboard1: TMenuItem;
+    N2: TMenuItem;
+    SetHeaderFont1: TMenuItem;
+    Scrolling1: TMenuItem;
+    ListBox1: TListBox;
+    Splitter1: TSplitter;
+    Timer1: TTimer;
+    ShowDataInListbox1: TMenuItem;
+    N4: TMenuItem;
+    LargeDataset576samples1: TMenuItem;
     procedure FormResize(Sender: TObject);
-    procedure SpeedButton2Click(Sender: TObject);
-    procedure SpeedButton4Click(Sender: TObject);
-    procedure SpeedButton3Click(Sender: TObject);
-    procedure SpeedButton5Click(Sender: TObject);
-    procedure SpeedButton6Click(Sender: TObject);
+    procedure ButtonLineClick(Sender: TObject);
+    procedure ButtonBarChartClick(Sender: TObject);
+    procedure ButtonStackedBarAveClick(Sender: TObject);
+    procedure ButtonStackedBarClick(Sender: TObject);
     procedure SpeedButton7Click(Sender: TObject);
-    procedure SpeedButton8Click(Sender: TObject);
+    procedure ButtonBarAveClick(Sender: TObject);
     procedure SpinEdit1Change(Sender: TObject);
     procedure Button2Click(Sender: TObject);
-    procedure SpeedButton10Click(Sender: TObject);
-    procedure SpeedButton11Click(Sender: TObject);
+    procedure ButtonPieClick(Sender: TObject);
     procedure Button4Click(Sender: TObject);
-    procedure SpeedButton12Click(Sender: TObject);
-    procedure SpeedButton14Click(Sender: TObject);
+    procedure ButtonMarkersClick(Sender: TObject);
+    procedure ButtonDeltaAverageClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure SpeedButton13Click(Sender: TObject);
+    procedure ButtonLineMarkerClick(Sender: TObject);
+    procedure Panel2DblClick(Sender: TObject);
+    procedure ShowgapinLineChart1Click(Sender: TObject);
+    procedure Print1Click(Sender: TObject);
+    procedure Generatenewrandomvalues1Click(Sender: TObject);
+    procedure CopyToClipboard1Click(Sender: TObject);
+    procedure SetHeaderFont1Click(Sender: TObject);
+    procedure About1Click(Sender: TObject);
+    procedure AboutJVCL301Click(Sender: TObject);
+    procedure Scrolling1Click(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+    procedure ShowDataInListbox1Click(Sender: TObject);
+    procedure LargeDataset576samples1Click(Sender: TObject);
   private
   public
+      procedure NewValues;
   end;
 
 var
@@ -86,9 +109,15 @@ var
 
 implementation
 
+uses JvJVCLAboutForm, JVCLVer, JvDsgnConsts, // JVCL About box stuff
+     Math, ShellAPi; // NaN handling
+
+
+
+
 {$R *.dfm}
 
-procedure TJvChartDemoForm.ButtonNewValuesClick(Sender: TObject);
+procedure TJvChartDemoForm.NewValues;
 var
   I: Integer;
   nValueCount: Integer;
@@ -97,49 +126,111 @@ var
   dt,ds:Double;
   foo,foo1,foo2:Integer;
 begin
+  ListBox1.Clear;
+  Chart.Data.Clear;
+
   StatHgt := TStatArray.Create(10); // Initialize for rolling average of last 10 samples.
   StatHg0 := TStatArray.Create(10);
   try
 
   Randomize;
 
-  if Chart.Options.ChartKind = ckChartLine then begin
+  if LargeDataset576samples1.Checked  then begin
+
+     // A larger bogus data set for demonstration purposes.
      nValueCount := 576;  // 2.5 minute sample period, 576 samples = 1 day.
-     foo := 20; // The allmighty foo factor.
+     foo := 20; // Used in generating our bogus data below, not really important.
+     Chart.Options.XAxisValuesPerDivision := 24; // 24 samples * 150 seconds = 1 hour time divisions (     Chart.Options.XAxisValuesPerDivision := 4;
+     Chart.Options.XAxisDateTimeMode := true;  // Use datetime timestamp labels, just Fer Instance.
+
   end else begin
+
+     // A smaller bogus data set for demonstration purposes.
      nValueCount := 20; // Other chart types don't deal well with 576 samples.
-     foo := 1;  // A non foo factor (aka 'bar')
+     foo := 1;  // Used in generating our bogus data below, not really important.
+     Chart.Options.XAxisValuesPerDivision := 4; // five divisions, 4 values per division
+     Chart.Options.XAxisDateTimeMode := false; // Use text labels, just Fer Instance.
+
+     // Print every fifth sample's X Axis Legend value, below
+     // the chart value (Set to XAxisLegendSkipBy 1 to print em all,
+     // which would get crowded awful quick)
+     Chart.Options.XAxisLegendSkipBy := 5;
   end;
-  Chart.Data.Clear;
-  Chart.ResetGraphModule;
+
+  //Chart.ResetGraphModule;    // Clears YMax.
   dt := Trunc(now- 1.0); // yesterday, midnight.
-  foo1 := Random(5)+2;
-  foo2 := Random(3)+5;
+  foo1 := Random(5)+2;   // more randomness
+  foo2 := Random(3)+5;   // more randomness
   for I := 0 to nValueCount - 1 do
   begin
     if i > 0 then
-    begin
+    begin  // generate random data that appears to show a sawtooth-frequency-pattern plus a lot of random noise:
       hgt := Abs(Random(80)+(Random(((I div foo) mod foo1) * 250) * 5 + 9500));
       hg0 := Abs(Random(280)+Random(((I div foo) mod foo2) * 650)*5 + 1003);
     end
     else
     begin
-      hgt := 7000; // first element must be fixed for debug 0/1 offset purposes
+      hgt := 7000; // First sample always known value, helps me troubleshoot.
       hg0 := 1000;
     end;
     StatHgt.AddValue(hgt);
     StatHg0.AddValue(hg0);
+
+
+    // PAY ATTENTION HERE, this is where we set the Chart.Data.Value[ index,pen] := <values>
+    // stuff which is the MOST IMPORTANT THING, unless you like blank charts:
+
     // Set Data.Value[Pen, Series] := dataValue ...
     Chart.Data.Value[0, I] := StatHgt.Average /1000;
-    Chart.Data.Value[1, I] := StatHg0.Average/1000;
+
+    // Test blanks on big chart, show missing data:
+     Chart.Data.Value[1, I] := StatHg0.Average/1000;
+
     hg2p := (StatHgt.Average - StatHg0.Average)/1000;
     if hg2p < 0.0 then
         hg2p := 0.0;
     Chart.Data.Value[2, I] := hg2p;
     ds := dt + (i / 576);
-    Chart.Options.XLegends.Add(FormatDateTime('yyyy-mm-dd', dt) );
-    Chart.Data.Timestamp[I] := dt;
+
+    // There are TWO ways to get an X Axis label plotted:
+    if Chart.Options.XAxisDateTimeMode then
+         Chart.Data.Timestamp[I] := ds // X legends generated by timestamps
+    else
+        // X Legends generated by user are used by default.
+        // This would be redundant, and would be a waste of memory
+        // if Chart.Options.XAxisDateTimeMode was also set.
+        Chart.Options.XLegends.Add(FormatDateTime('hh:nn:ss', ds) );
+
+
+
+    { How to make a gap in the data! }
+    if (nValueCount>100 ) and ShowgapinLineChart1.Checked then begin
+      if (I > 100) and (I < 130) then begin
+         Chart.Data.Value[0, I] := NaN; // Use special Math.NaN const
+         Chart.Data.Value[1, I] := NaN; // Use special Math.NaN const
+         Chart.Data.Value[2, I] := NaN; // Use special Math.NaN const
+      end;
+    end;
+
+    // Just so that the last two values are visibly different, to make sure
+    // the chart handles the final vaulues correctly, we set all the final
+    // values to known amounts also:
+    if (I=nValueCount-2) then begin
+         Chart.Data.Value[0, I] := 6.0; // Use special Math.NaN const
+         Chart.Data.Value[1, I] := 4.0; // Use special Math.NaN const
+         Chart.Data.Value[2, I] := 2.0; // Use special Math.NaN const
+    end else if (I=nValueCount-1) then begin
+         Chart.Data.Value[0, I] := 3.0; // Use special Math.NaN const
+         Chart.Data.Value[1, I] := 2.0; // Use special Math.NaN const
+         Chart.Data.Value[2, I] := 1.0; // Use special Math.NaN const
+
+    end;
+
+    ListBox1.Items.Append( Chart.Data.DebugStr(I) );
   end;
+
+
+
   with Chart.Options do
   begin
     Title := 'Chart Title';
@@ -171,46 +262,56 @@ begin
   end;
 end;
 
-procedure TJvChartDemoForm.SpeedButton1Click(Sender: TObject);
-begin
-  Chart.GraphToClipboard;
-end;
-
 procedure TJvChartDemoForm.FormResize(Sender: TObject);
 begin
   if Assigned(Chart) then
     Chart.ResizeChartCanvas;
 end;
 
-procedure TJvChartDemoForm.SpeedButton2Click(Sender: TObject);
-begin
-  Chart.PrintGraph;
-end;
-
-procedure TJvChartDemoForm.SpeedButton3Click(Sender: TObject);
+procedure TJvChartDemoForm.ButtonBarChartClick(Sender: TObject);
 begin
   Chart.Options.ChartKind := ckChartBar;
-  ButtonNewValuesClick(Sender);  
+  NewValues;
   //Chart.PlotGraph;
 end;
 
-procedure TJvChartDemoForm.SpeedButton4Click(Sender: TObject);
+procedure TJvChartDemoForm.ButtonLineClick(Sender: TObject);
+var
+ I:Integer;
 begin
-{   Chart.ShowAsLine; this show it without marks}
-  Chart.Options.ChartKind := ckChartLineWithMarkers;
-  ButtonNewValuesClick(Sender);
+  Chart.Options.ChartKind := ckChartLine;
+  for I := 0 to Chart.Options.PenCount-1 do begin
+     Chart.Options.PenMarkerKind[I] := pmkNone;
+  end;
+  
+  NewValues;
 end;
 
-procedure TJvChartDemoForm.SpeedButton5Click(Sender: TObject);
+
+procedure TJvChartDemoForm.ButtonLineMarkerClick(Sender: TObject);
+var
+  I:Integer;
+begin
+  Chart.Options.ChartKind := ckChartLine;
+  Chart.Options.PenMarkerKind[0] := pmkDiamond; // demonstrate both Diamond and Circle Marks.
+  Chart.Options.PenMarkerKind[1] := pmkDiamond;
+  Chart.Options.PenMarkerKind[2] := pmkCircle;
+
+  NewValues;
+
+end;
+
+
+procedure TJvChartDemoForm.ButtonStackedBarAveClick(Sender: TObject);
 begin
   Chart.Options.ChartKind := ckChartStackedBarAverage;
-  ButtonNewValuesClick(Sender);
+  NewValues;
 end;
 
-procedure TJvChartDemoForm.SpeedButton6Click(Sender: TObject);
+procedure TJvChartDemoForm.ButtonStackedBarClick(Sender: TObject);
 begin
   Chart.Options.ChartKind := ckChartStackedBar;
-  ButtonNewValuesClick(Sender);
+  NewValues;
 end;
 
 procedure TJvChartDemoForm.SpeedButton7Click(Sender: TObject);
@@ -218,19 +319,19 @@ begin
 //  Chart.PivotData; // TODO: This causes exceptions. not sure why we want this.
 end;
 
-procedure TJvChartDemoForm.SpeedButton8Click(Sender: TObject);
+procedure TJvChartDemoForm.ButtonBarAveClick(Sender: TObject);
 begin
   //Chart.ShowAsBarWithAve;
   Chart.Options.ChartKind := ckChartBarAverage;
 end;
 
-procedure TJvChartDemoForm.SpeedButton10Click(Sender: TObject);
+procedure TJvChartDemoForm.ButtonPieClick(Sender: TObject);
 begin
   // Chart.ShowAsPie;
   Chart.Options.ChartKind := ckChartPieChart;
 end;
 
-procedure TJvChartDemoForm.SpeedButton12Click(Sender: TObject);
+procedure TJvChartDemoForm.ButtonMarkersClick(Sender: TObject);
 begin
   //Chart.ShowAsMark;
   Chart.Options.ChartKind := ckChartMarkers;
@@ -249,7 +350,56 @@ begin
   Chart.PlotGraph;
 end;
 
-procedure TJvChartDemoForm.SpeedButton11Click(Sender: TObject);
+procedure TJvChartDemoForm.Button4Click(Sender: TObject);
+begin
+//   Chart.SetYMaxMin(10000,0);
+//  Chart.Options.YMax := 10000;
+//  Chart.Options.YMin := 10000;
+  Chart.AutoFormatGraph;
+//   Chart.PlotGraph;
+end;
+
+procedure TJvChartDemoForm.ButtonDeltaAverageClick(Sender: TObject);
+begin
+  Chart.Options.ChartKind := ckChartDeltaAverage;
+end;
+
+procedure TJvChartDemoForm.FormCreate(Sender: TObject);
+begin
+  if Assigned(Chart) then
+    //   Chart.ShowAsLineWithMark;
+    NewValues;
+end;
+
+procedure TJvChartDemoForm.Panel2DblClick(Sender: TObject);
+begin
+ ShellExecute( HWND(nil), 'show', 'http://homepages.borland.com/jedi/jvcl/', nil,nil,SW_SHOW);
+end;
+
+procedure TJvChartDemoForm.ShowgapinLineChart1Click(Sender: TObject);
+begin
+  ShowgapinLineChart1.Checked := not ShowgapinLineChart1.Checked;
+  ButtonLine.Down := true;
+  NewValues;
+
+end;
+
+procedure TJvChartDemoForm.Print1Click(Sender: TObject);
+begin
+  Chart.PrintGraph;
+end;
+
+procedure TJvChartDemoForm.Generatenewrandomvalues1Click(Sender: TObject);
+begin
+ NewValues;
+end;
+
+procedure TJvChartDemoForm.CopyToClipboard1Click(Sender: TObject);
+begin
+  Chart.GraphToClipboard;
+end;
+
+procedure TJvChartDemoForm.SetHeaderFont1Click(Sender: TObject);
 begin
    {Get the current font for the Header text...}
   FontDialog1.Font.Assign(Chart.Options.AxisFont);
@@ -259,32 +409,44 @@ begin
     Chart.Options.AxisFont := FontDialog1.Font;
   Chart.PlotGraph;
   Chart.Invalidate;
+
 end;
 
-procedure TJvChartDemoForm.Button4Click(Sender: TObject);
+procedure TJvChartDemoForm.About1Click(Sender: TObject);
 begin
-//   Chart.SetYMaxMin(10000,0);
-  Chart.Options.YMax := 10000;
-  Chart.Options.YMin := 10000;
-  Chart.AutoFormatGraph;
-//   Chart.PlotGraph;
+  Application.MessageBox( PChar(
+  'JvChart comes from AABSoft Graph written by  Mårten Henrichson, JVCL 3.0 '+
+  'version by Warren Postma.  '),'About JvChart', MB_OK);
 end;
 
-procedure TJvChartDemoForm.SpeedButton14Click(Sender: TObject);
+procedure TJvChartDemoForm.AboutJVCL301Click(Sender: TObject);
 begin
-  Chart.Options.ChartKind := ckChartDeltaAverage;
+  TJvJVCLAboutForm.Execute(False);
 end;
 
-procedure TJvChartDemoForm.FormCreate(Sender: TObject);
+procedure TJvChartDemoForm.Scrolling1Click(Sender: TObject);
 begin
-  if Assigned(Chart) then
-    //   Chart.ShowAsLineWithMark;
-    ButtonNewValuesClick(Sender);
+  Scrolling1.Checked := not Scrolling1.Checked;
+  Timer1.Enabled := Scrolling1.Checked;
 end;
 
-procedure TJvChartDemoForm.SpeedButton13Click(Sender: TObject);
+procedure TJvChartDemoForm.Timer1Timer(Sender: TObject);
 begin
-  Chart.Align := alClient;
+  Chart.Data.Scroll;
+  Chart.PlotGraph;
+
+end;
+
+procedure TJvChartDemoForm.ShowDataInListbox1Click(Sender: TObject);
+begin
+  ShowDataInListbox1.Checked := not ShowDataInListbox1.Checked;
+  ListBox1.Visible := ShowDataInListbox1.Checked;
+end;
+
+procedure TJvChartDemoForm.LargeDataset576samples1Click(Sender: TObject);
+begin
+   LargeDataset576samples1.Checked := not LargeDataset576samples1.Checked;
+   NewValues;
 end;
 
 end.
