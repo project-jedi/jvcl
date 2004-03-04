be751ca3541aff6acdf7685d044ccf0268aa17fa
asnepvangers <asnepvangers@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-03-04 23:19:32 +0000

    Added units

---
+++ jvcl/qrun/JvQScrollBar.pas
@@ -0,0 +1,99 @@
+{**************************************************************************************************}
+{  WARNING:  JEDI preprocessor generated unit. Manual modifications will be lost on next release.  }
+{**************************************************************************************************}
+
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/MPL-1.1.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: JvScrollBar.PAS, released on 2001-02-28.
+
+The Initial Developer of the Original Code is Sébastien Buysse [sbuysse@buypin.com]
+Portions created by Sébastien Buysse are Copyright (C) 2001 Sébastien Buysse.
+All Rights Reserved.
+
+Contributor(s): Michael Beck [mbeck@bigfoot.com].
+
+Last Modified: 2000-02-28
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
+located at http://jvcl.sourceforge.net
+
+Known Issues:
+-----------------------------------------------------------------------------}
+
+{$I jvcl.inc}
+
+unit JvQScrollBar;
+
+interface
+
+uses
+  SysUtils, Classes,
+  
+  
+  QGraphics, QControls, QForms, QStdCtrls, Types,
+  
+  JvQExStdCtrls;
+
+type
+  TJvScrollBar = class(TJvExScrollBar)
+  private
+    FHotTrack: Boolean;
+    procedure SetHotTrack(Value: Boolean);
+  protected
+    procedure MouseEnter(Control: TControl); override;
+    procedure MouseLeave(Control: TControl); override;
+  public
+    constructor Create(AOwner: TComponent); override;
+  published
+    property HotTrack: Boolean read FHotTrack write SetHotTrack default False;
+    property HintColor;
+    property OnMouseEnter;
+    property OnMouseLeave;
+    property OnParentColorChange;
+  end;
+
+implementation
+
+constructor TJvScrollBar.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+  FHotTrack := False;
+  // ControlStyle := ControlStyle + [csAcceptsControls];
+end;
+
+procedure TJvScrollBar.MouseEnter(Control: TControl);
+begin
+  if csDesigning in ComponentState then
+    Exit;
+  if not MouseOver then
+  begin
+    
+    inherited MouseEnter(Control);
+  end;
+end;
+
+procedure TJvScrollBar.MouseLeave(Control: TControl);
+begin
+  if MouseOver then
+  begin
+    
+    inherited MouseLeave(Control);
+  end;
+end;
+
+procedure TJvScrollBar.SetHotTrack(Value: Boolean);
+begin
+  FHotTrack := Value;
+    
+end;
+
+end.
+
