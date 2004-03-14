797feb5e2e6437878b69c8f29077a55fd545c4e5
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-03-14 21:27:43 +0000

    - Added Grouped, RollImages, HeaderHeight properties - Accepts controls dropped on it - Simplified default roll images (works with most color schemes) - Now updates when image list(s) changes - Demo updated

--- examples/JvXPControls/JvXPBar/MainFrm.pas
+++ examples/JvXPControls/JvXPBar/MainFrm.pas
@@ -5,13 +5,14 @@ interface
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms,
   Dialogs, JvXPCore, JvXPBar, JvXPContainer, ImgList, ActnList, ExtCtrls,
-  StdCtrls;
+  StdCtrls, ComCtrls;
 
 resourcestring
   SClickEvent =
     'You have clicked the action "%s"...';
 
 type
+  TProcControl = procedure (Control:TControl) of object;
   TfrmMain = class(TForm)
     acConnectAdministrator: TAction;
     acConnectLocalServer: TAction;
@@ -23,30 +24,48 @@ type
     acSettingsUsers: TAction;
     acSynchronizeUnknown: TAction;
     acSynchronizeWeb: TAction;
-    btnCollpaseAll: TButton;
+    btnCollapseAll: TButton;
     btnExpandAll: TButton;
     cntDetails: TJvXPContainer;
     cntWinXPBar: TJvXPContainer;
-    cntWinXPBarPanel1: TJvXPContainer;
-    dxContainer1: TJvXPContainer;
-    dxContainer2: TJvXPContainer;
-    dxWinXPBar1: TJvXPBar;
-    dxWinXPBar2: TJvXPBar;
-    dxWinXPBar3: TJvXPBar;
     imlWinXPBar: TImageList;
     lbWelcome: TLabel;
     sbxWinXPBar: TScrollBox;
     spltMain: TSplitter;
     btnToogleEnableMode: TButton;
     btnToggleVisibleMode: TButton;
+    ilOldButtons: TImageList;
+    chkGrouped: TCheckBox;
+    ilWhiteButtons: TImageList;
+    ilRedButtons: TImageList;
+    ilBlackButtons: TImageList;
+    acGettingStarted: TAction;
+    acHelp: TAction;
+    acHowDoI: TAction;
+    acCommonQuestions: TAction;
+    JvXPContainer1: TJvXPContainer;
+    dxWinXPBar4: TJvXPBar;
+    JvXPBar1: TJvXPBar;
+    dxWinXPBar3: TJvXPBar;
+    dxWinXPBar2: TJvXPBar;
+    dxWinXPBar1: TJvXPBar;
+    tvSelfView: TTreeView;
     procedure acConnectRemoteServerExecute(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure btnCollpaseAllClick(Sender: TObject);
+    procedure btnCollapseAllClick(Sender: TObject);
     procedure btnExpandAllClick(Sender: TObject);
     procedure btnToogleEnableModeClick(Sender: TObject);
     procedure btnToggleVisibleModeClick(Sender: TObject);
+    procedure chkGroupedClick(Sender: TObject);
   private
     { Private declarations }
+    procedure DoGrouped(Control:TControl);
+    procedure DoExpandAll(Control:TControl);
+    procedure DoCollapseAll(Control:TControl);
+    procedure DoEnableToggle(Control:TControl);
+    procedure DoVisibleToggle(Control:TControl);
+    procedure IterateControls(Proc:TProcControl);
+    procedure BuildStructure;
   public
     { Public declarations }
   end;
@@ -58,26 +77,11 @@ implementation
 
 {$R *.dfm}
 
-{-----------------------------------------------------------------------------
-  Procedure: TfrmMain.FormCreate
-  Author:    M. Hoffmann
-  Date:      06-Feb-2003
-  Arguments: Sender: TObject
-  Result:    None
------------------------------------------------------------------------------}
-
 procedure TfrmMain.FormCreate(Sender: TObject);
 begin
   cntDetails.Align := alClient;
+  BuildStructure;
 end;
-
-{-----------------------------------------------------------------------------
-  Procedure: TfrmMain.acConnectRemoteServerExecute
-  Author:    M. Hoffmann
-  Date:      06-Feb-2003
-  Arguments: Sender: TObject
-  Result:    None
------------------------------------------------------------------------------}
 
 procedure TfrmMain.acConnectRemoteServerExecute(Sender: TObject);
 begin
@@ -86,60 +90,98 @@ begin
       'Click', MB_ICONINFORMATION);
 end;
 
-{-----------------------------------------------------------------------------
-  Procedure: TfrmMain.btnCollpaseAllClick
-  Author:    M. Hoffmann
-  Date:      06-Feb-2003
-  Arguments: Sender: TObject
-  Result:    None
------------------------------------------------------------------------------}
-
-procedure TfrmMain.btnCollpaseAllClick(Sender: TObject);
+procedure TfrmMain.btnCollapseAllClick(Sender: TObject);
 begin
-  dxWinXPBar1.Collapsed := True;
-  dxWinXPBar2.Collapsed := True;
-  dxWinXPBar3.Collapsed := True;
+  IterateControls(DoCollapseAll);
 end;
 
-{-----------------------------------------------------------------------------
-  Procedure: TfrmMain.btnExpandAllClick
-  Author:    M. Hoffmann
-  Date:      06-Feb-2003
-  Arguments: Sender: TObject
-  Result:    None
------------------------------------------------------------------------------}
 
 procedure TfrmMain.btnExpandAllClick(Sender: TObject);
 begin
-  dxWinXPBar1.Collapsed := False;
-  dxWinXPBar2.Collapsed := False;
-  dxWinXPBar3.Collapsed := False;
+  IterateControls(DoExpandAll);
 end;
-
-{-----------------------------------------------------------------------------
-  Procedure: TfrmMain.btnToogleEnableModeClick
-  Author:    M. Hoffmann
-  Date:      06-Feb-2003
-  Arguments: Sender: TObject
-  Result:    None
------------------------------------------------------------------------------}
 
 procedure TfrmMain.btnToogleEnableModeClick(Sender: TObject);
 begin
-  dxWinXPBar1.Items[0].Enabled := not dxWinXPBar1.Items[0].Enabled;
+  IterateControls(DoEnableToggle);
 end;
-
-{-----------------------------------------------------------------------------
-  Procedure: TfrmMain.btnToggleVisibleModeClick
-  Author:    M. Hoffmann
-  Date:      06-Feb-2003
-  Arguments: Sender: TObject
-  Result:    None
------------------------------------------------------------------------------}
 
 procedure TfrmMain.btnToggleVisibleModeClick(Sender: TObject);
 begin
-  dxWinXPBar2.Items[1].Visible := not dxWinXPBar2.Items[1].Visible;
+  IterateControls(DoVisibleToggle);
+end;
+
+procedure TfrmMain.chkGroupedClick(Sender: TObject);
+begin
+  IterateControls(DoGrouped);
+  btnExpandAll.Enabled := not chkGrouped.Checked;
+end;
+
+procedure TfrmMain.DoGrouped(Control: TControl);
+begin
+  if Control is TJvXPBar then
+    TJvXPBar(Control).Grouped := chkGrouped.Checked;
+end;
+
+procedure TfrmMain.DoExpandAll(Control: TControl);
+begin
+  if Control is TJvXPBar then
+    TJvXPBar(COntrol).Collapsed := false;
+end;
+
+procedure TfrmMain.IterateControls(Proc: TProcControl);
+var i:integer;
+begin
+  for i := 0 to ComponentCount - 1 do
+    if Components[i] is TControl then
+      Proc(TControl(Components[i]));
+end;
+
+procedure TfrmMain.DoCollapseAll(Control: TControl);
+begin
+  if Control is TJvXPBar then
+    TJvXPBar(COntrol).Collapsed := true;
+end;
+
+procedure TfrmMain.DoEnableToggle(Control: TControl);
+var i:integer;
+begin
+  if Control is TJvXPBar then
+    for i := 0 to TJvXPBar(Control).Items.Count - 1 do
+      if Odd(i) then
+        TJvXPBar(Control).Items[i].Enabled := not TJvXPBar(Control).Items[i].Enabled;
+end;
+
+procedure TfrmMain.DoVisibleToggle(Control: TControl);
+var i:integer;
+begin
+  if Control is TJvXPBar then
+    for i := 0 to TJvXPBar(Control).Items.Count - 1 do
+      if Odd(i) then
+        TJvXPBar(Control).Items[i].Visible := not TJvXPBar(Control).Items[i].Visible;
+end;
+
+procedure TfrmMain.BuildStructure;
+var
+  i,j:integer;
+  Parent, Child:TTreeNode;
+  ABar:TJvXPBar;
+begin
+  tvSelfView.Items.Clear;
+  for i := ComponentCount - 1 downto 0 do
+    if Components[i] is TJvXPBar then
+    begin
+      ABar := TJvXPBar(Components[i]);
+      Parent := tvSelfView.Items.AddChild(nil,ABar.Caption);
+      if ABar.ControlCount = 0 then
+      for j := 0 to ABar.Items.Count - 1 do
+      begin
+        Child := tvSelfView.Items.AddChild(Parent,ABar.Items[j].Caption);
+        Child.ImageIndex := ABar.Items[j].ImageIndex;
+        Child.SelectedIndex := Child.ImageIndex;
+      end;
+    end;
+  tvSelfView.FullExpand;
 end;
 
 end.
