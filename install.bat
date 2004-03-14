84dbf83337561e6d57be1458267d9a163631995b
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-03-14 02:37:05 +0000

    Now tries to build using make.exe if the direct call to dcc32.exe fails

--- install.bat
+++ install.bat
@@ -5,7 +5,13 @@ REM if %JCLDIR%!==! set JCLDIR=..\JCL
 
 cd install\JVCL3Install
 REM dcc32 -$O+ -Q JVCL3Install.dpr -E..\..\packages -U%JCLDIR%\source\common -U%JCLDIR%\source\windows -I%JCLDIR%\source\common 
-dcc32 -$O+ -Q JVCL3Install.dpr -E..\..\packages -I..\..\common
+dcc32 -$O+ -Q JVCL3Install.dpr -E..\..\packages -I..\..\common -U"..\..\run;..\..\common"
+if ERRORLEVEL 1 goto trywithmake
+goto tryrunning
+:trywithmake
+make -fJVCL3Install.mak JVCL3Install.exe
+
+:tryrunning
 cd ..\..
 
 if NOT EXIST packages\JVCL3Install.exe goto error
