f7d33c8eae6912b4ce4f3d45648b5a0006a54ba9
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-03-20 05:43:49 +0000

    New editor for the DefaultGrabber property

--- design/JvUrlListGrabberEditors.pas
+++ design/JvUrlListGrabberEditors.pas
@@ -16,7 +16,7 @@ All Rights Reserved.
 
 Contributor(s): -
 
-Last Modified: 2003-11-02
+Last Modified: 2004-03-20
 
 You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
 located at http://jvcl.sourceforge.net
@@ -55,11 +55,23 @@ type
     function GetName: string; override;
   end;
 
+  TJvUrlGrabberIndexProperty = class (TIntegerProperty)
+  public
+    function GetAttributes: TPropertyAttributes; override;
+    procedure GetValues(Proc: TGetStrProc); override;
+    function GetValue: string; override;
+    procedure SetValue(const Value: string); override;
+  end;
+
 implementation
 
 uses
   TypInfo,
-  JvUrlListGrabber, JvUrlGrabbers;
+  JvUrlListGrabber,
+  SysUtils;
+
+const
+  None: string = '(none)';
 
 //=== TJvUrlGrabberDefaultPropertiesListEditor ===============================
 
@@ -113,6 +125,61 @@ begin
   // get Supported URL name from the real default properties
   EditorTrick := TJvUrlGrabberDefPropEdTrick(GetComponent(0));
   Result := EditorTrick.DefaultProperties.SupportedURLName;
+end;
+
+{ TJvUrlGrabberIndexProperty }
+
+function TJvUrlGrabberIndexProperty.GetAttributes: TPropertyAttributes;
+begin
+  Result := [paValueList, paMultiSelect, paRevertable];
+end;
+
+function TJvUrlGrabberIndexProperty.GetValue: string;
+var
+  Index: Integer;
+begin
+  Index := GetOrdValue;
+  if (Index < 0) or (Index >= JvUrlGrabberClassList.Count) then
+    Result := '-1 - ' + None
+  else
+    Result := IntToStr(Index) + ' - ' + JvUrlGrabberClassList[Index].GetSupportedUrlName;
+end;
+
+procedure TJvUrlGrabberIndexProperty.GetValues(Proc: TGetStrProc);
+var
+  I: Integer;
+begin
+  Proc('-1 - ' + None);
+  for I := 0 to JvUrlGrabberClassList.Count - 1 do
+    Proc(IntToStr(I) + ' - ' + JvUrlGrabberClassList[I].GetSupportedUrlName);
+end;
+
+procedure TJvUrlGrabberIndexProperty.SetValue(const Value: string);
+var
+  I: Integer;
+  OrdValue : Integer;
+  CleanValue: string;
+begin
+  // The value may be a simple text or may contain a number in front
+  // of it. So we clean it to only contain the text
+  CleanValue := Value;
+  if Pos('-', Value) > 0 then
+    CleanValue := Trim(Copy(Value, Pos('-', Value)+1, Length(Value)));
+
+  if CleanValue = None then
+    SetOrdValue(-1)
+  else
+  begin
+    I := 0;
+    OrdValue := -1;
+    while (I < JvUrlGrabberClassList.Count) and (OrdValue = -1) do
+    begin
+      if JvUrlGrabberClassList[I].GetSupportedURLName = CleanValue then
+        OrdValue := I;
+      Inc(I);
+    end;
+    SetOrdValue(OrdValue);
+  end;
 end;
 
 end.
