f17ef06698b26c0b28aedbc0250980d79d44adf3
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-03-26 11:03:36 +0000

    added makefiles in packages, tmp.bat and Make*.@@@ files.

--- clean.bat
+++ clean.bat
@@ -8,5 +8,8 @@ del lib\d6\*.dcp lib\d6\*.bpl >NUL
 del lib\d7\*.dcp lib\d7\*.bpl >NUL
 del lib\c5\obj\*.obj lib\c5\obj\*.dcu >NUL
 del lib\c6\obj\*.obj lib\c6\obj\*.dcu >NUL
+del packages\*Packages.mak packages\tmp.bat >/NUL
+del /S packages\Make*.@@@ >NUL
+
 : needs delete lines for k3
 :LEAVE
