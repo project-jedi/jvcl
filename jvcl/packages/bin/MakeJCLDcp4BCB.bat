22011a2c43f9735187e3bab2cf9f159ac7b565e8
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-03-29 10:03:57 +0000

    Batch file issue resolved by replacing build.bat with buildtarget.exe, that is no called build.exe

--- jvcl/packages/bin/MakeJCLDcp4BCB.bat
+++ jvcl/packages/bin/MakeJCLDcp4BCB.bat
@@ -4,7 +4,7 @@ if !%JCLROOT%==! SET JCLROOT=..\..\..\jcl
 del "%JCLROOT%\packages\error.log" 2>NUL
 
 echo ################################ BCB 5 #########################################
-buildtarget.exe c5 --make=-s --make=BuildJCLdcpFilesForce %1 %2 %3 %4 %5 %6 %7 %8 %9
+build.exe c5 --make=BuildJCLdcpFilesForce "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
 if NOT EXIST "%JCLROOT%\packages\error.log" goto NextC6
 type "%JCLROOT%\packages\error.log"
 del "%JCLROOT%\packages\error.log"
@@ -12,7 +12,7 @@ del "%JCLROOT%\packages\error.log"
 :NextC6
 echo.
 echo ################################ BCB 6 #########################################
-buildtarget.exe c6 --make=-s --make=BuildJCLdcpFilesForce %1 %2 %2 %3 %4 %5 %6 %7 %8 %9
+build.exe c6 --make=BuildJCLdcpFilesForce "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
 if NOT EXIST "%JCLROOT%\packages\error.log" goto END
 type "%JCLROOT%\packages\error.log"
 del "%JCLROOT%\packages\error.log"
--- jvcl/packages/bin/build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-if EXIST xml   cd bin
-buildtarget.exe "--make=-s" %1 %2 %3 %4 %5 %6 %7 %8 %9
