7104f714e73c749ca7730385e80446e965bca0c5
asnepvangers <asnepvangers@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-04-04 20:50:46 +0000

    added

---
+++ jvcl/qdesign/JvQScheduleEditors.pas
@@ -0,0 +1,109 @@
+{**************************************************************************************************}
+{  WARNING:  JEDI preprocessor generated unit. Manual modifications will be lost on next release.  }
+{**************************************************************************************************}
+
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/MPL-1.1.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: JvScheduleEditors.PAS, released on 2002-07-04.
+
+The Initial Developers of the Original Code are:
+
+Last Modified: 
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
+located at http://jvcl.sourceforge.net
+
+Known Issues:
+-----------------------------------------------------------------------------}
+
+{$I jvcl.inc}
+
+unit JvQScheduleEditors;
+
+interface
+
+uses
+  SysUtils, Classes,
+  
+  DesignIntf, DesignEditors,
+  
+  JvQScheduleEditorForm, JvQScheduledEvents;
+
+type
+  TJvSchedulePropertyEditor = class(TPropertyEditor)
+  public
+    procedure Edit; override;
+    function GetAttributes: TPropertyAttributes; override;
+    function GetValue: string; override;
+  end;
+
+  TJvSchedEventComponentEditor = class(TComponentEditor)
+  public
+    procedure ExecuteVerb(Index: Integer); override;
+    function GetVerb(Index: Integer): string; override;
+    function GetVerbCount: Integer; override;
+  end;
+
+implementation
+
+uses
+  ColnEdit,
+  
+  
+  QControls,
+  
+  JclSchedule,
+  JvQConsts, JvQDsgnConsts;
+
+
+//=== TJvSchedulePropertyEditor ==============================================
+
+procedure TJvSchedulePropertyEditor.Edit;
+begin
+  with TfrmScheduleEditor.Create(nil) do
+  try
+    Schedule := IJclSchedule({TEventSchedule(} GetOrdValue) {.Schedule};
+    if ShowModal = mrOk then
+      Self.Modified;
+  finally
+    Free;
+  end;
+end;
+
+function TJvSchedulePropertyEditor.GetAttributes: TPropertyAttributes;
+begin
+  Result := [paDialog, paReadOnly];
+end;
+
+function TJvSchedulePropertyEditor.GetValue: string;
+begin
+  Result := '(IJclSchedule)';
+end;
+
+//=== TJvSchedEventComponentEditor ===========================================
+
+procedure TJvSchedEventComponentEditor.ExecuteVerb(Index: Integer);
+begin
+  ShowCollectionEditorClass(Designer, TCollectionEditor, Component,
+    TJvCustomScheduledEvents(Component).Events, 'Events');
+end;
+
+function TJvSchedEventComponentEditor.GetVerb(Index: Integer): string;
+begin
+  Result := RsEventEditor;
+end;
+
+function TJvSchedEventComponentEditor.GetVerbCount: Integer;
+begin
+  Result := 1;
+end;
+
+end.
---
+++ jvcl/qdesign/JvQTransparentButtonEditors.pas
@@ -0,0 +1,74 @@
+{**************************************************************************************************}
+{  WARNING:  JEDI preprocessor generated unit. Manual modifications will be lost on next release.  }
+{**************************************************************************************************}
+
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/MPL-1.1.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: JvTransparentButtonEditors.PAS, released on 2002-05-26.
+
+The Initial Developer of the Original Code is John Doe.
+Portions created by John Doe are Copyright (C) 2003 John Doe.
+All Rights Reserved.
+
+Contributor(s):
+
+Last Modified: 2003-11-09
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
+located at http://jvcl.sourceforge.net
+
+Known Issues:
+-----------------------------------------------------------------------------}
+
+{$I jvcl.inc}
+
+unit JvQTransparentButtonEditors;
+
+interface
+
+uses
+  
+  DesignIntf, DesignEditors, DesignMenus, VCLEditors,
+  
+  ImgList,
+  JvQDsgnEditors;
+
+
+type
+  TJvTBImagesProperty = class(TJvDefaultImageIndexProperty)
+  protected
+    function ImageList: TCustomImageList; override;
+  end;
+
+implementation
+
+uses
+  SysUtils,
+  JvQTransparentButton;
+
+function TJvTBImagesProperty.ImageList: TCustomImageList;
+begin
+  if AnsiSameText(GetName,'ActiveIndex') then
+    Result := (GetComponent(0) as TJvTransparentButton2).ActiveImage
+  else
+  if AnsiSameText(GetName,'DisabledIndex') then
+    Result := (GetComponent(0) as TJvTransparentButton2).DisabledImage
+  else
+  if AnsiSameText(GetName,'DownIndex') then
+    Result := (GetComponent(0) as TJvTransparentButton2).DownImage
+  else
+  if AnsiSameText(GetName,'GrayIndex') then
+    Result := (GetComponent(0) as TJvTransparentButton2).GrayImage
+  else
+    Result := nil;
+end;
+
+end.
