bd5add8c1f2c9433f1f757145a6d56184f15f9d7
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-04-06 22:08:16 +0000

    JvTabControl is no longer in the repository

--- jvcl/design/JvStdCtrlsReg.pas
+++ jvcl/design/JvStdCtrlsReg.pas
@@ -84,7 +84,7 @@ begin
     TJvSpinEdit, TJvIPAddress]);
   RegisterComponents(RsPaletteImageAnimator, [TJvImage, TJvImageList]);
   RegisterComponents(RsPaletteBarPanel, [
-    TJvPageControl,  TJvTabControl, TJvTabDefaultPainter, 
+    TJvPageControl,  
     TJvProgressBar, TJvStatusBar, TJvToolBar, TJvControlBar, TJvCoolBar,
     TJvGroupBox, TJvHeaderControl, TJvPanel, TJvBevel,
     TJvSecretPanel {TJvTransparentPanel}]);
