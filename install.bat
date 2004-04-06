faba9c0db6d5899f264f17307d3231ee9b72949d
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-04-06 16:46:56 +0000

    New installer

--- install.bat
+++ install.bat
@@ -1,29 +1,4 @@
 @echo off
-
-set JCLDIR=%1
-REM if %JCLDIR%!==! set JCLDIR=..\JCL
-
-cd install\JVCL3Install
-REM dcc32 -$O+ -Q JVCL3Install.dpr -E..\..\packages -U%JCLDIR%\source\common -U%JCLDIR%\source\windows -I%JCLDIR%\source\common 
-dcc32 -$O+ -Q JVCL3Install.dpr -E..\..\packages -I..\..\common -U"..\..\run;..\..\common"
-if ERRORLEVEL 1 goto trywithmake
-goto tryrunning
-:trywithmake
-make -fJVCL3Install.mak JVCL3Install.exe
-
-:tryrunning
+cd packages\bin
+build.exe newest "--make=installer" "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
 cd ..\..
-
-if NOT EXIST packages\JVCL3Install.exe goto error
-start packages\JVCL3Install.exe
-
-goto end
-
-:error
-echo.
-echo Error: JVCL 3 Package Installer could not be compiled.
-echo.
-pause
-
-:end
-SET JCLDIR=
