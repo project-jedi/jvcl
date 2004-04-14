32611b4653f3f67acffcfb6bef4ff0017bf1a823
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-04-14 10:59:23 +0000

    Now shows a more meaningful help message

--- jvcl/packages/bin/MakeJCLDcp4BCB.bat
+++ jvcl/packages/bin/MakeJCLDcp4BCB.bat
@@ -1,3 +1,24 @@
 @echo off
 
-build.exe --make=BuildJCLdcpFilesForce "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
+if %1!==! goto help
+
+build.exe --make=BuildJCLdcpFilesForce %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto end
+:help
+echo MakeJCLDcp4BCB.bat - Builds the JCL dcp files for BCB
+echo.
+echo Usage:    MakeJCLDcp4BCB Version [--jcl-path=JCLDirectory]
+echo                          [--bpl-path=DCPDirectory]
+echo.
+echo     Version           The version of BCB to build for (c5 or c6)
+echo     JCLDirectory      The place where the JCL is installed.
+echo                       Defaults to ..\..\JCL
+echo                       You must enclose the value AND the option
+echo                       name in double quotes (")
+echo     DCPDirectory      The place where to put the DCP file.
+echo                       Defaults to $(BCB)\Projects\Bpl
+echo                       You must enclose the value AND the option
+echo                       name in double quotes (")
+echo.
+
+:end
