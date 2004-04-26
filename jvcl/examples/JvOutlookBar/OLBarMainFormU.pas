8d78245208d08972a2fcf1ec6a3bd5091cb63d33
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-04-26 09:10:17 +0000

    - Updated

--- jvcl/examples/JvOutlookBar/OLBarMainFormU.pas
+++ jvcl/examples/JvOutlookBar/OLBarMainFormU.pas
@@ -33,7 +33,7 @@ interface
 uses
   Windows, Messages, SysUtils, {$IFDEF DELPHI6_UP}Variants, {$ENDIF}Classes, Graphics, Controls, Forms,
   Dialogs, JvOutlookBar, ComCtrls, StdCtrls, Menus, ExtCtrls, ImgList,
-  JvComponent;
+  JvComponent, JvExControls, ActnList;
 
 type
   TOLBarMainForm = class(TForm)
@@ -41,31 +41,44 @@ type
     Splitter1: TSplitter;
     ImageList1: TImageList;
     ImageList2: TImageList;
-    Image1: TImage;
-    Image2: TImage;
     Defaultpopupmenu1: TMenuItem;
     popButton: TPopupMenu;
     popPage: TPopupMenu;
-    ImageList3: TImageList;
     Editbuttoncaption1: TMenuItem;
     Editpagecaption1: TMenuItem;
     StatusBar1: TStatusBar;
+    JvOutlookBar1: TJvOutlookBar;
+    Panel1: TPanel;
+    Panel2: TPanel;
     Button1: TButton;
+    chkSmallImages: TCheckBox;
     Button2: TButton;
     Button3: TButton;
-    procedure FormCreate(Sender: TObject);
-    procedure Editpagecaption1Click(Sender: TObject);
-    procedure Editbuttoncaption1Click(Sender: TObject);
+    chkButtonFont: TCheckBox;
+    RichEdit1: TRichEdit;
+    Smallbuttons1: TMenuItem;
+    Smallbuttons2: TMenuItem;
+    ActionList1: TActionList;
+    acSmallButtons: TAction;
+    acEditButtonCaption: TAction;
+    acEditPageCaption: TAction;
+    chkFlat: TCheckBox;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
+    procedure JvOutlookBar1ButtonClick(Sender: TObject; Index: Integer);
+    procedure JvOutlookBar1PageChanging(Sender: TObject; Index: Integer;
+      var AllowChange: Boolean);
+    procedure JvOutlookBar1PageChange(Sender: TObject; Index: Integer);
+    procedure JvOutlookBar1ContextPopup(Sender: TObject; MousePos: TPoint;
+      var Handled: Boolean);
+    procedure acSmallButtonsExecute(Sender: TObject);
+    procedure acEditButtonCaptionExecute(Sender: TObject);
+    procedure acEditPageCaptionExecute(Sender: TObject);
+    procedure chkFlatClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
-    OL:TJvOutlookBar;
-    procedure DoButtonClick(Sender:TObject; Index:integer);
-    procedure DoPageChange(Sender: TObject; Index: integer);
-    procedure DoPageChanging(Sender: TObject; Index: integer; var AllowChange: boolean);
-    procedure OLContextPopUp(Sender: TObject; MousePos: TPoint; var Handled: Boolean);
   end;
 
 var
@@ -75,209 +88,111 @@ implementation
 
 {$R *.dfm}
 
-procedure TOLBarMainForm.DoPageChange(Sender:TObject;Index:integer);
-begin
-  if Index > -1 then
-    Caption := Format('Page changed to "%s"',[OL.Pages[Index].Caption]);
-end;
-
-procedure TOLBarMainForm.DoPageChanging(Sender:TObject;Index:integer;var AllowChange:boolean);
-begin
-  if (OL.ActivePageIndex > -1) and (Index > -1) then
-  begin
-    Caption := Format('Page changing from "%s" to "%s"',[OL.Pages[OL.ActivePageIndex].Caption,OL.Pages[Index].Caption]);
-//    sleep(200);
-  end;
-end;
-
-procedure TOLBarMainForm.DoButtonClick(Sender:TObject; Index:integer);
-var P:TJvOutlookBarPage;
-begin
-  if (Index > -1) then
-  begin
-    P := OL.Pages[OL.ActivePageIndex];
-    Caption := Format('Clicked button "%s" on page "%s"',[P.Buttons[Index].Caption,P.Caption]);
-  end;
-end;
-
-procedure TOLBarMainForm.OLContextPopUp(Sender: TObject; MousePos: TPoint; var Handled: Boolean);
-begin
-  if OL.PopupObject is TJvOutlookBarPage then
-    OL.PopUpMenu := popPage
-  else if OL.PopupObject is TJvOutlookBarButton then
-    OL.PopUpMenu := popButton
-  else
-    OL.PopUpMenu := popOL;
-end;
-
-procedure TOLBarMainForm.FormCreate(Sender: TObject);
-begin
-  OL := TJvOutlookBar.Create(self);
-  OL.Constraints.MaxWidth := 300;
-  OL.Constraints.MinWidth := 150;
-  OL.OnPageChange := DoPageChange;
-  OL.OnPageChanging := DoPageChanging;
-  OL.OnButtonClick  := DoButtonClick;
-  OL.OnContextPopUp := OLContextPopUp;
-  OL.BorderStyle := bsSingle;
-  OL.Align := alLeft;
-//  OL.Anchors := [akTop,akLeft,akRight];
-
-  OL.Parent := self;
-//  OL.BorderStyle := bsNone; // placing this to early (i.e right after .Create above) has no effect ???
-  OL.LargeImages := ImageList1;
-  OL.SmallImages := ImageList2;
-  OL.Font.Name := 'Verdana';
-  OL.Font.Size := 10;
-  OL.ButtonSize := olbsLarge;
-  OL.Color := clGray;
-  OL.PopUpMenu := popOL;
-  with OL.Pages.Add do
-  begin
-    Image := Image2.Picture.Bitmap;
-    Caption := 'Page 0';
-    with Buttons.Add do
-    begin
-      Caption := 'Button 1';
-      ImageIndex := 0;
-    end;
-    with Buttons.Add do
-    begin
-      Caption := 'Button 2';
-      ImageIndex := 1;
-    end;
-    with Buttons.Add do
-    begin
-      Caption := 'Button 3';
-      ImageIndex := 2;
-    end;
-    with Buttons.Add do
-    begin
-      Caption := 'Button 4';
-      ImageIndex := 3;
-    end;
-    Color := clRed;
-  end;
-  with OL.Pages.Add do
-  begin
-    Caption := 'Page 1';
-    // no buttons
-  end;
-
-  with OL.Pages.Add do
-  begin
-    Image := Image1.Picture.Bitmap;
-    Caption := 'Page 2';
-    with Buttons.Add do
-      Caption := 'Button 4';
-    with Buttons.Add do
-      Caption := 'Button 3';
-    with Buttons.Add do
-      Caption := 'Button 2';
-    with Buttons.Add do
-      Caption := 'Button 1';
-//    Color := clBlue;
-  end;
-  with OL.Pages.Add do
-  begin
-    ButtonSize := olbsSmall;
-    Caption := 'Page 3';
-//    Color := clBtnFace;
-    with Buttons.Add do
-      Caption := 'Button 1';
-    with Buttons.Add do
-      Caption := 'Button 2';
-    with Buttons.Add do
-      Caption := 'Button 3';
-    with Buttons.Add do
-      Caption := 'Button 4';
-    with Buttons.Add do
-      Caption := 'Button 5';
-    with Buttons.Add do
-      Caption := 'Button 6';
-    with Buttons.Add do
-      Caption := 'Button 7';
-    with Buttons.Add do
-      Caption := 'Button 8';
-    with Buttons.Add do
-      Caption := 'Button 9';
-    with Buttons.Add do
-      Caption := 'Button 10';
-    with Buttons.Add do
-      Caption := 'Button 11';
-    with Buttons.Add do
-      Caption := 'Button 12';
-    with Buttons.Add do
-      Caption := 'Button 13';
-    with Buttons.Add do
-      Caption := 'Button 14';
-    with Buttons.Add do
-      Caption := 'Button 15';
-    with Buttons.Add do
-      Caption := 'Button 16';
-    with Buttons.Add do
-      Caption := 'Button 17';
-    with Buttons.Add do
-      Caption := 'Button 18';
-    with Buttons.Add do
-      Caption := 'Button 19';
-    with Buttons.Add do
-      Caption := 'Button 20';
-  end;
-  with OL.Pages.Add do
-  begin
-    Caption := 'Page 4';
-    Color := clBtnFace;
-    Font.Color := clBlack;
-    with Buttons.Add do
-      Caption := 'Button 1';
-    with Buttons.Add do
-      Caption := 'Button 2';
-  end;
-  Splitter1.Align := alLeft;
-  OL.ActivePageIndex := 2;
-  OL.Pages[2].TopButtonIndex := 3;
-end;
-
-procedure TOLBarMainForm.Editpagecaption1Click(Sender: TObject);
-begin
-  with OL.PopUpObject as TJvOutlookBarPage do
-    EditCaption;
-end;
-
-procedure TOLBarMainForm.Editbuttoncaption1Click(Sender: TObject);
-begin
-  with OL.PopUpObject as TJvOutlookBarButton do
-    EditCaption;
-end;
-
 procedure TOLBarMainForm.Button1Click(Sender: TObject);
 begin
-  OL.LargeImages := ImageList1;
-  OL.SmallImages := ImageList2;
+  JvOutlookBar1.LargeImages := ImageList1;
+  JvOutlookBar1.SmallImages := ImageList2;
 end;
 
 procedure TOLBarMainForm.Button2Click(Sender: TObject);
 begin
-  OL.LargeImages := nil;
-  OL.SmallImages := nil;
+  JvOutlookBar1.LargeImages := nil;
+  JvOutlookBar1.SmallImages := nil;
 end;
 
 procedure TOLBarMainForm.Button3Click(Sender: TObject);
-var i:integer;
+var i:integer; FD:TFontDialog;
 begin
-  with TFontDialog.Create(nil) do
+  FD := TFontDialog.Create(nil);
   try
-    Font := OL.Font;
-    if Execute then
+    if not chkButtonFont.Checked then
+      FD.Font := JvOutlookBar1.Font
+    else
+      FD.Font := JvOutlookBar1.ActivePage.Font;
+    if FD.Execute then
     begin
-      for i := 0 to OL.Pages.Count - 1 do
-        OL.Pages[i].Font := Font;
-      OL.Font := Font;
+      if not chkButtonFont.Checked then
+        JvOutlookBar1.Font := FD.Font // this sets the font of all the pages
+      else
+        for i := 0 to JvOutlookBar1.Pages.Count - 1 do
+          JvOutlookBar1.Pages[i].Font := FD.Font; // this sets the button's fonts!
     end;
   finally
-    Free;
+    FD.Free;
   end;
+end;
+
+procedure TOLBarMainForm.JvOutlookBar1ButtonClick(Sender: TObject;
+  Index: Integer);
+var P:TJvOutlookBarPage;
+begin
+  if (Index > -1) then
+  with JvOutlookBar1 do
+  begin
+    P := Pages[ActivePageIndex];
+    Caption := Format('Clicked button "%s" on page "%s"',[P.Buttons[Index].Caption,P.Caption]);
+  end;
+end;
+
+procedure TOLBarMainForm.JvOutlookBar1PageChanging(Sender: TObject;
+  Index: Integer; var AllowChange: Boolean);
+begin
+  with JvOutlookBar1 do
+    if (ActivePageIndex > -1) and (Index > -1) then
+      Caption := Format('Page changing from "%s" to "%s"',
+        [Pages[ActivePageIndex].Caption, Pages[Index].Caption]);
+end;
+
+procedure TOLBarMainForm.JvOutlookBar1PageChange(Sender: TObject;
+  Index: Integer);
+begin
+  if Index > -1 then
+    Caption := Format('Page changed to "%s"',[JvOutlookBar1.Pages[Index].Caption]);
+end;
+
+procedure TOLBarMainForm.JvOutlookBar1ContextPopup(Sender: TObject;
+  MousePos: TPoint; var Handled: Boolean);
+begin
+  with Sender as TJvOutlookbar do
+  if PopupObject is TJvOutlookBarPage then
+    PopUpMenu := popPage
+  else if PopupObject is TJvOutlookBarButton then
+    PopUpMenu := popButton
+  else
+    PopUpMenu := popOL;
+end;
+
+procedure TOLBarMainForm.acSmallButtonsExecute(Sender: TObject);
+const
+  cButtonSize: array[boolean] of TJvBarButtonSize = (olbsLarge,olbsSmall);
+begin
+  acSmallButtons.Checked := not acSmallButtons.Checked;
+  JvOutlookBar1.ButtonSize := cButtonSize[acSmallButtons.Checked];
+
+end;
+
+procedure TOLBarMainForm.acEditButtonCaptionExecute(Sender: TObject);
+begin
+  with JvOutlookBar1.PopUpObject as TJvOutlookBarButton do
+    EditCaption;
+end;
+
+procedure TOLBarMainForm.acEditPageCaptionExecute(Sender: TObject);
+begin
+  with JvOutlookBar1.PopUpObject as TJvOutlookBarPage do
+    EditCaption;
+end;
+
+procedure TOLBarMainForm.chkFlatClick(Sender: TObject);
+const
+  cBorderStyle:array [boolean] of TBorderStyle = (bsSingle, bsNone);
+begin
+  JvOutlookbar1.BorderStyle := cBorderStyle[chkFlat.Checked];
+end;
+
+procedure TOLBarMainForm.FormCreate(Sender: TObject);
+begin
+  RichEdit1.Wordwrap := True;
 end;
 
 end.
