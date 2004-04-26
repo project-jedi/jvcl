3668dc93ed85eea8848a2904f623519df4878ce3
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-04-26 11:15:29 +0000

    Parameters are now allowed

--- install.bat
+++ install.bat
@@ -1,4 +1,5 @@
 @echo off
 cd packages\bin
-build.exe newest "--make=installer" "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
+SET INSTALLOPTIONS="%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
+build.exe newest "--make=installer"
 cd ..\..
