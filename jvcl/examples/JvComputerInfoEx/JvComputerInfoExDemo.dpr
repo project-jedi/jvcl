1e6dae73613e94300fabc5b9103ae30aef20f7e8
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-04-27 10:27:56 +0000

    - updated

--- jvcl/examples/JvComputerInfoEx/JvComputerInfoExDemo.dpr
+++ jvcl/examples/JvComputerInfoEx/JvComputerInfoExDemo.dpr
@@ -2,8 +2,7 @@ program JvComputerInfoExDemo;
 
 uses
   Forms,
-  MainFrm in 'MainFrm.pas' {frmMain},
-  MSHTML_TLB in '..\..\..\..\..\Delphi6\Imports\MSHTML_TLB.pas';
+  MainFrm in 'MainFrm.pas' {frmMain};
 
 {$R *.res}
 
