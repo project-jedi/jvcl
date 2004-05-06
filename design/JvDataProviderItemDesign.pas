98091b165ced74561e71ea97957c5b58d4e6e7e3
asnepvangers <asnepvangers@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-05-06 18:53:25 +0000

    Linux compatible(?) version

--- design/JvDataProviderItemDesign.pas
+++ design/JvDataProviderItemDesign.pas
@@ -189,12 +189,17 @@ begin
     properties to be appended to the existing property list. }
   // (rom) is there some security so we do not blow up everything by exceeding the 2048 bytes?
   PNewInfo := CloneTypeInfo(Pointer(PInteger(P)^), 2048);
+{$IFDEF MSWINDOWS}
   if VirtualProtect(P, 4, PAGE_WRITECOPY, OldProtect) then
   try
     PInteger(P)^ := Integer(PNewInfo);
   finally
     VirtualProtect(P, 4, OldProtect, OldProtect);
   end;
+{$ENDIF MSWINDOWS}
+{$IFDEF LINUX}
+  WriteProcessMemory(GetCurrentProcess, P, PNewInfo, 4, OldProtect);  // asn ???
+{$ENDIF LINUX}
 end;
 
 procedure ClearTypeInfo(const AClass: TClass);
@@ -206,12 +211,17 @@ begin
   P := TypeInfoFromClass(AClass);
   PNewType := Pointer(PInteger(P)^);  // The new type currently in use.
   Dec(PNewType, 4);                   // Points to the original PTypeInfo value.
+  {$IFDEF MSWINDOWS}
   if VirtualProtect(P, 4, PAGE_WRITECOPY, OldProtect) then
   try
     PInteger(P)^ := Integer(PInteger(PNewType)^);
   finally
     VirtualProtect(P, 4, OldProtect, OldProtect);
   end;
+  {$ENDIF MSWINDOWS}
+  {$IFDEF LINUX}
+  WriteProcessMemory(GetCurrentProcess, P, PNewType, 4, OldProtect);  // asn ???
+  {$ENDIF LINUX}
 end;
 
 function GetPropData(TypeData: PTypeData): PPropData;
