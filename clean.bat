0ca90a0f33c29cce8625c25c11e8b8552b387fc6
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-05-14 16:12:54 +0000

    Updated for .dfm/.xfm

--- clean.bat
+++ clean.bat
@@ -3,13 +3,12 @@ echo erasing...
 del /S *.dcu *.ddp *.dsk *.~* *.cfg *.drc %1 %2 %3 %4 %5 %6 %7 %8 %9 >NUL
 del dcu\*.obj >NUL
 if NOT "%1" == "all" goto LEAVE
-del lib\d5\*.dcp lib\d5\*.bpl >NUL
-del lib\d6\*.dcp lib\d6\*.bpl >NUL
-del lib\d7\*.dcp lib\d7\*.bpl >NUL
-del lib\c5\obj\*.obj lib\c5\obj\*.dcu >NUL
-del lib\c6\obj\*.obj lib\c6\obj\*.dcu >NUL
-del packages\*Packages.mak packages\tmp.bat >/NUL
-del /S packages\Make*.@@@ >NUL
+del lib\d5\*.dcp lib\d5\*.bpl lib\d5\*.dfm >NUL
+del lib\d6\*.dcp lib\d6\*.bpl lib\d6\*.dfm >NUL
+del lib\d7\*.dcp lib\d7\*.bpl lib\d7\*.dfm >NUL
+del lib\d7clx\*.dcp lib\d7clx\*.bpl lib\d7clx\*.xfm >NUL
+del lib\c5\obj\*.obj lib\c5\obj\*.dcu lib\c5\obj\*.dfm >NUL
+del lib\c6\obj\*.obj lib\c6\obj\*.dcu lib\c6\obj\*.dfm >NUL
+del packages\*Packages.mak >NUL
 
-: needs delete lines for k3
 :LEAVE
