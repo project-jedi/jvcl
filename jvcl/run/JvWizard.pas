e8a0deb6b767f8444065e0782b121103d1252093
asnepvangers <asnepvangers@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-06-02 23:00:47 +0000

    Updated for VisualCLX

--- jvcl/run/JvWizard.pas
+++ jvcl/run/JvWizard.pas
@@ -3117,6 +3117,7 @@ var
   AEnabledButtonSet: TJvWizardButtonSet;
   AVisibleButtonSet: TJvWizardButtonSet;
 begin
+  if not assigned(Parent) then exit;
   AEnabledButtonSet := [bkCancel];
   AVisibleButtonSet := [bkBack, bkNext, bkCancel];
   if Assigned(FActivePage) then
@@ -3176,6 +3177,7 @@ var
   end;
 
 begin
+  if not assigned(Parent) then exit;
   if FButtonBarHeight > ciButtonHeight then
   begin
     AButtonSet := [bkBack, bkNext, bkCancel];
