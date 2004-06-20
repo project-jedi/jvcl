d22f8c9aa26e4a283ebb9abb52eefd8667f2b8bc
asnepvangers <asnepvangers@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-06-20 07:28:36 +0000

    More examples

---
+++ qexamples/JvStarfield/StarFieldMain.pas
@@ -0,0 +1,28 @@
+{******************************************************************************}
+{* WARNING:  JEDI VCL To CLX Converter generated unit.                        *}
+{*           Manual modifications will be lost on next release.               *}
+{******************************************************************************}
+
+unit StarFieldMain;
+
+interface
+
+uses
+  QWindows, QMessages, SysUtils, Variants, Classes, Types, QGraphics, QControls, QForms,
+  QDialogs, JvQExControls, JvQComponent, JvQStarfield;
+
+type
+  TStarfieldMainForm = class(TForm)
+    JvStarfield1: TJvStarfield;
+  private
+  public
+  end;
+
+var
+  StarfieldMainForm: TStarfieldMainForm;
+
+implementation
+
+{$R *.xfm}
+
+end.
