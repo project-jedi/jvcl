adfae7c80b0a4e0beac881d76f0faced7c9d24d5
asnepvangers <asnepvangers@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-07-07 00:40:07 +0000

    more examples

---
+++ qexamples/JvValidators/JvQValidatorsDemo.dpr
@@ -0,0 +1,18 @@
+{******************************************************************************}
+{* WARNING:  JEDI VCL To CLX Converter generated unit.                        *}
+{*           Manual modifications will be lost on next release.               *}
+{******************************************************************************}
+
+program JvQValidatorsDemo;
+
+uses
+  QForms,
+  MainFrm in 'MainFrm.pas' {frmMain};
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
