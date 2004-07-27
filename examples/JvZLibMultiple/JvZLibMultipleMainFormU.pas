f7a6c55c5072adc904912b0f4c398beee6c88905
wpostma <wpostma@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-07-27 16:31:27 +0000

    COMPONENT EVENT CHANGED: OnDecompressingFile -> (Sender: TObject; const FileName: string;  {NEW!} var WriteFile:Boolean   )

--- examples/JvZLibMultiple/JvZLibMultipleMainFormU.pas
+++ examples/JvZLibMultiple/JvZLibMultipleMainFormU.pas
@@ -61,6 +61,10 @@ type
     { Private declarations }
     procedure DoProgress(Sender: TObject; Position, Total: Integer);
     procedure DoCompressFile(Sender: TObject; const Filename: string);
+
+    procedure DoDecompressFile(Sender:TObject;const Filename:string;var WriteFile:Boolean); // NEW!
+
+
   end;
 
 var
@@ -80,6 +84,22 @@ begin
   lblFilename.Caption := Filename;
   Update;
 end;
+
+// Event handler for before the WRITING of each file. 
+procedure TJvZLibMultipleMainForm.DoDecompressFile(Sender:TObject;const Filename:string;var WriteFile:Boolean);
+begin
+
+// NEW EVENT PARAMETER: var WriteFile:Boolean
+//
+// If you want to skip the writing of all or some files during decompression,
+// such as when you might want to generate a listing of archive contents without
+// writing to disk, just set WriteFile := false in this event!
+
+  lblFilename.Caption := Filename;
+  Update;
+end;
+
+
 
 procedure TJvZLibMultipleMainForm.btnCompressClick(Sender: TObject);
 var
@@ -111,7 +131,7 @@ begin
     lblFilename.Caption := '';
     pbProgress.Position := 0;
     z.OnProgress := DoProgress;
-    z.OnDecompressingFile := DoCompressFile;
+    z.OnDecompressingFile := DoDecompressFile;
     z.DecompressFile(edFilename.Text,edDestFolder.Text,true);
   finally
     z.Free;
