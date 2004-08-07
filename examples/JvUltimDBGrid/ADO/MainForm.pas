75a22e51a85c6ffd35e5d4d0e7a13b018e157c22
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-08-07 17:35:43 +0000

    Backported from BCB to Delphi

---
+++ examples/JvUltimDBGrid/ADO/MainForm.pas
@@ -0,0 +1,238 @@
+unit MainForm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, DB, ADODB, StdCtrls, ExtCtrls, DBCtrls, JvExStdCtrls,
+  JvDBCombobox, Grids, DBGrids, JvExDBGrids, JvDBGrid, JvDBUltimGrid;
+
+type
+  TfrmMain = class(TForm)
+    JvDBGrid1: TJvDBUltimGrid;
+    DBMemo1: TDBMemo;
+    JvDBComboBox1: TJvDBComboBox;
+    DBLookupComboBox1: TDBLookupComboBox;
+    Panel1: TPanel;
+    B_Connect: TButton;
+    B_TitleIndic: TButton;
+    B_WordWrap: TButton;
+    B_RowHeight: TButton;
+    B_ShowEdit: TButton;
+    B_Search: TButton;
+    ADOConnection1: TADOConnection;
+    MainTable: TADOTable;
+    MainTableRefLogiciel: TAutoIncField;
+    MainTableSoftware: TWideStringField;
+    MainTableCategory: TWideStringField;
+    MainTableFirstBool: TBooleanField;
+    MainTableSecondBool: TBooleanField;
+    MainTableLicenses: TSmallintField;
+    MainTablePrice: TBCDField;
+    MainTableComment: TMemoField;
+    DataSource1: TDataSource;
+    DataSource2: TDataSource;
+    LookupTable: TADOTable;
+    procedure FormShow(Sender: TObject);
+    procedure MainTableCategoryGetText(Sender: TField; var Text: String;
+      DisplayText: Boolean);
+    procedure B_RowHeightClick(Sender: TObject);
+    procedure B_ConnectClick(Sender: TObject);
+    procedure B_TitleIndicClick(Sender: TObject);
+    procedure B_WordWrapClick(Sender: TObject);
+    procedure B_ShowEditClick(Sender: TObject);
+    procedure DBMemo1Enter(Sender: TObject);
+    procedure JvDBComboBox1KeyPress(Sender: TObject; var Key: Char);
+    procedure DBLookupComboBox1KeyPress(Sender: TObject; var Key: Char);
+    procedure JvDBGrid1RestoreGridPosition(Sender: TJvDBUltimGrid;
+      SavedBookmark: Pointer; SavedRowPos: Integer);
+    procedure B_SearchClick(Sender: TObject);
+  private
+    { Private declarations }
+    OldRowsHeight, Compteur : Integer;
+    DisplayList : TStringList;
+    procedure FillUpList;
+  public
+    { Public declarations }
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.dfm}
+
+{ TfrmMain }
+
+constructor TfrmMain.Create(AOwner: TComponent);
+begin
+  inherited;
+  
+  OldRowsHeight := JvDBGrid1.RowsHeight;
+  Compteur := 0;
+  DisplayList := TStringList.Create;
+end;
+
+destructor TfrmMain.Destroy;
+begin
+  DisplayList.Free;
+  inherited;
+end;
+
+procedure TfrmMain.FillUpList;
+begin
+  if ((DisplayList = nil) or (not LookupTable.Active)) then
+    Exit;
+  DisplayList.Clear;
+  LookupTable.First;
+  while (not LookupTable.Eof) do
+  begin
+    DisplayList.Add(LookupTable.FieldByName('CodeLogiciel').AsString +
+             '=' + LookupTable.FieldByName('LibelleLog').AsString);
+    LookupTable.Next;
+  end;
+end;
+
+procedure TfrmMain.FormShow(Sender: TObject);
+var
+  InitialSort: TSortFields;
+begin
+  SetLength(InitialSort, 3);
+  InitialSort[0].Name := 'Category';
+  InitialSort[0].Order := JvGridSort_ASC;
+  InitialSort[1].Name := 'Licenses';
+  InitialSort[1].Order := JvGridSort_DESC;
+  InitialSort[2].Name := 'Software';
+  InitialSort[2].Order := JvGridSort_ASC;
+  JvDBGrid1.Sort(InitialSort);
+end;
+
+procedure TfrmMain.MainTableCategoryGetText(Sender: TField;
+  var Text: String; DisplayText: Boolean);
+begin
+  if not Assigned(DisplayList) then
+    Exit;
+  if (DisplayList.IndexOfName(Sender.AsString) = -1) then
+    FillUpList; // Key value not found -> the list is (re)loaded
+  Text := DisplayList.Values[Sender.AsString];
+end;
+
+procedure TfrmMain.B_RowHeightClick(Sender: TObject);
+begin
+  if (OldRowsHeight = JvDBGrid1.RowsHeight) then
+    JvDBGrid1.RowsHeight := Round(OldRowsHeight * 1.5)
+  else
+    JvDBGrid1.RowsHeight := OldRowsHeight;
+  JvDBGrid1.RowResize := not JvDBGrid1.RowResize;
+end;
+
+procedure TfrmMain.B_ConnectClick(Sender: TObject);
+begin
+  ADOConnection1.Connected := not ADOConnection1.Connected;
+  MainTable.Active := ADOConnection1.Connected;
+  LookupTable.Active := ADOConnection1.Connected;
+end;
+
+procedure TfrmMain.B_TitleIndicClick(Sender: TObject);
+begin
+  Inc(Compteur);
+  if (Compteur = 1) then
+  begin
+    JvDBGrid1.Options := JvDBGrid1.Options - [dgTitles];
+  end
+  else if (Compteur = 2) then
+  begin
+    JvDBGrid1.Options := JvDBGrid1.Options - [dgIndicator];
+  end
+  else if (Compteur = 3) then
+  begin
+    JvDBGrid1.Options := JvDBGrid1.Options + [dgTitles];
+  end
+  else if (Compteur = 4) then
+  begin
+    JvDBGrid1.Options := JvDBGrid1.Options + [dgIndicator];
+    Compteur := 0;
+  end;
+end;
+
+procedure TfrmMain.B_WordWrapClick(Sender: TObject);
+begin
+  JvDBGrid1.WordWrap := not JvDBGrid1.WordWrap;
+end;
+
+procedure TfrmMain.B_ShowEditClick(Sender: TObject);
+begin
+  if (dgAlwaysShowEditor in JvDBGrid1.Options) then
+    JvDBGrid1.Options := JvDBGrid1.Options - [dgAlwaysShowEditor]
+  else
+    JvDBGrid1.Options := JvDBGrid1.Options + [dgAlwaysShowEditor];
+end;
+
+procedure TfrmMain.DBMemo1Enter(Sender: TObject);
+begin
+  // Text is selected automatically
+  TDBMemo(Sender).SelectAll;
+end;
+
+procedure TfrmMain.JvDBComboBox1KeyPress(Sender: TObject; var Key: Char);
+begin
+  if (Key = #13) then
+    JvDBGrid1.CloseControl;
+end;
+
+procedure TfrmMain.DBLookupComboBox1KeyPress(Sender: TObject;
+  var Key: Char);
+begin
+  if (Key = #13) then
+    JvDBGrid1.CloseControl
+  else
+  if (Key = #27) then
+    DBLookupComboBox1.Field.Value := DBLookupComboBox1.Field.OldValue;
+end;
+
+procedure TfrmMain.JvDBGrid1RestoreGridPosition(Sender: TJvDBUltimGrid;
+  SavedBookmark: Pointer; SavedRowPos: Integer);
+begin
+  // Perfect cursor replacement
+  if (MainTable.BookmarkValid(SavedBookmark)) then
+    MainTable.Recordset.Bookmark := POleVariant(SavedBookmark)^;
+  try
+    MainTable.Resync([rmExact]);
+  except
+  end;
+end;
+
+procedure TfrmMain.B_SearchClick(Sender: TObject);
+var
+  ResultCol: Integer;
+  ResultField: TField;
+  Found: Boolean;
+begin
+  // Search of W (uppercase) in Software field, then search of 1 in Licenses field
+  Found := false;
+  JvDBGrid1.SaveGridPosition;
+  JvDBGrid1.SearchFields.Clear;
+  JvDBGrid1.SearchFields.Add('Software');
+  if (JvDBGrid1.Search('W', ResultCol, ResultField, True, False, True)) then
+  begin
+    Found := (MainTable.FieldByName('Licenses').AsString = '1');
+    while (not Found) do
+    begin
+      if (not JvDBGrid1.SearchNext(ResultCol, ResultField, True, False, True)) then
+        break;
+      Found := (MainTable.FieldByName('Licenses').AsString = '1');
+    end;
+  end;
+  if (Found) then
+    ShowMessage('Result found:'#13#10 + ResultField.AsString)
+  else
+  begin
+    JvDBGrid1.RestoreGridPosition();
+    ShowMessage('Not found');
+  end;
+end;
+
+end.
---
+++ examples/JvUltimDBGrid/BDE/MainForm.pas
@@ -0,0 +1,162 @@
+unit MainForm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, DB, DBTables, DBCtrls, StdCtrls, Buttons, ExtCtrls, Grids,
+  DBGrids, JvExDBGrids, JvDBGrid, JvDBUltimGrid;
+
+type
+  TfrmMain = class(TForm)
+    MyUltimGrid: TJvDBUltimGrid;
+    Panel1: TPanel;
+    Label1: TLabel;
+    ValueToSearch: TEdit;
+    B_Search: TBitBtn;
+    B_SearchNext: TBitBtn;
+    DBNavigator1: TDBNavigator;
+    B_ResizeCols: TBitBtn;
+    Table1: TTable;
+    DataSource1: TDataSource;
+    procedure FormShow(Sender: TObject);
+    procedure MyUltimGridIndexNotFound(Sender: TJvDBUltimGrid;
+      FieldsToSort: TSortFields; IndexFieldNames, DescFields: String;
+      var Retry: Boolean);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure B_SearchClick(Sender: TObject);
+    procedure B_SearchNextClick(Sender: TObject);
+    procedure MyUltimGridDrawColumnCell(Sender: TObject; const Rect: TRect;
+      DataCol: Integer; Column: TColumn; State: TGridDrawState);
+    procedure B_ResizeColsClick(Sender: TObject);
+  private
+    { Private declarations }
+   ResultCol: Integer;
+   ResultField: TField;
+  public
+    { Public declarations }
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.dfm}
+
+procedure TfrmMain.FormShow(Sender: TObject);
+var
+  InitialSort: TSortFields;
+begin
+  Table1.Open;
+  SetLength(InitialSort, 2);
+  InitialSort[0].Name := 'Category';
+  InitialSort[0].Order := JvGridSort_ASC;
+  InitialSort[1].Name := 'Common_Name';
+  InitialSort[1].Order := JvGridSort_ASC;
+  MyUltimGrid.Sort(InitialSort);
+end;
+
+procedure TfrmMain.MyUltimGridIndexNotFound(Sender: TJvDBUltimGrid;
+  FieldsToSort: TSortFields; IndexFieldNames, DescFields: String;
+  var Retry: Boolean);
+var
+  NewIndex: Integer;
+  DescMarker: string;
+  NewIndexName: string;
+begin
+  // Nota bene: you need to update Biolife.db to Paradox 7 if you want a sort
+  //            in descending order. Otherwise, the index creation will fail.
+  DescMarker := '';
+  if DescFields = '' then
+    DescMarker := ' (DESC)';
+  NewIndex := Application.MessageBox(PChar('Index not found for field: ' +
+              IndexFieldNames + DescMarker +
+              #13#10'Do you want to create an index ?'), 'New index ?', MB_YESNO);
+  if (NewIndex = ID_YES) then
+  begin
+    NewIndexName := IndexFieldNames;
+    while (Pos(';', NewIndexName) > 0) do
+       Delete(NewIndexName, Pos(';', NewIndexName), 1);
+    while (Pos(' ', NewIndexName) > 0) do
+       Delete(NewIndexName, Pos(' ', NewIndexName), 1);
+    NewIndexName := 'Idx_' + NewIndexName + DescMarker;
+
+    Table1.Close;
+    try
+       Table1.AddIndex(NewIndexName, IndexFieldNames,
+                        [ixCaseInsensitive], DescFields);
+    except
+      on ErrAddIdx: Exception do
+        ShowMessage('AddIndex(' + NewIndexName + ') failed: ' + ErrAddIdx.Message);
+    end;
+    Table1.Open;
+    Retry := true;
+  end;
+end;
+
+procedure TfrmMain.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  Table1.Close;
+end;
+
+procedure TfrmMain.B_SearchClick(Sender: TObject);
+var
+  SearchOk: Boolean;
+begin
+  MyUltimGrid.SearchFields.Clear();
+  MyUltimGrid.SearchFields.Add('Category');
+  MyUltimGrid.SearchFields.Add('Common_Name');
+  MyUltimGrid.SearchFields.Add('Species Name');
+  MyUltimGrid.SearchFields.Add('Notes');
+  SearchOK := MyUltimGrid.Search(ValueToSearch.Text, ResultCol, ResultField,
+                                     False, False, True);
+  B_SearchNext.Enabled := SearchOK;
+  if (SearchOK) then
+    ShowMessage('Hurrah! Text found in field ' + Trim(ResultField.FieldName))
+  else
+    ShowMessage('Text not found');
+end;
+
+procedure TfrmMain.B_SearchNextClick(Sender: TObject);
+begin
+  if (not MyUltimGrid.SearchNext(ResultCol, ResultField, False, False, True)) then
+  begin
+    ShowMessage('Text not found - End of search');
+    B_SearchNext.Enabled := False;
+  end;
+end;
+
+procedure TfrmMain.MyUltimGridDrawColumnCell(Sender: TObject;
+  const Rect: TRect; DataCol: Integer; Column: TColumn;
+  State: TGridDrawState);
+var
+  Image: TBitmap;
+begin
+  // Hey fish, let me see your face...hum...your fins
+  if (Column.Field.FieldName = 'Graphic') then
+  begin
+    Image := TBitmap.Create;
+    try
+      Image.Assign(TBlobField(Column.Field));
+      if (Image.Height > Rect.Bottom - Rect.Top) then
+        Image.Height := Rect.Bottom - Rect.Top;
+      if (Image.Width > Rect.Right - Rect.Left) then
+        Image.Width := Rect.Right - Rect.Left;
+      MyUltimGrid.Canvas.FillRect(Rect);
+      MyUltimGrid.Canvas.Draw(Rect.Left, Rect.Top, Image);
+    finally
+      Image.Free;
+    end;
+  end;
+end;
+
+procedure TfrmMain.B_ResizeColsClick(Sender: TObject);
+begin
+  MyUltimGrid.RowsHeight := 129;
+  MyUltimGrid.InitializeColumnsWidth(70, False, False);
+  MyUltimGrid.AutoSizeColumnIndex := JvGridResizeLastVisibleCol;
+  MyUltimGrid.AutoSizeColumns := True;
+end;
+
+end.
