eb4ce257a0ec52cba2726a06d5a11458e6040163
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-09-01 15:02:09 +0000

    *** empty log message ***

---
+++ devtools/UnitVersioning/UnitVersioning.dpr
@@ -0,0 +1,13 @@
+program UnitVersioning;
+
+uses
+  Forms,
+  Main in 'Main.pas' {FormMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TFormMain, FormMain);
+  Application.Run;
+end.
