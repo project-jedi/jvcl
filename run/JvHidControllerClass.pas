24a0df2e8351f934f5c4723fced3412912020bdc
marquardt <marquardt@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-09-06 04:59:45 +0000

    just a version string change of the component

--- run/JvHidControllerClass.pas
+++ run/JvHidControllerClass.pas
@@ -39,7 +39,7 @@ uses
 
 const
   // a version string for the component
-  cHidControllerClassVersion = '1.0.25';
+  cHidControllerClassVersion = '1.0.26';
 
   // strings from the registry for CheckOutByClass
   cHidNoClass = 'HIDClass';
