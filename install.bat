81933aff6ab43e47fe9c52a5c17e08044d2a7043
asnepvangers <asnepvangers@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-09-11 11:47:46 +0000

    JvExVCL units are created prior to install.

--- install.bat
+++ install.bat
@@ -1,5 +1,7 @@
-@echo off
-cd packages\bin
+echo off
+cd devtools\JvExVcl
+call preprocess.bat
+cd ..\..\packages\bin
 SET INSTALLOPTIONS="%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
 build.exe newest "--make=installer"
 cd ..\..
