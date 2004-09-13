4073ecc3ea52796cacdfc462c2fc3104aa80faec
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-09-13 14:01:04 +0000

    Preprocess is now executed

--- install.bat
+++ install.bat
@@ -1,7 +1,5 @@
 echo off
-cd devtools\JvExVcl
-call preprocess.bat
-cd ..\..\packages\bin
+cd packages\bin
 SET INSTALLOPTIONS="%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
 build.exe newest "--make=installer"
 cd ..\..
