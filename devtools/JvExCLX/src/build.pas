67587407ed942d74dbee74ebf9b781d93a1ec1fb
asnepvangers <asnepvangers@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-09-21 20:10:17 +0000

    typo + added MPL-header

--- devtools/JvExCLX/src/build.pas
+++ devtools/JvExCLX/src/build.pas
@@ -1,4 +1,31 @@
-unit qbuild;
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http:{www.mozilla.org/MPL/MPL-1.1.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: JvQExControls.macros, released on 2004-09-21
+
+The Initial Developer of the Original Code is André Snepvangers [ASnepvangers att users.sourceforge.net]
+Portions created by André Snepvangers are Copyright (C) 2004 André Snepvangers.
+All Rights Reserved.
+
+Contributor(s):
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
+located at http:(jvcl.sourceforge.net
+
+Description:
+
+Contains all JvExCLX units to process by the preprocessor jpp.exe
+
+Known Issues:
+-----------------------------------------------------------------------------}
+unit build;
 
 interface
 
