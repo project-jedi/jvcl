ca349a27fad7cae400d0eff54ccc288ce6fdcbb3
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-10-22 20:23:41 +0000

    0002247: JVCLVer modification

--- jvcl/run/JVCLVer.pas
+++ jvcl/run/JVCLVer.pas
@@ -15,7 +15,17 @@ Portions created by Joe Doe are Copyright (C) 1999 Joe Doe.
 Portions created by XXXX Corp. are Copyright (C) 1998, 1999 XXXX Corp.
 All Rights Reserved.
 
-Contributor(s): Michael Beck [mbeck att bigfoot dott com].
+Contributor(s): 
+  Michael Beck [mbeck att bigfoot dott com].
+  Hofi
+
+Last Modified: 2004-10-19
+
+Changes:
+2004-10-10:
+  * Added by Hofi
+      JVCL_VERSION
+        Helps conditional compiling in BCB.
 
 You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
 located at http://jvcl.sourceforge.net
@@ -32,6 +42,7 @@ interface
 
 {$IFDEF VCL}
 const
+  JVCL_VERSION = 300;
   JVCL_VERSIONSTRING = '3.00';
 {$ENDIF VCL}
 {$IFDEF VisualCLX}
