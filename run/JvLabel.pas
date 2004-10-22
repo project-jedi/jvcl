42a61f69113dae80fa725efb6a325fdda83d4f72
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-10-22 20:37:53 +0000

    - Mantis 0002233: the TJvCustomLabel procedure AdjustBounds lost keyword virtual

--- run/JvLabel.pas
+++ run/JvLabel.pas
@@ -135,7 +135,7 @@ type
     procedure VisibleChanged; override;
 
     procedure DoDrawText(var Rect: TRect; Flags: Integer); virtual;
-    procedure AdjustBounds;override;
+    procedure AdjustBounds;virtual;
     {$IFDEF VCL}
     procedure SetAutoSize(Value: Boolean); override;
     {$ENDIF VCL}
