ac566c7615e43e9f5fed41bd7825cef145351690
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-11-01 10:20:31 +0000

    - Fixed for D5

--- run/JvStringHolder.pas
+++ run/JvStringHolder.pas
@@ -167,7 +167,7 @@ type
     property Strings: TStrings read FStrings write SetStrings;
   end;
 
-  TJvMultiStringHolderCollection = class(TCollection)
+  TJvMultiStringHolderCollection = class(TOwnedCollection)
   protected
     function GetItem(Index: Integer): TJvMultiStringHolderCollectionItem;
     procedure SetItem(Index: Integer; Value: TJvMultiStringHolderCollectionItem);
@@ -904,7 +904,7 @@ end;
 constructor TJvMultiStringHolder.Create(AOwner: TComponent);
 begin
   inherited Create(AOwner);
-  FMultipleStrings := TJvMultiStringHolderCollection.Create(TJvMultiStringHolderCollectionItem);
+  FMultipleStrings := TJvMultiStringHolderCollection.Create(Self, TJvMultiStringHolderCollectionItem);
 end;
 
 destructor TJvMultiStringHolder.Destroy;
