64988007f6fbc58e3ac01f7a779d86e8e3087d97
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-11-06 19:52:36 +0000

    Now uses the condition parser to allow complex expressions in conditions

--- jvcl/devtools/PackagesGenerator/pg.dpr
+++ jvcl/devtools/PackagesGenerator/pg.dpr
@@ -8,7 +8,8 @@ uses
   GenerateUtils in 'GenerateUtils.pas',
   FileUtils in 'FileUtils.pas',
   PackageModels in '..\Common\PackageModels.pas',
-  PackageInformation in '..\Common\PackageInformation.pas';
+  PackageInformation in '..\Common\PackageInformation.pas',
+  ConditionParser in 'ConditionParser.pas';
 
 begin
   CmdLineRun;
