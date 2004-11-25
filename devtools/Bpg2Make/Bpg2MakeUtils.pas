12d6540207adab7c6c4a6dc882a8c434810f002d
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-11-25 19:31:49 +0000

    D2k support

--- devtools/Bpg2Make/Bpg2MakeUtils.pas
+++ devtools/Bpg2Make/Bpg2MakeUtils.pas
@@ -1,3 +1,29 @@
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/MPL-1.1.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: Bpg2MakeUtils.pas, released on 2003-09-29.
+
+The Initial Developer of the Original Code is Andreas Hausladen
+(Andreas dott Hausladen att gmx dott de)
+Portions created by Andreas Hausladen are Copyright (C) 2004 Andreas Hausladen.
+All Rights Reserved.
+
+Contributor(s): -
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL
+home page, located at http://jvcl.sourceforge.net
+
+Known Issues:
+-----------------------------------------------------------------------------}
+// $Id$
+
 {$I jvcl.inc}
 
 unit Bpg2MakeUtils;
@@ -11,7 +37,7 @@ implementation
 
 uses
   Windows, Classes, SysUtils,
-  JvConsts; // (rom) for sLineBreak, PathDelim  no dependencies to packages
+  JvConsts, JvSimpleXml;
 
 const
   DefaultMakeFile =
@@ -59,7 +85,6 @@ begin
   end;
 end;
 
-
 function FastStringReplace(const Text, SearchText, ReplaceText: string;
   ReplaceAll, IgnoreCase: Boolean): string;
 var
@@ -86,7 +111,6 @@ begin
   else
     SetLength(Result, LenText + (LenReplaceText - LenSearchText));
 
-
   Len := 0;
   StartIndex := 1;
   for Index := 1 to LenText do
@@ -108,7 +132,7 @@ begin
       end; 
 
       if not ReplaceAll then Break; 
-    end; 
+    end;
   end; 
 
   Index := LenText + 1;
@@ -125,7 +149,7 @@ procedure ShowHelp;
 begin
   writeln('');
   writeln('');
-  writeln('Bpg2Make: Creates a MAKEFILE from a BPG file');
+  writeln('Bpg2Make: Creates a MAKEFILE from a bpg/bdsgroup file');
   writeln('');
   writeln('Usage:');
   writeln(ExtractFilename(ParamStr(0)),' <bpgfile>');
@@ -147,29 +171,103 @@ begin
   end;
 end;
 
-function CreateMakeFile(const Filename: string): Boolean;
+function LoadUtf8File(const Filename: string): string;
+var
+  Content: UTF8String;
+  Stream: TFileStream;
+begin
+  Stream := TFileStream.Create(Filename, fmOpenRead or fmShareDenyWrite);
+  try
+    SetLength(Content, Stream.Size);
+    Stream.Read(Content[1], Stream.Size);
+  finally
+    Stream.Free;
+  end;
+  Delete(Content, 1, 3); // This a little bit dirty but unless we mix UTF8,
+                         // UTF16 and ANSI files there is no problem.
+  {$IFDEF COMPILER6_UP}
+  Result := Utf8ToAnsi(Content);
+  {$ELSE}
+    { Delphi 5 (should) never reachs this because the Installer uses the newest
+      installed Delphi version and only reads the project groups of installed
+      Delphi/BCB/BDN versions. }
+  Result := Content;
+  {$ENDIF COMPILIER6_UP}
+end;
+
+procedure ProcessBdsgroupFile(const Filename: string; MkLines, Targets, Commands: TStrings);
+var
+  xml: TJvSimpleXML;
+  Options, Projects: TJvSimpleXMLElem;
+  i, OptIndex, PrjIndex: Integer;
+  Personality: string;
+  TgName: string;
+begin
+  xml := TJvSimpleXML.Create(nil);
+  try
+    xml.LoadFromString(LoadUtf8File(Filename));
+
+    for i := 0 to xml.Root.Items.Count - 1 do
+    begin
+      if (CompareText(xml.Root.Items[i].Name, 'PersonalityInfo') = 0) and // <PersonalityInfo>
+         (xml.Root.Items[i].Items.Count > 0) then
+      begin
+        // find correct Personality
+        Options := xml.Root.Items[i].Items[0];
+        if CompareText(Options.Name, 'Option') = 0 then
+        begin
+          for OptIndex := 0 to Options.Items.Count - 1 do
+            if CompareText(Options.Items[OptIndex].Properties.Value('Name'), 'Personality') = 0 then
+            begin
+              Personality := Options.Items[OptIndex].Value;
+              Break;
+            end;
+        end;
+      end
+      else
+      if (CompareText(xml.Root.Items[i].Name, Personality) = 0) and
+         (xml.Root.Items[i].Items.Count > 0) and
+         (CompareText(xml.Root.Items[i].Items[0].Name, 'Projects') = 0) then
+      begin
+         // Read project list
+         Projects := xml.Root.Items[i].Items[0];
+         for PrjIndex := 0 to Projects.Items.Count - 1 do
+         begin
+           TgName := Projects.Items[PrjIndex].Properties.Value('Name');
+           if CompareText(TgName, 'Targets') <> 0 then
+           begin
+             // change .bdsproj to .dpk and add the target
+             Targets.Add(TgName + '=' + ChangeFileExt(Projects.Items[PrjIndex].Value, '.dpk'));
+             Commands.Add(#9'$(DCC)');
+           end;
+         end;
+      end;
+    end;
+
+    if Targets.Count > 1 then
+    begin
+      MkLines.Add('PROJECTS = ' + Targets.Names[0] + ' \');
+      for i := 2 to Targets.Count - 2 do
+        MkLines.Add(#9 + Targets.Names[i] + ' \');
+      MkLines.Add(#9 + Targets.Names[Targets.Count - 1]);
+    end
+    else if Targets.Count = 1 then
+      MkLines.Add('PROJECTS = ' + Targets.Names[0]);
+  finally
+    xml.Free;
+  end;
+end;
+
+procedure ProcessBpgFile(const Filename: string; MkLines, Targets, Commands: TStrings);
 var
   i, ps: Integer;
-  List, MkLines, Targets, Commands: TStringlist;
-  S, SourceFile, Dir: string;
+  List: TStringlist;
+  S: string;
   ProjectCommands: string;
 begin
-  Result := False;
-  if not FileExists(Filename) then
-  begin
-    WriteLn('ERROR: ', Filename, ' not found!');
-    Exit;
-  end;
-
   List := TStringList.Create;
-  MkLines := TStringList.Create;
-  Targets := TStringList.Create;
-  Commands := TStringList.Create;
   try
     List.LoadFromFile(Filename);
-
-    MkLines.Text := DefaultMakeFile;
-
     i := 0;
     while i < List.Count do
     begin
@@ -202,6 +300,37 @@ begin
       end;
       Inc(i);
     end;
+  finally
+    List.Free;
+  end;
+end;
+
+
+function CreateMakeFile(const Filename: string): Boolean;
+var
+  MkLines, Targets, Commands: TStrings;
+  S, SourceFile, Dir, ProjectCommands: string;
+  i, ps: Integer;
+begin
+  Result := False;
+  if not FileExists(Filename) then
+  begin
+    WriteLn('ERROR: ', Filename, ' not found!');
+    Exit;
+  end;
+
+  MkLines := TStringList.Create;
+  Targets := TStringList.Create;
+  Commands := TStringList.Create;
+  try
+    MkLines.Text := DefaultMakeFile;
+
+    if CompareText(ExtractFileExt(Filename), '.bpg') = 0 then
+      ProcessBpgFile(Filename, MkLines, Targets, Commands)
+    else
+      ProcessBdsgroupFile(Filename, MkLines, Targets, Commands);
+
+    MkLines.Add('');
     MkLines.Add('#---------------------------------------------------------------------------------------------------');
     MkLines.Add('default: $(PROJECTS)');
     MkLines.Add('#---------------------------------------------------------------------------------------------------');
@@ -218,7 +347,6 @@ begin
       if Dir <> '' then
         MkLines.Add(#9'@cd ' + Dir);
 
-
       ProjectCommands := Commands[i];
       ProjectCommands := FastStringReplace(ProjectCommands, '$**', SourceFile, True, False);
       ProjectCommands := FastStringReplace(ProjectCommands, '$*',
@@ -226,7 +354,7 @@ begin
      // long path name support
       ProjectCommands := FastStringReplace(ProjectCommands, '$(ROOT)\bin\bpr2mak', '"$(ROOT)\bin\bpr2mak"', False, True);
       ProjectCommands := FastStringReplace(ProjectCommands, '$(ROOT)\bin\make', '"$(ROOT)\bin\make"', False, True);
-      
+
       MkLines.Add(#9'@echo [Compiling: ' + S + ']');
       MkLines.Add(ProjectCommands);
       MkLines.Add(#9'@echo.');
@@ -239,18 +367,16 @@ begin
     Commands.Free;
     Targets.Free;
     MkLines.Free;
-    List.Free;
   end;
   Result := True;
 end;
 
 procedure Run;
+var
+  Ext: string;
 begin
-  {
-    command-line:
-      <filename>
-  }
-  if (ParamCount <> 1) or (LowerCase(ExtractFileExt(ParamStr(1))) <> '.bpg') then
+  Ext := LowerCase(ExtractFileExt(ParamStr(1)));
+  if (ParamCount <> 1) or ((Ext <> '.bpg') and (Ext <> '.bdsgroup')) then
     ShowHelp
   else
   try
@@ -258,7 +384,7 @@ begin
       WriteLn(' Makefile created');
   except
     on E: Exception do
-      writeln('ERROR: ', E.Message);
+      WriteLn('ERROR: ', E.Message);
   end;
 end;
 
