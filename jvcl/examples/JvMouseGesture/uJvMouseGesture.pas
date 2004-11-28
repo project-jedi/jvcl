736bd78f2fab01349a817aa42672892687cbec2c
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-11-28 20:59:07 +0000

    - Updated to reflect changes to JvMouseGesture component

--- jvcl/examples/JvMouseGesture/uJvMouseGesture.pas
+++ jvcl/examples/JvMouseGesture/uJvMouseGesture.pas
@@ -30,32 +30,39 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, JvMouseGesture, ExtCtrls, JvComponent;
+  Dialogs, StdCtrls, JvMouseGesture, ExtCtrls, JvComponent, Menus,
+  JvExStdCtrls, JvCheckBox, JvRadioButton;
 
 type
   TForm1 = class(TForm)
     Memo1: TMemo;
-    RadioGroup1: TRadioGroup;
     JvMouseGesture1: TJvMouseGesture;
     JvMouseGestureHook1: TJvMouseGestureHook;
-    ComboBox1: TComboBox;
+    GroupBox1: TGroupBox;
+    rbFormOnly: TJvRadioButton;
+    rbAppEvents: TJvRadioButton;
     Label1: TLabel;
+    cbMouseButton: TComboBox;
+    chkNoPopup: TJvCheckBox;
+    pnlGesture: TPanel;
+    Label2: TLabel;
     procedure FormMouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
     procedure FormMouseMove(Sender: TObject; Shift: TShiftState; X,
       Y: Integer);
     procedure FormMouseUp(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
-    procedure JvMouseGestureHook1JvMouseGestureCustomInterpretation(
-      aGesture: String);
     procedure FormShow(Sender: TObject);
-    procedure RadioGroup1Click(Sender: TObject);
-    procedure ComboBox1Change(Sender: TObject);
+    procedure cbMouseButtonChange(Sender: TObject);
+    procedure Memo1ContextPopup(Sender: TObject; MousePos: TPoint;
+      var Handled: Boolean);
+    procedure JvMouseGestureHook1MouseGestureCustomInterpretation(
+      Sender: TObject; const AGesture: String);
+    procedure FormCreate(Sender: TObject);
+    procedure rbFormOnlyClick(Sender: TObject);
   private
     procedure RefreshCaption;
-    { Private-Deklarationen }
   public
-    { Public-Deklarationen }
   end;
 
 var
@@ -66,7 +73,7 @@ implementation
 
 {$R *.dfm}
 
-function booltostr(AValue:boolean):string;
+function BoolToStr(AValue:boolean):string;
 const cBool:array[boolean] of PChar = ('false','true');
 begin
   Result := cBool[AValue];
@@ -75,11 +82,10 @@ end;
 procedure TForm1.FormMouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
-  if RadioGroup1.ItemIndex = 1 then exit;
-
+  if not rbFormOnly.Checked then
+    Exit;
   if Button = mbRight then
     JvMouseGesture1.StartMouseGesture(x,y);
-
 end;
 
 procedure TForm1.FormMouseMove(Sender: TObject; Shift: TShiftState; X,
@@ -92,17 +98,11 @@ end;
 procedure TForm1.FormMouseUp(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
-  if JvMouseGesture1.TrailActive then begin
+  if JvMouseGesture1.TrailActive then
+  begin
     JvMouseGesture1.EndMouseGesture;
     Memo1.Lines.Add('Gesture = ' + JvMouseGesture1.Gesture);
   end;
-end;
-
-
-procedure TForm1.JvMouseGestureHook1JvMouseGestureCustomInterpretation(
-  aGesture: String);
-begin
-  Memo1.Lines.Add('Gesture (via hook) = ' + aGesture);
 end;
 
 procedure TForm1.FormShow(Sender: TObject);
@@ -112,36 +112,60 @@ begin
   RefreshCaption;
 end;
 
-procedure TForm1.RadioGroup1Click(Sender: TObject);
-begin
-  if RadioGroup1.ItemIndex = 0 then begin
-    JvMouseGesture1.Active := true;
-    JvMouseGestureHook1.Active := false;
-  end
-  else begin
-    JvMouseGesture1.Active := false;
-    JvMouseGestureHook1.Active := true;
-  end;
-  RefreshCaption;
-end;
-
 procedure TForm1.RefreshCaption;
 begin
   Memo1.Clear;
-  Caption := 'hook on form active = ' + booltostr(JvMouseGesture1.Active) +
-             '  ApplicationHook active = '+ booltostr(JvMouseGestureHook1.Active);
+  Caption := 'Panel Hooked = ' + BoolToStr(JvMouseGesture1.Active) +
+             ', Application Hooked = '+ BoolToStr(JvMouseGestureHook1.Active);
   Memo1.Lines.Add(Caption);
 end;
 
 
-procedure TForm1.ComboBox1Change(Sender: TObject);
+procedure TForm1.cbMouseButtonChange(Sender: TObject);
 begin
-  case Combobox1.ItemIndex of
-    0: JvMouseGestureHook1.MouseButton := JvMButtonLeft;
-    1: JvMouseGestureHook1.MouseButton := JvMButtonMiddle;
-    2: JvMouseGestureHook1.MouseButton := JvMButtonRight;
+  chkNoPopup.Enabled := false;
+  case cbMouseButton.ItemIndex of
+    0: JvMouseGestureHook1.MouseButton := mbLeft;
+    1: JvMouseGestureHook1.MouseButton := mbMiddle;
+    2:
+    begin
+      JvMouseGestureHook1.MouseButton := mbRight;
+      chkNoPopup.Enabled := true;
+    end;
   end;
-    
+end;
+
+procedure TForm1.Memo1ContextPopup(Sender: TObject; MousePos: TPoint;
+  var Handled: Boolean);
+begin
+  // if we have a gesture and the user wants to surpress the popup, return true
+  Handled := chkNoPopup.Checked and rbAppEvents.Checked and (JvMouseGestureHook1.MouseGesture.Gesture <> '');
+end;
+
+procedure TForm1.JvMouseGestureHook1MouseGestureCustomInterpretation(
+  Sender: TObject; const AGesture: String);
+begin
+  Memo1.Lines.Add('Gesture (via hook) = ' + AGesture);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  cbMouseButton.ItemIndex := 2;
+end;
+
+procedure TForm1.rbFormOnlyClick(Sender: TObject);
+begin
+  if rbFormOnly.Checked then
+  begin
+    JvMouseGesture1.Active := true;
+    JvMouseGestureHook1.Active := false;
+  end
+  else
+  begin
+    JvMouseGesture1.Active := false;
+    JvMouseGestureHook1.Active := true;
+  end;
+  RefreshCaption;
 end;
 
 end.
