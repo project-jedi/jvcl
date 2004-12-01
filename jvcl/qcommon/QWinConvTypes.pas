1e2b885a1b524ccf45fa0a8c0a9928b71f5aedab
asnepvangers <asnepvangers@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-12-01 17:20:28 +0000

    update from JVCL

---
+++ jvcl/qcommon/QWinConvTypes.pas
@@ -0,0 +1,87 @@
+{******************************************************************************}
+{* WARNING:  JEDI VCL To CLX Converter generated unit.                        *}
+{*           Manual modifications will be lost on next release.               *}
+{******************************************************************************}
+
+{******************************************************************}
+{                                                                  }
+{       Borland Delphi Runtime Library                             }
+{       Config Manager API interface unit                          }
+{                                                                  }
+{ The original Pascal code is: WinConvTypes.pas,                   }
+{ released 5 Nov 2004.                                             }
+{ The initial developer of the Pascal code is Robert Marquardt     }
+{ (robert_marquardt att gmx dott de)                               }
+{                                                                  }
+{ Portions created by Robert Marquardt are                         }
+{ Copyright (C) 2004 Robert Marquardt.                             }
+{                                                                  }
+{ Contributor(s):                                                  }
+{                                                                  }
+{ Obtained through:                                                }
+{ Joint Endeavour of Delphi Innovators (Project JEDI)              }
+{                                                                  }
+{ You may retrieve the latest version of this file at the Project  }
+{ JEDI home page, located at http://delphi-jedi.org                }
+{                                                                  }
+{ The contents of this file are used with permission, subject to   }
+{ the Mozilla Public License Version 1.1 (the "License"); you may  }
+{ not use this file except in compliance with the License. You may }
+{ obtain a copy of the License at                                  }
+{ http://www.mozilla.org/MPL/MPL-1.1.html                          }
+{                                                                  }
+{ Software distributed under the License is distributed on an      }
+{ "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or   }
+{ implied. See the License for the specific language governing     }
+{ rights and limitations under the License.                        }
+{                                                                  }
+{******************************************************************}
+
+unit QWinConvTypes;
+
+{$I windowsversion.inc}
+
+interface
+
+{$WEAKPACKAGEUNIT ON}
+
+uses
+  QWindows;
+
+type
+  {$IFDEF UNICODE}
+  LPCTSTR = PWideChar;
+  LPTSTR  = PWideChar;
+  PCTSTR  = PWideChar;
+  PTSTR   = PWideChar;
+  TCHAR   = WideChar;
+  {$ELSE}
+  LPCTSTR = PAnsiChar;
+  LPTSTR  = PAnsiChar;
+  PCTSTR  = PAnsiChar;
+  PTSTR   = PAnsiChar;
+  TCHAR   = Char;
+  {$ENDIF UNICODE}
+  {$EXTERNALSYM LPCTSTR}
+  {$EXTERNALSYM LPTSTR}
+  {$EXTERNALSYM PCTSTR}
+  PPWSTR = ^PWideChar;
+  PPASTR = ^PAnsiChar;
+  PPSTR  = ^PTSTR;
+  {$EXTERNALSYM PTSTR}
+
+type
+  ULONG_PTR = DWORD;
+  {$EXTERNALSYM ULONG_PTR}
+  DWORD_PTR = DWORD;
+  {$EXTERNALSYM DWORD_PTR}
+  UINT_PTR  = DWORD;
+  {$EXTERNALSYM UINT_PTR}
+  ULONG32 = ULONG;
+  {$EXTERNALSYM ULONG32}
+  ULONG64 = Int64;   // (rom) no unsigned Int64 available in Delphi
+  {$EXTERNALSYM ULONG64}
+
+implementation
+
+end.
