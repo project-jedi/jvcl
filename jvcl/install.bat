602a25034d387576c3c348862523c6bd634ac35a
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-12-12 19:47:19 +0000

    User can now choose which Delphi version he want to use for the Installer

--- jvcl/install.bat
+++ jvcl/install.bat
@@ -1,6 +1,10 @@
 @echo off
+
+SET DELPHIVERSION=%1
+if "%1" == "" SET DELPHIVERSION=newest
+
 cd packages\bin
-SET INSTALLOPTIONS="%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
-build.exe newest "--make=installer"
+SET INSTALLOPTIONS="%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
+build.exe %DELPHIVERSION% "--make=installer"
 cd ..\..
 SET INSTALLOPTIONS=
