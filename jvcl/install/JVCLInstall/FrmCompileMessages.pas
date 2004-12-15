872b6863dede13a79e16f0787bd964ddcfc6b1c7
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-12-15 22:04:32 +0000

    Added MPL Header

--- jvcl/install/JVCLInstall/FrmCompileMessages.pas
+++ jvcl/install/JVCLInstall/FrmCompileMessages.pas
@@ -1,3 +1,29 @@
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/MPL-1.1.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: FrmCompileMessages.pas, released on 2004-12-13.
+
+The Initial Developer of the Original Code is Andreas Hausladen
+(Andreas dott Hausladen att gmx dott de)
+Portions created by Andreas Hausladen are Copyright (C) 2004 Andreas Hausladen.
+All Rights Reserved.
+
+Contributor(s): -
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL
+home page, located at http://jvcl.sourceforge.net
+
+Known Issues:
+-----------------------------------------------------------------------------}
+// $Id$
+
 unit FrmCompileMessages;
 
 {$I jvcl.inc}
