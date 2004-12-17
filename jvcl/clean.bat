f631f8067b9da22da9a508c4ee4d5c830b913a8c
jfudickar <jfudickar@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-12-17 23:09:56 +0000

    Also deleting *.dsm

--- jvcl/clean.bat
+++ jvcl/clean.bat
@@ -1,6 +1,6 @@
 @echo off
 echo erasing...
-del /S *.dcu *.ddp *.dsk *.~* *.cfg *.drc %1 %2 %3 %4 %5 %6 %7 %8 %9 >NUL
+del /S *.dcu *.ddp *.dsk *.~* *.cfg *.drc *.dsm %1 %2 %3 %4 %5 %6 %7 %8 %9 >NUL
 del dcu\*.obj >NUL
 if NOT "%1" == "all" goto LEAVE
 del lib\d5\*.dcp lib\d5\*.bpl lib\d5\*.dfm >NUL
