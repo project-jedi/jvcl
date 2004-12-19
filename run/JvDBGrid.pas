09eba10681162d9ceeba344ee7247a0d6c349193
peter3 <peter3@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-12-19 11:33:43 +0000

    - Mantis 0002401: DBGrid shows rtf-codes instead of image - again

--- run/JvDBGrid.pas
+++ run/JvDBGrid.pas
@@ -912,7 +912,7 @@ begin
         Result := Ord(gpData);
       ftReference, ftDataSet:
         Result := Ord(gpData);
-      ftFmtMemo, ftOraClob:
+      ftMemo, ftFmtMemo, ftOraClob:
         Result := Ord(gpMemo);
       ftOraBlob:
         Result := Ord(gpBlob);
