4bd61add6ce966c7d74f1020490916646c4fbf2e
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2004-12-20 19:36:25 +0000

    New example from Korecek

---
+++ jvcl/examples/JvUIB/BCB/QuickScript/QuickScript.cpp
@@ -0,0 +1,33 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+//---------------------------------------------------------------------------
+USEFORM("main.cpp", frmMain);
+//---------------------------------------------------------------------------
+WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+        try
+        {
+                 Application->Initialize();
+                 Application->CreateForm(__classid(TfrmMain), &frmMain);
+                 Application->Run();
+        }
+        catch (Exception &exception)
+        {
+                 Application->ShowException(&exception);
+        }
+        catch (...)
+        {
+                 try
+                 {
+                         throw Exception("");
+                 }
+                 catch (Exception &exception)
+                 {
+                         Application->ShowException(&exception);
+                 }
+        }
+        return 0;
+}
+//---------------------------------------------------------------------------
