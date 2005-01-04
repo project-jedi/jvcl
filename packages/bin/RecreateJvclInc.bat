680ea767669c632eac83de11efae023dd416506b
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2005-01-04 22:49:18 +0000

    initial upload

---
+++ packages/bin/RecreateJvclInc.bat
@@ -0,0 +1,10 @@
+@echo off
+SET DIR=..\..\common
+copy %DIR%\jvclbase.inc %DIR%\jvclc5.inc
+copy %DIR%\jvclbase.inc %DIR%\jvclc6.inc
+copy %DIR%\jvclbase.inc %DIR%\jvcld5.inc
+copy %DIR%\jvclbase.inc %DIR%\jvcld6.inc
+copy %DIR%\jvclbase.inc %DIR%\jvcld7.inc
+copy %DIR%\jvclbase.inc %DIR%\jvcld9.inc
+copy %DIR%\jvclbase.inc %DIR%\jvclk3.inc
+SET DIR=
