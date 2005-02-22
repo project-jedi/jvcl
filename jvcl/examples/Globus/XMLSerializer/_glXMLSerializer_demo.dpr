b17d1b2ac159f6f3aa0bc71ba50a2ce5d229c226
grenzi_r <grenzi_r@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2005-02-22 07:14:22 +0000

    change FormName so that it can be included in the JVCL MegaDemo

--- jvcl/examples/Globus/XMLSerializer/_glXMLSerializer_demo.dpr
+++ jvcl/examples/Globus/XMLSerializer/_glXMLSerializer_demo.dpr
@@ -2,13 +2,14 @@ program _glXMLSerializer_demo;
 
 uses
   Forms,
-  main in 'main.pas' {fglXMLSerializerDemo},
-  testClasses in 'testClasses.pas';
+  testClasses in 'testClasses.pas',
+  XMLSerializerMainFormU in 'XMLSerializerMainFormU.pas' {fglXMLSerializerDemo};
 
 {$R *.RES}
 
 begin
   Application.Initialize;
+  Application.CreateForm(TfglXMLSerializerDemo, fglXMLSerializerDemo);
   Application.CreateForm(TfglXMLSerializerDemo, fglXMLSerializerDemo);
   Application.Run;
 end.
