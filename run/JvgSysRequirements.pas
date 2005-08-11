87398b4b45105cabfb1ca7982d5bc5efc1b91650
marquardt <marquardt@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2005-08-11 08:38:07 +0000

    rewritten for absorption, will be extended further

--- run/JvgSysRequirements.pas
+++ run/JvgSysRequirements.pas
@@ -32,68 +32,63 @@ unit JvgSysRequirements;
 interface
 
 uses
-  {$IFDEF USEJVCL}
   {$IFDEF UNITVERSIONING}
   JclUnitVersioning,
   {$ENDIF UNITVERSIONING}
-  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
-  JvComponent;
-  {$ELSE}
-  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs;
-  {$ENDIF USEJVCL}
+  Windows, SysUtils, Classes, Forms,
+  JvComponent,
+  JclSysInfo;
 
 type
-  TglMinVideoVRefreshRate = (frrIgnore, frr70Hertz, frr75Hertz, frr85Hertz);
-  TglMinGraphicResolution = (fgrIgnore, fgr800x600, fgr1024x768);
-  TglMinColorDepth = (fcdIgnore, fcd16BitColor, fcd32BitColor);
-  TglSystemFont = (fsfIgnore, fsfSmallFont, fsfBigFont);
-  //  TglProcessor = (fsrPentium, fsrMMX);
-  TglOS = (fosWindowsNT, fosWindows95);
-  TglOSSet = set of TglOS;
+  TJvSystemFont = (fsfSmallFont, fsfBigFont);
+  TJvSystemFontSet = set of TJvSystemFont;
+  TWindowsVersionSet = set of TWindowsVersion;
 
-  TglRequirement = (fsrVideoVRefreshRate, fsrGraphicResolution,
-    fsrColorDepth, fsrSystemFont, {fsrProcessor, } fsrOSPlatform);
-  TglRequirements = set of TglRequirement;
-
-  TglSysReqBehavior = (fsbHalt, fsbWarning);
-  TglWarningEvent = procedure(Sender: TObject; var ReportMessage: string;
+  TJvSysReqBehavior = (fsbHalt, fsbWarning);
+  TJvWarningEvent = procedure(Sender: TObject; var ReportMessage: string;
     var DoShowWarning, DoHalt: Boolean) of object;
 
-  {$IFDEF USEJVCL}
+const
+  AllWindowsVersions = [wvUnknown];
+  AllSystemFonts = [fsfSmallFont, fsfBigFont];
+
+type
   TJvgSysRequirements = class(TJvComponent)
-  {$ELSE}
-  TJvgSysRequirements = class(TComponent)
-  {$ENDIF USEJVCL}
   private
+    FBehavior: TJvSysReqBehavior;
     FEnabled: Boolean;
-    FMinColorDepth: TglMinColorDepth;
-    FMinGraphicResolution: TglMinGraphicResolution;
-    FOSPlatform: TglOSSet;
-    FSystemFont: TglSystemFont;
-    FMinVideoVRefreshRate: TglMinVideoVRefreshRate;
-    FBehavior: TglSysReqBehavior;
-    FOnWarning: TglWarningEvent;
+    FMinColorDepth: Integer;
+    FMaxColorDepth: Integer;
+    FMinScreenX: Integer;
+    FMaxScreenX: Integer;
+    FMinScreenY: Integer;
+    FMaxScreenY: Integer;
+    FSystemFonts: TJvSystemFontSet;
+    FWindowsVersions: TWindowsVersionSet;
+    FMinVideoRefreshRate: Integer;
+    FMaxVideoRefreshRate: Integer;
+    FOnWarning: TJvWarningEvent;
   protected
     procedure Loaded; override;
+    function TestRequirements(var ReportMessage: string): Boolean; dynamic;
   public
     constructor Create(AOwner: TComponent); override;
-    function TestRequirements(var ReportMessage: string): Boolean;
   published
+    property Behavior: TJvSysReqBehavior read FBehavior write FBehavior default fsbHalt;
     property Enabled: Boolean read FEnabled write FEnabled default True;
-    property MinVideoVRefreshRate: TglMinVideoVRefreshRate read
-      FMinVideoVRefreshRate write FMinVideoVRefreshRate;
-    property MinGraphicResolution: TglMinGraphicResolution read
-      FMinGraphicResolution write FMinGraphicResolution;
-    property MinColorDepth: TglMinColorDepth read FMinColorDepth write
-      FMinColorDepth;
-    property SystemFont: TglSystemFont read FSystemFont write FSystemFont;
-    //    property Processor: TglProcessor;
-    property OSPlatform: TglOSSet read FOSPlatform write FOSPlatform;
-    property Behavior: TglSysReqBehavior read FBehavior write FBehavior;
-    property OnWarning: TglWarningEvent read FOnWarning write FOnWarning;
+    property MinColorDepth: Integer read FMinColorDepth write FMinColorDepth default 0;
+    property MaxColorDepth: Integer read FMaxColorDepth write FMaxColorDepth default 0;
+    property MinScreenX: Integer read FMinScreenX write FMinScreenX default 0;
+    property MaxScreenX: Integer read FMaxScreenX write FMaxScreenX default 0;
+    property MinScreenY: Integer read FMinScreenY write FMinScreenY default 0;
+    property MaxScreenY: Integer read FMaxScreenY write FMaxScreenY default 0;
+    property MinVideoRefreshRate: Integer read FMinVideoRefreshRate write FMinVideoRefreshRate default 0;
+    property MaxVideoRefreshRate: Integer read FMaxVideoRefreshRate write FMaxVideoRefreshRate default 0;
+    property WindowsVersions: TWindowsVersionSet read FWindowsVersions write FWindowsVersions default AllWindowsVersions;
+    property SystemFonts: TJvSystemFontSet read FSystemFonts write FSystemFonts default AllSystemFonts;
+    property OnWarning: TJvWarningEvent read FOnWarning write FOnWarning;
   end;
 
-{$IFDEF USEJVCL}
 {$IFDEF UNITVERSIONING}
 const
   UnitVersioning: TUnitVersionInfo = (
@@ -103,41 +98,34 @@ const
     LogPath: 'JVCL\run'
   );
 {$ENDIF UNITVERSIONING}
-{$ENDIF USEJVCL}
 
 implementation
 
-{$IFDEF USEJVCL}
 uses
   JvResources;
-{$ENDIF USEJVCL}
-
-{$IFNDEF USEJVCL}
-resourcestring
-(* RUSSIAN
-  RsVideoVRefreshRate = 'Частота обновления экрана должна быть %d герц или выше. Измените частоту обновления в свойствах экрана.';
-  RsGraphicResolution = 'Разрешение экрана должно быть %s точек или выше. Измените разрешение в свойствах экрана.';
-  RsColorDepth = 'Количество цветов экрана должно быть %s цветов или выше. Измените число цветов в свойствах экрана.';
-  RsSystemFont = 'В системе должен быть установлен %s шрифт. Измените вид шрифта в свойствах экрана.';
-  RsOSPlatform = 'Для работы программы необходима операционная система %s.';
-*)
-  RsVideoVRefreshRate = 'The monitor refresh rate should be %d Hertz or higher. Change monitor refresh rate in Monitor Control Panel.';
-  RsGraphicResolution = 'The screen resolution should be equal %s pixels or higher. Change screen resolution in Monitor Control Panel.';
-  RsColorDepth = 'The number of colors of the screen should be equal to %s colors or higher. Change screen colors in Monitor Control Panel.';
-  RsSystemFont = 'In system the small font should be established. Change to small fonts in Monitor Control Panel.';
-  RsOSPlatform = 'The program requires %s or better.';
-{$ENDIF !USEJVCL}
 
 constructor TJvgSysRequirements.Create(AOwner: TComponent);
 begin
   inherited Create(AOwner);
+  FBehavior := fsbHalt;
   FEnabled := True;
+  FMinColorDepth := 0;
+  FMaxColorDepth := 0;
+  FMinScreenX := 0;
+  FMaxScreenX := 0;
+  FMinScreenY := 0;
+  FMaxScreenY := 0;
+  FMinVideoRefreshRate := 0;
+  FMaxVideoRefreshRate := 0;
+  FWindowsVersions := AllWindowsVersions;
+  FSystemFonts := AllSystemFonts;
 end;
 
 procedure TJvgSysRequirements.Loaded;
 var
   ReportMessage: string;
   DoShowWarning, DoHalt: Boolean;
+  Icon: Integer;
 begin
   inherited Loaded;
   if not (csDesigning in ComponentState) then
@@ -147,9 +135,13 @@ begin
       DoHalt := Behavior = fsbHalt;
       if Assigned(FOnWarning) then
         FOnWarning(Self, ReportMessage,DoShowWarning, DoHalt);
+      if DoHalt then
+        Icon := MB_ICONWARNING
+      else
+        Icon := MB_ICONERROR;
       if DoShowWarning then
         Application.MessageBox(PChar(ReportMessage),
-          PChar(ExtractFilename(ParamStr(0))), MB_OK + MB_ICONINFORMATION);
+          PChar(ExtractFileName(ParamStr(0))), MB_OK + Icon);
       if DoHalt then
         Application.Terminate;
     end;
@@ -158,7 +150,7 @@ end;
 function TJvgSysRequirements.TestRequirements(var ReportMessage: string): Boolean;
 var
   DC: HDC;
-  OSVersionInfo: TOSVersionInfo;
+  Refresh, ScreenX, ScreenY, BitsPerPixel, LogPixels: Integer;
 
   procedure Test(Value: Boolean; const ErrMsg: string);
   begin
@@ -166,71 +158,47 @@ var
     if not Value then
     begin
       if ReportMessage <> '' then
-        ReportMessage := ReportMessage + #13#10#13#10;
+        ReportMessage := ReportMessage + sLineBreak;
       ReportMessage := ReportMessage + ErrMsg;
     end;
+  end;
+
+  procedure TestMinMax(TestVal, MinVal, MaxVal: Integer; const MinMsg, MaxMsg, BetweenMsg: string);
+  begin
+    if (MinVal > 0) or (MaxVal > 0) then
+      if (MinVal > 0) and (MaxVal > 0) then
+        Test((TestVal >= MinVal) and (TestVal <= MaxVal), Format(BetweenMsg, [TestVal, MinVal, MaxVal]))
+      else
+      if MinVal > 0 then
+        Test(TestVal >= MinVal, Format(MinMsg, [TestVal, MinVal]))
+      else
+        Test(TestVal <= MaxVal, Format(MaxMsg, [TestVal, MaxVal]));
   end;
 
 begin
   ReportMessage := '';
   Result := True;
+
   DC := GetDC(HWND_DESKTOP);
-
-  case MinVideoVRefreshRate of
-    frr70Hertz:
-      Test(GetDeviceCaps(DC, VREFRESH) >= 70, Format(RsVideoVRefreshRate, [70]));
-    frr75Hertz:
-      Test(GetDeviceCaps(DC, VREFRESH) >= 75, Format(RsVideoVRefreshRate, [75]));
-    frr85Hertz:
-      Test(GetDeviceCaps(DC, VREFRESH) >= 85, Format(RsVideoVRefreshRate, [85]));
-  end;
-
-  case MinGraphicResolution of
-    fgr800x600:
-      Test((GetDeviceCaps(DC, HORZRES) >= 800) and
-        (GetDeviceCaps(DC, VERTRES) >= 600), Format(RsGraphicResolution, ['800x600']));
-    fgr1024x768:
-      Test((GetDeviceCaps(DC, HORZRES) >= 1024) and
-        (GetDeviceCaps(DC, VERTRES) >= 768), Format(RsGraphicResolution, ['1024x768']));
-  end;
-
-  case MinColorDepth of
-    fcd16BitColor:
-      Test(GetDeviceCaps(DC, BITSPIXEL) >= 16, Format(RsColorDepth, ['65.536 (hi color)']));
-    fcd32BitColor: Test(GetDeviceCaps(DC, BITSPIXEL) >= 32,
-        Format(RsColorDepth, ['4.294.967.296 (true color)']));
-  end;
-
-  case SystemFont of
-    fsfSmallFont:
-      Test(GetDeviceCaps(DC, LOGPIXELSX) = 96, Format(RsSystemFont, ['small']));
-    fsfBigFont:
-      Test(GetDeviceCaps(DC, LOGPIXELSX) = 120, Format(RsSystemFont, ['large']));
-  end;
-
-  OSVersionInfo.dwOSVersionInfoSize := SizeOf(OSVersionInfo);
-
-  GetVersionEx(OSVersionInfo);
-
-  if OSPlatform = [fosWindowsNT] then
-    Test(OSVersionInfo.dwPlatformId = VER_PLATFORM_WIN32_NT,
-      Format(RsOSPlatform, ['Windows NT/2000']));
-  if OSPlatform = [fosWindows95] then
-    Test(OSVersionInfo.dwPlatformId = VER_PLATFORM_WIN32_WINDOWS,
-      Format(RsOSPlatform, ['Windows 95/98']));
-
-  {  fHalt := fHalt or fsrVideoVRefreshRate in HaltOptions;
-    fHalt := fHalt or fsrGraphicResolution in HaltOptions;
-    fHalt := fHalt or fsrColorDepth in HaltOptions;
-    fHalt := fHalt or fsrSystemFont in HaltOptions;
-    fHalt := fHalt or fsrOSPlatform in HaltOptions;
-  }
-  //  IsProcessorFeaturePresent(PF_MMX_INSTRUCTIONS_AVAILABLE)
-
+  BitsPerPixel := GetDeviceCaps(DC, BITSPIXEL);
+  ScreenX := GetDeviceCaps(DC, HORZRES);
+  ScreenY := GetDeviceCaps(DC, VERTRES);
+  Refresh := GetDeviceCaps(DC, VREFRESH);
+  LogPixels := GetDeviceCaps(DC, LOGPIXELSX);
   ReleaseDC(HWND_DESKTOP, DC);
+
+  TestMinMax(BitsPerPixel, MinColorDepth, MaxColorDepth, RsMinColorDepthReq, RsMaxColorDepthReq, RsBetweenColorDepthReq);
+  TestMinMax(ScreenX, MinScreenX, MaxScreenX, RsMinScreenXReq, RsMaxScreenXReq, RsBetweenScreenXReq);
+  TestMinMax(ScreenY, MinScreenY, MaxScreenY, RsMinScreenYReq, RsMaxScreenYReq, RsBetweenScreenYReq);
+  TestMinMax(Refresh, MinVideoRefreshRate, MaxVideoRefreshRate, RsMinRefreshReq, RsMaxRefreshReq, RsBetweenRefreshReq);
+  if not (wvUnknown in WindowsVersions) then
+    Test(GetWindowsVersion in WindowsVersions, RsWindowsVersionReq);
+  if SystemFonts = [fsfSmallFont] then
+    Test(LogPixels = 96, RsSystemFontSmallReq);
+  if SystemFonts = [fsfBigFont] then
+    Test(LogPixels = 120, RsSystemFontBigReq);
 end;
 
-{$IFDEF USEJVCL}
 {$IFDEF UNITVERSIONING}
 initialization
   RegisterUnitVersion(HInstance, UnitVersioning);
@@ -238,6 +206,5 @@ initialization
 finalization
   UnregisterUnitVersion(HInstance);
 {$ENDIF UNITVERSIONING}
-{$ENDIF USEJVCL}
 
 end.
