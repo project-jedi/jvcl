6e1d943b5dc3aadf332afd2543bafa276341bd0e
marquardt <marquardt@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2005-09-15 08:07:43 +0000

    *** empty log message ***

---
+++ run/JvComponentBase.pas
@@ -0,0 +1,81 @@
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/MPL-1.1.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: JvComponent.PAS, released on 2000-09-22.
+
+The Initial Developer of the Original Code is Joe Doe .
+Portions created by Joe Doe are Copyright (C) 1999 Joe Doe.
+Portions created by XXXX Corp. are Copyright (C) 1998, 1999 XXXX Corp.
+All Rights Reserved.
+
+Contributor(s): -
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
+located at http://jvcl.sourceforge.net
+
+Known Issues:
+-----------------------------------------------------------------------------}
+// $Id$
+
+unit JvComponentBase;
+
+{$I jvcl.inc}
+
+interface
+
+uses
+  {$IFDEF UNITVERSIONING}
+  JclUnitVersioning,
+  {$ENDIF UNITVERSIONING}
+  Classes,
+  {$IFDEF USE_DXGETTEXT}
+  JvGnugettext,
+  {$ENDIF USE_DXGETTEXT}
+  Windows, Messages, Controls,
+  {$IFDEF VisualCLX}
+  Qt, QGraphics, QStdCtrls, // TOwnerDrawState 
+  {$ENDIF VisualCLX}
+  JvConsts,
+  JVCLVer;
+
+type
+  TJvComponent = class(TComponent)
+  private
+    FAboutJVCL: TJVCLAboutInfo;
+  published
+    {$IFDEF VCL}
+    property AboutJVCL: TJVCLAboutInfo read FAboutJVCL write FAboutJVCL stored False;
+    {$ENDIF VCL}
+    {$IFDEF VisualCLX}
+    property AboutJVCLX: TJVCLAboutInfo read FAboutJVCL write FAboutJVCL stored False;
+    {$ENDIF VisualCLX}
+  end;
+
+{$IFDEF UNITVERSIONING}
+const
+  UnitVersioning: TUnitVersionInfo = (
+    RCSfile: '$RCSfile$';
+    Revision: '$Revision$';
+    Date: '$Date$';
+    LogPath: 'JVCL\run'
+  );
+{$ENDIF UNITVERSIONING}
+
+implementation
+
+{$IFDEF UNITVERSIONING}
+initialization
+  RegisterUnitVersion(HInstance, UnitVersioning);
+
+finalization
+  UnregisterUnitVersion(HInstance);
+{$ENDIF UNITVERSIONING}
+
+end.
