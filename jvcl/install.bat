d43bb6f4cc9d269917531112ffe411d65f2c92d4
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2005-09-17 21:48:44 +0000

    Added call to pretest.bat

--- jvcl/install.bat
+++ jvcl/install.bat
@@ -5,6 +5,7 @@ if "%1" == "" SET DELPHIVERSION=newest
 
 cd packages\bin
 SET INSTALLOPTIONS="%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
+call pretest.bat
 build.exe %DELPHIVERSION% "--make=installer"
 cd ..\..
 SET INSTALLOPTIONS=
