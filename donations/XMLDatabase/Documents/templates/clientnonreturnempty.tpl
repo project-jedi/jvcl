44c367b8e1b7726bb52108193783157a03508994
(no author) <(no author)@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2005-10-30 19:13:25 +0000

    This commit was manufactured by cvs2svn to create branch 'JVCL3_10_PREPARATION'.

--- donations/XMLDatabase/Documents/templates/clientnonreturnempty.tpl
+++
@@ -1,5 +0,0 @@
-
-<tr>
- <td colspan="3">Aucun élément a afficher</td>
-</tr>
-
--- tests/RALib/interpreter/Languages/russian/res.bat
+++
@@ -1,7 +0,0 @@
-brcc32.exe rafd.rc
-copy JvCtlConst.pas ..\..\Lib\JvCtlConst.pas
-copy JvDBConst.pas ..\..\Lib\JvDBConst.pas
-copy JvInterpreterConst.pas ..\..\Lib\JvInterpreterConst.pas
-copy rafd.res ..\..\Lib\rafd.res
-copy packconst.pas ..\..\DPacks\packconst.pas
-copy ..\all\rafd2.res ..\..\Lib\rafd2.res
