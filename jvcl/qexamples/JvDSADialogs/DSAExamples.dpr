c59edabd57517c52fe8ea6b4ccf62ef89f701159
asnepvangers <asnepvangers@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2005-11-01 21:03:07 +0000

    For VisualCLX die-hards: updates

---
+++ jvcl/qexamples/JvDSADialogs/DSAExamples.dpr
@@ -0,0 +1,22 @@
+{******************************************************************************}
+{* WARNING:  JEDI VCL To CLX Converter generated unit.                        *}
+{*           Manual modifications will be lost on next release.               *}
+{******************************************************************************}
+
+program DSAExamples;
+
+uses
+  QForms,
+  DSAExamplesCustom1 in 'DSAExamplesCustom1.pas' {frmDSAExamplesCustomDlg1},
+  DSAExamplesCustom2 in 'DSAExamplesCustom2.pas' {frmDSAExamplesCustomDlg2},
+  DSAExamplesProgressDlg in 'DSAExamplesProgressDlg.pas' {frmDSAExamplesProgressDlg},
+  DSADialogsMainFormU in 'DSADialogsMainFormU.pas' {DSADialogsMainForm};
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  Application.Title := 'Don''t Show Again (DSA) Examples and tests';
+  Application.CreateForm(TDSADialogsMainForm, DSADialogsMainForm);
+  Application.Run;
+end.
---
+++ jvcl/qexamples/JvDSADialogs/DSAExamplesProgressDlg.pas
@@ -0,0 +1,125 @@
+{******************************************************************************}
+{* WARNING:  JEDI VCL To CLX Converter generated unit.                        *}
+{*           Manual modifications will be lost on next release.               *}
+{******************************************************************************}
+
+{******************************************************************
+
+                       JEDI-VCL Demo
+
+ Copyright (C) 2002 Project JEDI
+
+ Original author:
+
+ Contributor(s):
+
+ You may retrieve the latest version of this file at the JEDI-JVCL
+ home page, located at http://jvcl.sourceforge.net
+
+ The contents of this file are used with permission, subject to
+ the Mozilla Public License Version 1.1 (the "License"); you may
+ not use this file except in compliance with the License. You may
+ obtain a copy of the License at
+ http://www.mozilla.org/MPL/MPL-1_1Final.html
+
+ Software distributed under the License is distributed on an
+ "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ implied. See the License for the specific language governing
+ rights and limitations under the License.
+
+******************************************************************}
+
+unit DSAExamplesProgressDlg;
+
+interface
+
+uses
+  QWindows, QMessages, SysUtils, Classes, QGraphics, QControls, QForms, QDialogs,
+  QStdCtrls, QComCtrls, JvQProgressBar, JvQExComCtrls;
+
+type
+  TMsgProgressCallback = function(const Position, Max: Integer): Integer of object;
+
+  TfrmDSAExamplesProgressDlg = class(TForm)
+    lblItemCount: TLabel;
+    edItems: TEdit;
+    lblNote: TLabel;
+    btnStart: TButton;
+    btnCancel: TButton;
+    pbProgress: TJvProgressBar;
+    procedure FormResize(Sender: TObject);
+    procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+    procedure btnStartClick(Sender: TObject);
+  private
+    { Private declarations }
+    DoMessage: TMsgProgressCallback;
+  end;
+
+procedure DoProgress(const MessageCallback: TMsgProgressCallback);
+
+implementation
+
+uses
+  JvQDSADialogs;
+
+{$R *.xfm}
+
+procedure DoProgress(const MessageCallback: TMsgProgressCallback);
+begin
+  with TfrmDSAExamplesProgressDlg.Create(Screen.ActiveForm) do
+  try
+    DoMessage := MessageCallback;
+    ShowModal;
+  finally
+    Free;
+  end;
+end;
+
+{ TfrmDSAExamplesProgressDlg }
+
+procedure TfrmDSAExamplesProgressDlg.FormResize(Sender: TObject);
+var
+  TotCtrlW: Integer;
+  Distance: Integer;
+begin
+  TotCtrlW := btnCancel.Left + btnCancel.Width - btnStart.Left;
+  Distance := btnCancel.Left - btnStart.Left;
+  btnStart.Left := (ClientWidth - TotCtrlW) div 2;
+  btnCancel.Left := btnStart.Left + Distance;
+end;
+
+procedure TfrmDSAExamplesProgressDlg.FormCloseQuery(Sender: TObject;
+  var CanClose: Boolean);
+begin
+  CanClose := btnStart.Enabled;
+end;
+
+procedure TfrmDSAExamplesProgressDlg.btnStartClick(Sender: TObject);
+var
+  I: Integer;
+begin
+  btnStart.Enabled := False;
+  pbProgress.Position := 0;
+  pbProgress.Max := StrToInt(edItems.Text);
+  Application.ProcessMessages;
+  try
+    I := 1;
+    while I <= pbProgress.Max do
+    begin
+      pbProgress.Position := I;
+      Application.ProcessMessages;
+      if DoMessage(I, pbProgress.Max) in [mrNo, mrCancel] then
+      begin
+        ShowMessage('Canceled.', dckActiveForm);
+        Break;
+      end;
+      Inc(I);
+    end;
+  finally
+    pbProgress.Position := 0;
+    btnStart.Enabled := True;
+    DSAQueueStore.Clear;
+  end;
+end;
+
+end.
