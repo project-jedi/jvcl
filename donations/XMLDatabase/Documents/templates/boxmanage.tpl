bb6002c64fe5f5cce96de2512f5a85032e6aa233
(no author) <(no author)@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2006-02-21 22:19:21 +0000

    This commit was manufactured by cvs2svn to create tag 'JVCL3_20'.

--- donations/XMLDatabase/Documents/templates/boxmanage.tpl
+++
@@ -1,7 +0,0 @@
-
-<tr>
- <td><a href="box.xhs?id=%BoxId%">%BoxName%</a></td>
- <td>%StatName%</td>
- <td>%BoxPeremption%</td>
-</tr>
-
--- tests/restructured/devtools/bin/MakeImages.bat
+++
@@ -1,7 +0,0 @@
-@echo Extracts BMP images from resource files (res and dcr), builds a RC file and converts BMP to PNG...
-Res2Bmp *.res
-Res2Bmp *.dcr
-MakeRC *.bmp
-rem ren new.rc JVCLReg.rc
-mkdir PNG
-MakePng *.bmp PNG\
