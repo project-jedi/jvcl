6290963b85ca4ed585a95fb8c4ebc15aefc9d3ac
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2006-08-30 13:15:47 +0000

    Mantis 3879: Setting FTabs to nil was a bad idea

--- run/JvTabBar.pas
+++ run/JvTabBar.pas
@@ -1,4 +1,4 @@
-{-----------------------------------------------------------------------------
+﻿{-----------------------------------------------------------------------------
 The contents of this file are subject to the Mozilla Public License
 Version 1.1 (the "License"); you may not use this file except in compliance
 with the License. You may obtain a copy of the License at
@@ -566,8 +566,9 @@ begin
   Painter := nil;
   Images := nil;
   FChangeLink.Free;
-  FreeAndNil(FTabs);
-  FreeAndNil(FScrollButtonBackground);
+  FTabs.Free;
+  FScrollButtonBackground.Free;
+  
   inherited Destroy;
 end;
 
