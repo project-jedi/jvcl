aafcf06411294ecbfb3cfa39df4bea02371fd2f9
jfudickar <jfudickar@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2006-09-25 07:35:08 +0000

    Updated for Mantis 3894 (Mantis #3894)

--- jvcl/examples/JvProgramVersionCheck/Main.pas
+++ jvcl/examples/JvProgramVersionCheck/Main.pas
@@ -6,7 +6,7 @@ uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, JvProgramVersionCheck, JvComponent, JvAppStorage, JvAppIniStorage,
   StdCtrls, JvUrlListGrabber, JvUrlGrabbers, ImgList, JvPropertyStore,
-  IdBaseComponent, IdComponent, IdTCPConnection, IdTCPClient, IdFTP, IdHTTP;
+  JvComponentBase;
 
 type
   TForm1 = class(TForm)
@@ -14,14 +14,15 @@ type
     JvProgramVersionDatabaseLocation1: TJvProgramVersionDatabaseLocation;
     JvProgramVersionFTPLocation1: TJvProgramVersionFTPLocation;
     JvProgramVersionHTTPLocation1: TJvProgramVersionHTTPLocation;
-    JvProgramVersionHTTPLocationIndy1: TJvProgramVersionHTTPLocationIndy;
     JvProgramVersionNetworkLocation1: TJvProgramVersionNetworkLocation;
     Label1: TLabel;
     Label2: TLabel;
     ProgramVersionCheck: TJvProgramVersionCheck;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
     procedure FormShow(Sender: TObject);
   public
-    procedure VersionCheck;
+    procedure VersionCheck;            
   end;
 
 var
@@ -32,6 +33,11 @@ implementation
 {$R *.dfm}
 
 Uses JclFileUtils, JvTypes;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  VersionCheck;
+end;
 
 procedure TForm1.FormShow(Sender: TObject);
 begin
