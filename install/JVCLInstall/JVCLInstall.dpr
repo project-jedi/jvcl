ad9069991091d3f30664c913943335b11c877d1c
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2006-12-16 22:25:34 +0000

    Jvcl.inc include directive correction

--- install/JVCLInstall/JVCLInstall.dpr
+++ install/JVCLInstall/JVCLInstall.dpr
@@ -24,9 +24,9 @@ Known Issues:
 -----------------------------------------------------------------------------}
 // $Id$
 
-{$I jvcl.inc}
-
 program JVCLInstall;
+
+{$I jvcl.inc}
 
 uses
   //FastMM4,
