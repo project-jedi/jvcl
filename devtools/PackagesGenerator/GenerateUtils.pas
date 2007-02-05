63b215231699e6b7e8ea464b4e7cd090c9c76962
marquardt <marquardt@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2007-02-05 05:19:51 +0000

    made WARNINGS OFF unconditionally, showed up on D6 also

--- devtools/PackagesGenerator/GenerateUtils.pas
+++ devtools/PackagesGenerator/GenerateUtils.pas
@@ -1143,9 +1143,7 @@ begin
     Result := xml.Description;
 end;
 
-{$IFDEF COMPILER10}
 {$WARNINGS OFF} // hide wrong warning: "Function return value could be undefined."
-{$ENDIF COMPILER10}
 function ApplyTemplateAndSave(const path, target, package, extension: string;
   template : TStrings; xml : TPackageXmlInfo;
   const templateName, xmlName : string): string;
@@ -1574,9 +1572,7 @@ begin
     outFile.Free;
   end;
 end;
-{$IFDEF COMPILER10}
 {$WARNINGS ON}
-{$ENDIF COMPILER10}
 
 function Max(d1, d2 : TDateTime): TDateTime;
 begin
