6a27ae1bd21d1f7a382edb57fe55cb3f9a6bb962
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2007-09-17 23:13:41 +0000

    Fixed missing MPL header and UnitVersioning

--- run/JvDBCheckBox.pas
+++ run/JvDBCheckBox.pas
@@ -1,8 +1,38 @@
+{-----------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/MPL-1.1.html
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: JvDBCheckBox.pas, released on 2007-06-28.
+
+The Initial Developer of the Original Code is Andreas Hausladen [andreas dott hausladen  att gmx dott de]
+Portions created by Andreas Hausladen are Copyright (C) 2007 Andreas Hausladen.
+All Rights Reserved.
+
+Contributor(s): -
+
+You may retrieve the latest version of this file at the Project JEDI's JVCL home page,
+located at http://jvcl.sourceforge.net
+
+Known Issues:
+-----------------------------------------------------------------------------}
+// $Id$
+
 unit JvDBCheckBox;
+
+{$I jvcl.inc}
 
 interface
 
 uses
+  {$IFDEF UNITVERSIONING}
+  JclUnitVersioning,
+  {$ENDIF UNITVERSIONING}
   Windows, Messages, SysUtils, Classes, Contnrs, Controls, DB, DBCtrls;
 
 type
@@ -34,6 +64,16 @@ type
     property OnChanging: TJvDBCheckBoxChangingEvent read FOnChanging write FOnChanging;
     property OnChange: TNotifyEvent read FOnChange write FOnChange;
   end;
+
+{$IFDEF UNITVERSIONING}
+const
+  UnitVersioning: TUnitVersionInfo = (
+    RCSfile: '$URL$';
+    Revision: '$Revision$';
+    Date: '$Date$';
+    LogPath: 'JVCL\run'
+    );
+{$ENDIF UNITVERSIONING}
 
 implementation
 
@@ -121,5 +161,14 @@ begin
   end;
 end;
 
-end.
+initialization
+  {$IFDEF UNITVERSIONING}
+  RegisterUnitVersion(HInstance, UnitVersioning);
+  {$ENDIF UNITVERSIONING}
 
+finalization
+  {$IFDEF UNITVERSIONING}
+  UnregisterUnitVersion(HInstance);
+  {$ENDIF UNITVERSIONING}
+
+end.
