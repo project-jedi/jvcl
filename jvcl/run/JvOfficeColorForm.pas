87d6fa0852219d1b45727d9a58f4c23d74044f44
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2007-10-25 16:31:32 +0000

    Made JvOfficeColorForm more generic => JvOfficeDragBarForm

--- jvcl/run/JvOfficeColorForm.pas
+++ jvcl/run/JvOfficeColorForm.pas
@@ -38,81 +38,17 @@ uses
   {$ENDIF UNITVERSIONING}
   SysUtils, Classes,
   Windows, Messages, Graphics, Controls, Forms, StdCtrls, ExtCtrls,
-  JvConsts, JvComponent, JvOfficeColorPanel;
+  JvConsts, JvComponent, JvOfficeColorPanel, JvOfficeDragBarForm;
 
 {------------------------------------------------------------------------------}
-const
-  MinDragBarHeight = 7;
-  MinDragBarSpace = 3;
-
-  Tag_DragBarHeight = 9;
-  Tag_DragBarSpace = 10;
-
-  JvDefaultSubDragBarActiveColor = clActiveCaption;
-  JvDefaultSubDragBarInactiveColor = clInactiveCaption;
 
 type
-  TJvOfficeColorPanelDragBar = class(TJvWinControl)
+  TJvOfficeColorForm = class(TJvOfficeDragBarForm)
   private
-    FOwnerForm: TControl;
-  protected
-    procedure MouseEnter(Control: TControl); override;
-    procedure MouseLeave(Control: TControl); override;
-    procedure MouseMove(Shift: TShiftState; X, Y: Integer); override;
-    procedure MouseDown(Button: TMouseButton; Shift: TShiftState;
-      X, Y: Integer); override;
-    procedure MouseUp(Button: TMouseButton; Shift: TShiftState;
-      X, Y: Integer); override;
-  end;
-
-  TJvOfficeColorForm = class(TJvForm)
-  private
-    FBusy: Boolean;
-    FInited: Boolean;
-    FWordStyle: Boolean;
-    FColorPanel: TJvCustomOfficeColorPanel;
-    FDragBar: TJvOfficeColorPanelDragBar;
-    FDragBarSpace: Integer;
-    FDragBarHeight: Integer;
-    FFlat: Boolean;
-    FToolWindowStyle: Boolean;
-    FOnShowingChanged: TNotifyEvent;
-    FOnKillFocus: TNotifyEvent;
-    FOnWindowStyleChanged: TNotifyEvent;
-    FShowDragBar: Boolean;
-    FDragBarHint: string;
-    procedure FormDeactivate(Sender: TObject);
-    procedure FormKeyUp(Sender: TObject; var Key: Word; Shift: TShiftState);
-    procedure SetMeasure(const Index, Value: Integer);
-    procedure SetFlat(const Value: Boolean);
-    procedure SetWordStyle(const Value: Boolean);
-    procedure SetToolWindowStyle(const Value: Boolean);
-    procedure SetShowDragBar(const Value: Boolean);
-    procedure SetDragBarHint(const Value: string);
-  protected
-    DropDownMoved: Boolean;
-    DropDownMoving: Boolean;
-    MoveEnd: Boolean;
-    MoveStart: Boolean;
-    procedure Resize; override;
-    procedure VisibleChanged; override;
-    procedure CreateParams(var Params: TCreateParams); override;
-    procedure Paint; override;
-    procedure FocusKilled(NextWnd: THandle); override;
-    procedure ShowingChanged; override;
-    property OnShowingChanged: TNotifyEvent read FOnShowingChanged write FOnShowingChanged;
-    property OnKillFocus: TNotifyEvent read FOnKillFocus write FOnKillFocus;
-    property OnWindowStyleChanged: TNotifyEvent read FOnWindowStyleChanged write FOnWindowStyleChanged;
+    function GetColorPanel: TJvCustomOfficeColorPanel;
   public
-    constructor Create(AOwner: TComponent;AOfficeColorPanelClass: TJvOfficeColorPanelClass); reintroduce; virtual;
-    procedure AdjustColorForm;
-    property ColorPanel: TJvCustomOfficeColorPanel read FColorPanel;
-    property ShowDragBar: Boolean read FShowDragBar write SetShowDragBar default True;
-    property DragBarHeight: Integer index Tag_DragBarHeight read FDragBarHeight write SetMeasure;
-    property DragBarHint: string read FDragBarHint write SetDragBarHint;
-    property DragBarSpace: Integer index Tag_DragBarSpace read FDragBarSpace write SetMeasure;
-    property ToolWindowStyle: Boolean read FToolWindowStyle write SetToolWindowStyle default False;
-    property Flat: Boolean read FFlat write SetFlat;
+    constructor Create(AOwner: TComponent; AOfficeColorPanelClass: TJvOfficeColorPanelClass); reintroduce; virtual;
+    property ColorPanel: TJvCustomOfficeColorPanel read GetColorPanel;
   end;
 
 {$IFDEF UNITVERSIONING}
@@ -127,8 +63,6 @@ const
 
 implementation
 
-uses
-  JvResources;
 type
   TJvOfficeColorPanelAccessProtected = class(TJvOfficeColorPanel);
 
@@ -136,355 +70,20 @@ type
 
 constructor TJvOfficeColorForm.Create(AOwner: TComponent;
   AOfficeColorPanelClass: TJvOfficeColorPanelClass);
-var
-  ParentControl: TWinControl;
 begin
-  inherited CreateNew(AOwner);
-  HintColor := Application.HintColor;
-  FInited := False;
-  FShowDragBar := True;
-//  Scaled := False;
-  AutoScroll := False;
-  BorderIcons := [];
-  BorderStyle := bsDialog;
-  BorderWidth := 0;
-  AutoSize := True;
-  FormStyle := fsStayOnTop;
-
-  FToolWindowStyle := False;
-  ParentControl := Self;
-
-  FDragBar := TJvOfficeColorPanelDragBar.Create(Self);
-  with FDragBar do
+  inherited Create(AOwner);
+  SetClient(AOfficeColorPanelClass.Create(Self));
+  with TJvOfficeColorPanelAccessProtected(ColorPanel) do
   begin
-    Parent := ParentControl;
-    FOwnerForm := Self;
-    AutoSize := False;
-    Caption := '';
-    Color := JvDefaultSubDragBarActiveColor;
-    Height := MinDragBarHeight;
-    ShowHint := True;
-  end;
-
-  FColorPanel := AOfficeColorPanelClass.Create(Self);
-  with TJvOfficeColorPanelAccessProtected(FColorPanel) do
-  begin
-    Parent := ParentControl;
+    Parent := Self;
     FlatBorder := True;
     BorderWidth := 0;
   end;
-
-  SetWordStyle(True);
-  KeyPreview := True;
-  OnDeactivate := FormDeactivate;
-  OnKeyUp := FormKeyUp;
-
-  FInited := True;
 end;
 
-
-procedure TJvOfficeColorForm.CreateParams(var Params: TCreateParams);
+function TJvOfficeColorForm.GetColorPanel: TJvCustomOfficeColorPanel;
 begin
-  inherited CreateParams(Params);
-  with Params do
-  begin
-    Style := Style and not WS_CAPTION;
-  end;
-end;
-
-
-
-
-procedure TJvOfficeColorForm.Paint;
-
-begin
-  inherited Paint;
-
-end;
-
-procedure TJvOfficeColorForm.FormKeyUp(Sender: TObject; var Key: Word;
-  Shift: TShiftState);
-begin
-  if Key = VK_ESCAPE then
-  begin
-    Hide;
-    ModalResult := mrCancel;
-  end;
-end;
-
-procedure TJvOfficeColorForm.AdjustColorForm;
-var
-  TempHeight: Integer;
-  HasDragBar: Boolean;
-  Offset: Integer;
-begin
-  if not FInited or FBusy then
-    Exit;
-  FBusy := True;
-
-  DisableAlign;
-
-  if ShowDragBar and not ToolWindowStyle then
-  begin
-    FDragBar.Visible := True;
-    HasDragBar := FDragBar.Visible;
-    FDragBar.Height := FDragBarHeight;
-  end
-  else
-  begin
-    HasDragBar := False;
-    FDragBar.Visible := False;
-  end;
-
-  Offset := 0;
-
-  if HasDragBar then
-    TempHeight := FDragBarHeight + FDragBarSpace * 2
-  else
-    TempHeight := 0;
-
-  ClientHeight := TempHeight + FColorPanel.ClientHeight + Offset * 2;
-
-  Width := FColorPanel.Width + Offset * 2;
-
-
-  if FDragBar.Visible then
-    FDragBar.SetBounds(Offset, FDragBarSpace + Offset, FColorPanel.Width, FDragBarHeight);
-
-  FColorPanel.SetBounds(Offset, TempHeight + 1, FColorPanel.Width, FColorPanel.Height);
-  FBusy := False;
-end;
-
-procedure TJvOfficeColorForm.SetMeasure(const Index, Value: Integer);
-var
-  MeasureItem: PInteger;
-  MeasureConst: Integer;
-begin
-  case Index of
-    Tag_DragBarHeight:
-      begin
-        MeasureItem := @FDragBarHeight;
-        MeasureConst := MinDragBarHeight;
-      end;
-    Tag_DragBarSpace:
-      begin
-        MeasureItem := @FDragBarSpace;
-        MeasureConst := MinDragBarSpace;
-      end;
-  else
-    Exit;
-  end;
-  if MeasureItem^ = Value then
-    Exit;
-
-  MeasureItem^ := Value;
-  FWordStyle := False;
-  if MeasureItem^ < MeasureConst then
-    MeasureItem^ := MeasureConst;
-  AdjustColorForm;
-end;
-
-procedure TJvOfficeColorForm.SetFlat(const Value: Boolean);
-begin
-  if FFlat <> Value then
-  begin
-    FFlat := Value;
-  end;
-end;
-
-procedure TJvOfficeColorForm.SetToolWindowStyle(const Value: Boolean);
-begin
-  if ShowDragBar then
-  begin
-    FToolWindowStyle := Value;
-    if Value then
-    begin
-      BorderIcons := [biSystemMenu];
-      BorderStyle := bsToolWindow;
-      {$IFDEF COMPILER10_UP}
-      if HandleAllocated then // BDS 2006 bug
-        RecreateWnd;
-      {$ENDIF COMPILER10_UP}
-      FDragBar.Visible := False;
-    end
-    else
-    begin
-      BorderIcons := [];
-      BorderStyle := bsDialog;
-      {$IFDEF COMPILER10_UP}
-      if HandleAllocated then // BDS 2006 bug
-        RecreateWnd;
-      {$ENDIF COMPILER10_UP}
-      FDragBar.Visible := True;
-    end;
-    if not DropDownMoving then
-      AdjustColorForm;
-    if Assigned(FOnWindowStyleChanged) then
-      FOnWindowStyleChanged(Self);
-  end
-  else
-  begin
-    FToolWindowStyle := False;
-    BorderIcons := [];
-    BorderStyle := bsDialog;
-    {$IFDEF COMPILER10_UP}
-    if HandleAllocated then // BDS 2006 bug
-      RecreateWnd;
-    {$ENDIF COMPILER10_UP}
-    FDragBar.Visible := False;
-  end;
-end;
-
-procedure TJvOfficeColorForm.ShowingChanged;
-begin
-  inherited ShowingChanged;
-  if Assigned(FOnShowingChanged) then
-    FOnShowingChanged(ActiveControl);
-end;
-
-procedure TJvOfficeColorForm.FocusKilled(NextWnd: THandle);
-begin
-  inherited FocusKilled(NextWnd);
-  if Assigned(FOnKillFocus) and not DropDownMoving then
-    FOnKillFocus(ActiveControl);
-end;
-
-procedure TJvOfficeColorForm.FormDeactivate(Sender: TObject);
-begin
-  MoveStart := False;
-  DropDownMoved := False;
-end;
-
-procedure TJvOfficeColorForm.VisibleChanged;
-begin
-  inherited VisibleChanged;
-  if not Visible then
-  begin
-    if ToolWindowStyle then
-      ToolWindowStyle := False;
-
-    DropDownMoving := False;
-    MoveStart := False;
-    DropDownMoved := False;
-  end;
-end;
-
-procedure TJvOfficeColorForm.SetDragBarHint(const Value: string);
-begin
-  if FDragBarHint<>Value then
-  begin
-    FDragBarHint := Value;
-    FDragBar.Hint := Value;
-  end;
-end;
-
-//=== { TJvOfficeColorPanelDragBar } =========================================
-
-procedure TJvOfficeColorPanelDragBar.MouseDown(Button: TMouseButton; Shift: TShiftState;
-  X, Y: Integer);
-begin
-  inherited MouseDown(Button, Shift, X, Y);
-  if Button = mbLeft then
-  begin
-    TJvOfficeColorForm(FOwnerForm).MoveStart := True;
-  end;
-end;
-
-procedure TJvOfficeColorPanelDragBar.MouseEnter(Control: TControl);
-begin
-  inherited MouseEnter(Control);
-  Color := JvDefaultSubDragBarActiveColor;
-  Cursor := crSizeAll;
-end;
-
-procedure TJvOfficeColorPanelDragBar.MouseLeave(Control: TControl);
-begin
-  inherited MouseLeave(Control);
-  Color := JvDefaultSubDragBarInactiveColor;
-  Cursor := crDefault;
-end;
-
-procedure TJvOfficeColorForm.Resize;
-begin
-  inherited Resize;
-  if FInited then
-    AdjustColorForm;
-end;
-
-procedure TJvOfficeColorForm.SetWordStyle(const Value: Boolean);
-begin
-  if FWordStyle <> Value then
-  begin
-    FWordStyle := Value;
-    if FWordStyle then
-    begin
-      FDragBarHeight := MinDragBarHeight;
-      FDragBarSpace := MinDragBarSpace;
-    end;
-  end;
-end;
-
-
-
-procedure DragControl(WinControl: TWinControl);
-const
-  SM = $F012;
-begin
-  ReleaseCapture;
-  WinControl.Perform(WM_SYSCOMMAND, SM, 0);
-end;
-
-
-
-procedure TJvOfficeColorPanelDragBar.MouseMove(Shift: TShiftState; X, Y: Integer);
-var
-  lOwnerForm: TJvOfficeColorForm;
-begin
-  inherited MouseMove(Shift, X, Y);
-  lOwnerForm := TJvOfficeColorForm(FOwnerForm);
-  if lOwnerForm.MoveStart or lOwnerForm.DropDownMoving then
-  begin
-    if not lOwnerForm.DropDownMoved then
-      lOwnerForm.DropDownMoved := True;
-    if lOwnerForm.MoveStart and not lOwnerForm.ToolWindowStyle then
-    begin
-      lOwnerForm.ToolWindowStyle := True;
-      lOwnerForm.AdjustColorForm;
-    end;
-    DragControl(lOwnerForm);
-
-    lOwnerForm.DropDownMoving := True;
-    lOwnerForm.MoveStart := False;
-
-  end;
-end;
-
-procedure TJvOfficeColorPanelDragBar.MouseUp(Button: TMouseButton; Shift: TShiftState;
-  X, Y: Integer);
-
-begin
-  inherited MouseUp(Button, Shift, X, Y);
-  if Button = mbLeft then
-  begin
-    TJvOfficeColorForm(FOwnerForm).MoveStart := False;
-    TJvOfficeColorForm(FOwnerForm).DropDownMoving := False;
-  end;
-end;
-
-procedure TJvOfficeColorForm.SetShowDragBar(const Value: Boolean);
-begin
-  FShowDragBar := Value;
-  if Value then
-  begin
-    if not DropDownMoved then
-      FDragBar.Visible := True;
-  end
-  else
-  begin
-    if DropDownMoved then
-      SetToolWindowStyle(False);
-  end;
-  AdjustColorForm;
+  Result := TJvCustomOfficeColorPanel(Client);
 end;
 
 {$IFDEF UNITVERSIONING}
