d79a911343249155268008263b51546717e607f8
jfudickar <jfudickar@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2008-01-06 18:43:26 +0000

    Updated Example

--- examples/JvProgramVersionCheck/Main.pas
+++ examples/JvProgramVersionCheck/Main.pas
@@ -5,21 +5,23 @@ interface
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, JvProgramVersionCheck, JvComponent, JvAppStorage, JvAppIniStorage,
-  StdCtrls, JvUrlListGrabber, JvUrlGrabbers, ImgList, JvPropertyStore,
+  StdCtrls, JvUrlListGrabber, JvUrlGrabbers, ImgList, JvPropertyStore,JvAppxmlStorage,
   JvComponentBase;
 
 type
   TForm1 = class(TForm)
     JvAppIniFileStorageVersionCheck: TJvAppIniFileStorage;
-    JvProgramVersionDatabaseLocation1: TJvProgramVersionDatabaseLocation;
-    JvProgramVersionFTPLocation1: TJvProgramVersionFTPLocation;
-    JvProgramVersionHTTPLocation1: TJvProgramVersionHTTPLocation;
-    JvProgramVersionNetworkLocation1: TJvProgramVersionNetworkLocation;
+    JvProgramVersionDatabaseLocation: TJvProgramVersionDatabaseLocation;
+    JvProgramVersionFTPLocation: TJvProgramVersionFTPLocation;
+    JvProgramVersionHTTPLocation: TJvProgramVersionHTTPLocation;
+    JvProgramVersionNetworkLocation: TJvProgramVersionNetworkLocation;
     Label1: TLabel;
     Label2: TLabel;
     ProgramVersionCheck: TJvProgramVersionCheck;
     Button1: TButton;
+    Button2: TButton;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
     procedure FormShow(Sender: TObject);
   public
     procedure VersionCheck;            
@@ -32,11 +34,19 @@ implementation
 
 {$R *.dfm}
 
-Uses JclFileUtils, JvTypes;
+Uses JclFileUtils, JvTypes, JvPropertyStoreEditor;
 
 procedure TForm1.Button1Click(Sender: TObject);
 begin
   VersionCheck;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  if EditPropertyStore(ProgramVersionCheck.RemoteProgramVersionHistory) then
+  begin
+    ProgramVersionCheck.StoreRemoteVersionHistoryToFile (hffini, 'LocalRemoteVersionHistory');
+  end;
 end;
 
 procedure TForm1.FormShow(Sender: TObject);
