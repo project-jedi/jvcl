65e2b8005d7524c2b37ebd3c2e5423b2b5f28cc8
jfudickar <jfudickar@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2008-07-29 22:56:36 +0000

    New Sample Project for the JvPropertyStoreEditor

---
+++ examples/JvPropertyStoreEditor/JvProperyStoreEditorDemo.dpr
@@ -0,0 +1,13 @@
+program JvProperyStoreEditorDemo;
+
+uses
+  Forms,
+  JvProperyStoreEditorMain in 'JvProperyStoreEditorMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
