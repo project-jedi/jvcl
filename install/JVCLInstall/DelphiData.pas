233ab5b611ff57752a6d655993b167556d16880c
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2008-09-21 19:55:36 +0000

    Optimization

--- install/JVCLInstall/DelphiData.pas
+++ install/JVCLInstall/DelphiData.pas
@@ -1264,29 +1264,9 @@ begin
 end;
 
 function TCompileTarget.GetCommonProjectsDir: string;
-var
-  RsVarsOutput, ComSpec: string;
-  Lines: TStrings;
 begin
   if IsBDS and (IDEVersion >= 5) then
-  begin
-    // Inspired by the JCL installer. Maybe all this should be centralized...
-    if GetEnvironmentVar('COMSPEC', ComSpec) and (JclSysUtils.Execute(Format('%s /C "%s%sbin%srsvars.bat && set BDS"',
-      [ComSpec, ExtractShortPathName(RootDir), DirDelimiter, DirDelimiter]), RsVarsOutput) = 0) then
-    begin
-      Lines := TStringList.Create;
-      try
-        Lines.Text := RsVarsOutput;
-        Result := Lines.Values['BDSCOMMONDIR'];
-      finally
-        Lines.Free;
-      end;
-    end;
-
-    if Result = '' then // ignore BDSCOMMONDIR env-var because Delphi and BCB do not know them
-      Result := FCommonProjectsDir;
-    Result := ExcludeTrailingPathDelimiter(FixBackslashBackslash(Result));
-  end
+    Result := FCommonProjectsDir    // Filled in LoadFromRegistry
   else
     Result := GetBDSProjectsDir;
 end;
