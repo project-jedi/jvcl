cf36ec55552b83aabdbe25eff0485e1befaf3211
wpostma <wpostma@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2008-10-28 13:59:29 +0000

    moved out of RaLib

---
+++ jvcl/examples/JvInterpreterDemos/JvInterpreterNotepad/Lib/fIniSections.pas
@@ -0,0 +1,54 @@
+{******************************************************************
+
+                       JEDI-VCL Demo
+
+ Copyright (C) 2002 Project JEDI
+
+ Original author:
+
+ Contributor(s):
+
+ You may retrieve the latest version of this file at the JEDI-JVCL
+ home page, located at http://jvcl.sourceforge.net
+
+ The contents of this file are used with permission, subject to
+ the Mozilla Public License Version 1.1 (the "License"); you may
+ not use this file except in compliance with the License. You may
+ obtain a copy of the License at
+ http://www.mozilla.org/MPL/MPL-1_1Final.html
+
+ Software distributed under the License is distributed on an
+ "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ implied. See the License for the specific language governing
+ rights and limitations under the License.
+
+******************************************************************}
+
+unit fIniSections;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  StdCtrls;
+
+type
+  TIniSections = class(TForm)
+    Button2: TButton;
+    Button1: TButton;
+    ListBox1: TListBox;
+    Label1: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  IniSections: TIniSections;
+
+implementation
+
+{$R *.DFM}
+
+end.
