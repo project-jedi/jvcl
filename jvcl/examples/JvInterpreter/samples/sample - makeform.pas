deb938a3a7e570135279a4bc701944b78a23be84
wpostma <wpostma@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2008-10-28 13:20:27 +0000

    move stuff

--- jvcl/examples/JvInterpreter/samples/sample - makeform.pas
+++
@@ -1,8 +0,0 @@
-var
-  MyForm: TForm;
-begin
-  MyForm := JvInterpreterMakeForm(SamplesDir+'fModalForm.pas');
-  if MyForm.ShowModal = mrOk then
-    showmessage('OK button clicked');
-  MyForm.Free;
-end;
