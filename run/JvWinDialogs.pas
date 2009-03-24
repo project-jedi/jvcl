6a0c4fb3a7c5a3601aa045de05b17f2ca9bdad95
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2009-03-24 20:54:58 +0000

    DoOrganizeFavDlg wants a CHAR*=PAnsiCharand not a TCHAR*=PChar.

--- run/JvWinDialogs.pas
+++ run/JvWinDialogs.pas
@@ -788,7 +788,7 @@ const
   SHFMT_NOFORMAT = $FFFFFFFD; // Drive is not formatable
 
 type
-  LPFNORGFAV = function(Wnd: hWnd; Str: LPTSTR): Integer; stdcall;
+  LPFNORGFAV = function(Wnd: hWnd; Str: LPCSTR): Integer; stdcall;
 
 function ExtractIconFromFile(FileName: string; Index: Integer): HICON;
 var
@@ -807,18 +807,17 @@ end;
 
 function TJvOrganizeFavoritesDialog.Execute: Boolean;
 var
-  Path: string;
+  Path: AnsiString;
   lpfnDoOrganizeFavDlg: LPFNORGFAV;
 begin
   Result := False;
-  //  lpfnDoOrganizeFavDlg := nil;
-  if SHDocvwHandle > HINSTANCE_ERROR then
+  if SHDocvwHandle <> 0 then
   begin
-    Path := GetSpecialFolderPath('Favorites', True) + #0#0;
+    Path := AnsiString(GetSpecialFolderPath('Favorites', True)) + #0#0;
     lpfnDoOrganizeFavDlg := LPFNORGFAV(GetProcAddress(SHDocvwHandle, 'DoOrganizeFavDlg'));
     if not Assigned(lpfnDoOrganizeFavDlg) then
       raise EWinDialogError.CreateRes(@RsEFunctionNotSupported);
-    lpfnDoOrganizeFavDlg(GetForegroundWindow, PChar(Path));
+    lpfnDoOrganizeFavDlg(GetForegroundWindow, PAnsiChar(Path));
     Result := True;
   end;
 end;
@@ -1172,18 +1171,18 @@ begin
     for BufferIndex := 0 to High(Parameters) do
     begin
       PPointer(@ParamBuffer[BufferIndex * SizeOf(Pointer)])^ :=
-      Parameters[High(Parameters) - BufferIndex];
+        Parameters[High(Parameters) - BufferIndex];
     end;
     asm
       mov ECX, ParamCount
       cmp ECX, 0
       je  @MethodCall
       mov EDX, ParamBuffer
-      @StartLoop:
+@StartLoop:
       push DWORD PTR[EDX]
       add  EDX, 4
       loop @StartLoop
-      @MethodCall:
+@MethodCall:
       push Style
       push Caption
       push Text
@@ -1207,8 +1206,7 @@ begin
   ShellDLL := SafeLoadLibrary(Shell32);
   MethodPtr := GetProcAddress(ShellDLL, PChar(183));
   if Assigned(MethodPtr) then
-    Result := ExecuteShellMessageBox(MethodPtr, Instance, Owner, Text, Caption,
-      Style, Parameters)
+    Result := ExecuteShellMessageBox(MethodPtr, Instance, Owner, Text, Caption, Style, Parameters)
   else
     Result := ID_CANCEL;
 end;
