fce8d8280fc1aef1186813ac0a7091fdef8fb1e7
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2009-06-24 17:56:37 +0000

    Mantis 4793: Added pt_BR locale

--- jvcl/locale/SetLanguages.bat
+++ jvcl/locale/SetLanguages.bat
@@ -3,4 +3,4 @@
 :: This is the list of languages that are known to the JVCL
 :: Update this line when a new language is added and all other batch files
 :: will benefit from the new value
-set LANGUAGES=bg de es fr it nl pl ro ru sv
+set LANGUAGES=bg de es fr it nl pl pt_BR ro ru sv
