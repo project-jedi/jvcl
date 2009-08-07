cca857b1abd6754d0e5256a5c085cb5cbba64734
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2009-08-07 11:52:11 +0000

    Warnings removed

--- run/JvToolEdit.pas
+++ run/JvToolEdit.pas
@@ -1,4 +1,4 @@
-{-----------------------------------------------------------------------------
+﻿{-----------------------------------------------------------------------------
 The contents of this file are subject to the Mozilla Public License
 Version 1.1 (the "License"); you may not use this file except in compliance
 with the License. You may obtain a copy of the License at
@@ -1312,10 +1312,7 @@ end;
 
 function EditorTextMargins(Editor: TCustomEdit): TPoint;
 var
-  DC: HDC;
   I: Integer;
-  SaveFont: HFONT;
-  SysMetrics, Metrics: TTextMetric;
   ed: TCustomEditAccessProtected;
 begin
   ed := TCustomEditAccessProtected(Editor);
