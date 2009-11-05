ec00c3ca1bd66f1d1176a3c0a593f9dfebe6f469
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2009-11-05 21:39:03 +0000

    3.39 is out, 3.40 is next

--- run/JVCLVer.pas
+++ run/JVCLVer.pas
@@ -46,9 +46,9 @@ uses
 {$ENDIF UNITVERSIONING}
 
 const
-  sJVCLVersion = '3.39';
+  sJVCLVersion = '3.40';
   JVCLVersionMajor   = 3;    // 0=pre-release|beta/1, 2, ...=final
-  JVCLVersionMinor   = 39;   // minor release
+  JVCLVersionMinor   = 40;   // minor release
   JVCLVersionRelease = 0;    // 0: pre-release|beta/>=1: release
   JVCLVersionBuild   = 0;    // build number, days since march 1, 2006
 
