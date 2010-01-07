b3e4d53bffbd9ec379d690e90f06981b4f7eb10d
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2010-01-07 16:11:28 +0000

    Add target defines that come from the compiler

--- devtools/PackagesGenerator/GenerateTargets.pas
+++ devtools/PackagesGenerator/GenerateTargets.pas
@@ -66,9 +66,30 @@ uses
   JclBase,
   JclStrings;
 
+type
+  TTargetDefine = record
+    TargetName: string;
+    Define: string;
+  end;
+
+const
+  TargetDefines: array [0..7] of TTargetDefine =
+                (
+                  (TargetName: 'c6'; Define: 'VER140'),
+                  (TargetName: 'd6'; Define: 'VER140'),
+                  (TargetName: 'd7'; Define: 'VER150'),
+                  (TargetName: 'd9'; Define: 'VER170'),
+                  (TargetName: 'd10'; Define: 'VER180'),
+                  (TargetName: 'd11'; Define: 'VER185'),
+                  (TargetName: 'd12'; Define: 'VER200'),
+                  (TargetName: 'd14'; Define: 'VER210')
+                );
+
 { TTarget }
 
 constructor TTarget.Create(Node: TJclSimpleXmlElem);
+var
+  I: Integer;
 begin
   inherited Create;
   FName := AnsiLowerCase(Node.Properties.ItemNamed['name'].Value);
@@ -102,6 +123,14 @@ begin
   FIsDotNet := False;
   if Assigned(Node.Properties.ItemNamed['IsDotNet']) then
     FIsDotNet := Node.Properties.ItemNamed['IsDotNet'].BoolValue;
+
+  FDefines.Add('WIN32');
+  FDefines.Add('CONDITIONALEXPRESSIONS');
+  FDefines.Add('BORLAND');
+
+  for I := Low(TargetDefines) to High(TargetDefines) do
+    if TargetDefines[I].TargetName = Name then
+      FDefines.Add(TargetDefines[I].Define);
 end;
 
 destructor TTarget.Destroy;
