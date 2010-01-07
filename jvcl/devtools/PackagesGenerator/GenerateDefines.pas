626ad31160fbdc5fe58964bfa335a4ca6491942e
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2010-01-07 15:55:58 +0000

    Avoid using defines in comments Added TDefinesList.RemoveDefine and TDefinesList.Find A condition might be defined in numerous places

--- jvcl/devtools/PackagesGenerator/GenerateDefines.pas
+++ jvcl/devtools/PackagesGenerator/GenerateDefines.pas
@@ -30,9 +30,11 @@ type
     constructor Create(incfile : TStrings); overload;
 
     procedure Append(incfile : TStrings);
+    function Find(const DefineName: string; DefineLimit : Integer = -1; DefineStart: Integer = 0): TDefine;
+    procedure RemoveDefine(const DefineName: string);
 
     function IsDefined(const Condition: string; TargetDefines: TStrings;
-      DefineLimit : Integer = -1): Boolean;
+      DefineLimit : Integer = -1; DefineStart: Integer = 0): Boolean;
 
     property Items[index: Integer]: TDefine read GetItems write SetItems; default;
   end;
@@ -76,6 +78,7 @@ var
   IfDefs : TStringList;
   ClosePos: Integer;
   LastIfIsIfDef: Boolean;
+  PrefixUsed: Boolean;
 begin
   LastIfIsIfDef := False;
   IfDefs := TStringList.Create;
@@ -87,36 +90,43 @@ begin
 
         while Pos('{', curLine) > 0 do
         begin
+          PrefixUsed := False;
           if StrHasPrefix(curLine, [IfDefMarker]) then
           begin
             IfDefs.AddObject(Copy(curLine, Length(IfDefMarker)+2, Pos('}', curLine) - Length(IfDefMarker) - 2), TObject(True));
             LastIfIsIfDef := True;
+            PrefixUsed := True;
           end
           else if StrHasPrefix(curLine, [IfNDefMarker]) then
           begin
             IfDefs.AddObject(Copy(curLine, Length(IfNDefMarker)+2, Pos('}', curLine) - Length(IfNDefMarker) - 2), TObject(False));
             LastIfIsIfDef := True;
+            PrefixUsed := True;
           end
           else if StrHasPrefix(curLine, [ElseMarker]) then
           begin
-            IfDefs.Objects[IfDefs.Count-1] := TObject(not Boolean(IfDefs.Objects[IfDefs.Count-1]))
+            IfDefs.Objects[IfDefs.Count-1] := TObject(not Boolean(IfDefs.Objects[IfDefs.Count-1]));
+            PrefixUsed := True;
           end
           else if StrHasPrefix(curLine, [EndIfMarker]) then
           begin
             if LastIfIsIfDef then
-              IfDefs.Delete(IfDefs.Count-1)
+              IfDefs.Delete(IfDefs.Count-1);
+            PrefixUsed := True;
           end
           else if StrHasPrefix(curLine, [DefineMarker]) then
           begin
             Add(TDefine.Create(Copy(curLine, Length(DefineMarker)+2, Pos('}', curLine) - Length(DefineMarker) - 2), IfDefs));
+            PrefixUsed := True;
           end
           else if StrHasPrefix(curLine, [IfMarker]) then
           begin
             LastIfIsIfDef := False;
+            PrefixUsed := True;
           end;
 
           ClosePos := Pos('}', curLine);
-          if ClosePos > 0 then
+          if PrefixUsed and (ClosePos > 0) then
             curLine := Trim(Copy(curLine, ClosePos + 1, MaxInt))
           else
             curLine := '';
@@ -134,16 +144,38 @@ begin
   Append(incfile);
 end;
 
+function TDefinesList.Find(const DefineName: string; DefineLimit : Integer = -1; DefineStart: Integer = 0): TDefine;
+var
+  I: Integer;
+begin
+  if DefineLimit = -1 then
+    DefineLimit := Count
+  else
+  if DefineLimit > Count then
+    DefineLimit := Count;
+
+  Result := nil;
+  for I := DefineStart to DefineLimit - 1 do
+  begin
+    if SameText(Items[I].Name, DefineName) then
+    begin
+      Result := Items[I];
+      Break;
+    end;
+  end;
+end;
+
 function TDefinesList.GetItems(index: integer): TDefine;
 begin
   Result := TDefine(inherited Items[index]);
 end;
 
 function TDefinesList.IsDefined(const Condition: string; TargetDefines: TStrings;
-  DefineLimit : Integer = -1): Boolean;
+  DefineLimit : Integer = -1; DefineStart: Integer = 0): Boolean;
 var
   I : Integer;
   Define : TDefine;
+  DefineIndex: Integer;
 begin
   if DefineLimit = -1 then
     DefineLimit := Count
@@ -153,12 +185,14 @@ begin
 
   Result := False;
   Define := nil;
-  for i := 0 to DefineLimit - 1 do
+  DefineIndex := -1;
+  for i := DefineStart to DefineLimit - 1 do
   begin
     if SameText(Items[I].Name, Condition) then
     begin
       Result := True;
       Define := Items[I];
+      DefineIndex := I;
       Break;
     end;
   end;
@@ -175,10 +209,18 @@ begin
     for I := 0 to Define.IfDefs.Count - 1 do
     begin
       if Boolean(Define.IfDefs.Objects[I]) then
-        Result := Result and IsDefined(Define.IfDefs[I], TargetDefines, IndexOf(Define))
+        Result := Result and IsDefined(Define.IfDefs[I], TargetDefines, DefineIndex)
       else
-        Result := Result and not IsDefined(Define.IfDefs[I], TargetDefines, IndexOf(Define));
-    end
+        Result := Result and not IsDefined(Define.IfDefs[I], TargetDefines, DefineIndex);
+    end;
+
+  if not Result and (DefineIndex >= 0) then
+    Result := IsDefined(Condition, TargetDefines, -1, DefineIndex + 1);
+end;
+
+procedure TDefinesList.RemoveDefine(const DefineName: string);
+begin
+  Remove(Find(DefineName));
 end;
 
 procedure TDefinesList.SetItems(index: integer; const Value: TDefine);
