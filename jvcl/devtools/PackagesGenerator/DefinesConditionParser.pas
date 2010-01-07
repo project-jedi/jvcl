594f4238d05cf3773f14cbcf40bef27f65f6cd50
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2010-01-07 15:22:03 +0000

    Added the Append method

--- jvcl/devtools/PackagesGenerator/DefinesConditionParser.pas
+++ jvcl/devtools/PackagesGenerator/DefinesConditionParser.pas
@@ -20,8 +20,11 @@ type
   public
     constructor Create(incfile, ATargetDefines: TStrings);
     destructor Destroy; override;
+
     procedure EnsureCondition(lines: TStrings; Condition: string);
     function EnsurePFlagsCondition(const pflags: string): string;
+
+    procedure Append(incfile, ATargetDefines: TStrings);
   end;
 
 implementation
@@ -31,6 +34,12 @@ uses
   JclStrings;
 
 { TDefinesConditionParser }
+
+procedure TDefinesConditionParser.Append(incfile, ATargetDefines: TStrings);
+begin
+  FTargetDefines := ATargetDefines;
+  FDefinesList.Append(incfile);
+end;
 
 constructor TDefinesConditionParser.Create(incfile, ATargetDefines: TStrings);
 begin
