90d05fb85cb45580613973d6b0b71df534a7e6e7
ahuser <ahuser@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2010-11-28 22:46:54 +0000

    Started component integration Mantis #5037: JvExplorerBar
    Lot's of TODOs, designtime code must be written, repainting must be optimized, storage format of items in the DFM

--- jvcl/examples/JvDesigner/JvDesignerDemo.dpr
+++ jvcl/examples/JvDesigner/JvDesignerDemo.dpr
@@ -2,7 +2,9 @@ program JvDesignerDemo;
 
 uses
   Forms,
-  Main in 'Main.pas' {MainForm};
+  Main in 'Main.pas' {MainForm},
+  JvDesignSurface in '..\..\run\JvDesignSurface.pas',
+  JvDesignImp in '..\..\run\JvDesignImp.pas';
 
 {$R *.res}
 
