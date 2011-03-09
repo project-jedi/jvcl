c41d83bea30d30a73815569c9e81413f5bae66c1
outchy <outchy@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2011-03-09 18:53:57 +0000

    help update:  - remove/rewrite unsupported DOM syntax;  - new scripts to compile help files (Chm,H2,PDF,Hlp,Xml);  - add dependency to the JEDI help database in addition to the JCL help database.

---
+++ jvcl/packages/help/Xml Help Packages.bat
@@ -0,0 +1,57 @@
+"%DMCC%" -config "XML" -noshow -w3 JvCore.dox
+"%XMLLINKS%" JvCore.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvSystem.dox
+"%XMLLINKS%" JvSystem.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvStdCtrls.dox
+"%XMLLINKS%" JvStdCtrls.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvControls.dox
+"%XMLLINKS%" JvControls.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvCmp.dox
+"%XMLLINKS%" JvCmp.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvCustom.dox
+"%XMLLINKS%" JvCustom.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvDlgs.dox
+"%XMLLINKS%" JvDlgs.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvCrypt.dox
+"%XMLLINKS%" JvCrypt.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvMM.dox
+"%XMLLINKS%" JvMM.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvNet.dox
+"%XMLLINKS%" JvNet.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvAppFrm.dox
+"%XMLLINKS%" JvAppFrm.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvDB.dox
+"%XMLLINKS%" JvDB.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvBDE.dox
+"%XMLLINKS%" JvBDE.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvPascalInterpreter.dox
+"%XMLLINKS%" JvPascalInterpreter.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvBands.dox
+"%XMLLINKS%" JvBands.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvPluginSystem.dox
+"%XMLLINKS%" JvPluginSystem.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvJans.dox
+"%XMLLINKS%" JvJans.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvGlobus.dox
+"%XMLLINKS%" JvGlobus.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvPrintPreview.dox
+"%XMLLINKS%" JvPrintPreview.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvPageComps.dox
+"%XMLLINKS%" JvPageComps.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvWizards.dox
+"%XMLLINKS%" JvWizards.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvTimeFramework.dox
+"%XMLLINKS%" JvTimeFramework.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvHMI.dox
+"%XMLLINKS%" JvHMI.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvManagedThreads.dox
+"%XMLLINKS%" JvManagedThreads.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvXPCtrls.dox
+"%XMLLINKS%" JvXPCtrls.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvDocking.dox
+"%XMLLINKS%" JvDocking.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvDotNetCtrls.dox
+"%XMLLINKS%" JvDotNetCtrls.dox XML "JVCL_Help:%%id"
+"%DMCC%" -config "XML" -noshow -w3 JvRuntimeDesign.dox
+"%XMLLINKS%" JvRuntimeDesign.dox XML "JVCL_Help:%%id"
+
