77e30d6135ce8ac6b973c482b16593eda7c45d60
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2011-11-22 11:10:42 +0000

    Can now give the version to use on the command line

--- jvcl/examples/CompileExamples.bat
+++ jvcl/examples/CompileExamples.bat
@@ -1,14 +1,12 @@
 :start
 @echo off
 cls
-echo. Change the JVCLMAKE line in this batch file to use the make program of
-echo. your choice or from a specific Delphi/BCB version (for example
-echo. C:\Program Files\Borland\Delphi5\bin\make.exe
-echo. to use the Delphi 5 make).
-echo. Defaults to: make.exe (whatever that is on your system), always build.
+echo. Call with the version you want to use as the first parameter
+echo. If no version is given, the highest is used, just like with install.bat
 echo.
 rem pause
-SET JVCLMAKE=make.exe -B -i -f 
+SET JVCLMAKE=make.exe -B -i -f
+set DELPHIVERSION=%1 
 echo. Making all examples (get some coffee, this takes a while!):
 echo.
 %JVCLMAKE% makefile.mak 
