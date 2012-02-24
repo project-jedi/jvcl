3410f7ee8c448adb84f9ce5ba2bb608fae7db480
obones <obones@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2012-02-24 10:12:26 +0000

    Mantis 5669 : missing published events

--- jvcl/run/JvValidateEdit.pas
+++ jvcl/run/JvValidateEdit.pas
@@ -1,4 +1,4 @@
-{-----------------------------------------------------------------------------
+﻿{-----------------------------------------------------------------------------
 The contents of this file are subject to the Mozilla Public License
 Version 1.1 (the "License"); you may not use this file except in compliance
 with the License. You may obtain a copy of the License at
@@ -328,6 +328,8 @@ type
     property OnKeyPress;
     property OnKeyUp;
     property OnMouseDown;
+    property OnMouseEnter;
+    property OnMouseLeave;
     property OnMouseMove;
     property OnMouseUp;
     property OnStartDrag;
