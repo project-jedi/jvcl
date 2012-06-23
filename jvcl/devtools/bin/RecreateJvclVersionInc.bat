6c7292758893c843ccdccd262a9f9b2177597b32
jfudickar <jfudickar@dd1dc939-a311-0410-afcf-ce1454c84e0b>
2012-06-23 15:38:50 +0000

    Batch to copy jvclbase.inc to the jvcl<version>.inc files

---
+++ jvcl/devtools/bin/RecreateJvclVersionInc.bat
@@ -0,0 +1,11 @@
+copy ..\..\common\jvclbase.inc  ..\..\common\jvclc6.inc
+copy ..\..\common\jvclbase.inc  ..\..\common\jvcld10.inc
+copy ..\..\common\jvclbase.inc  ..\..\common\jvcld11.inc
+copy ..\..\common\jvclbase.inc  ..\..\common\jvcld12.inc
+copy ..\..\common\jvclbase.inc  ..\..\common\jvcld14.inc
+copy ..\..\common\jvclbase.inc  ..\..\common\jvcld15.inc
+copy ..\..\common\jvclbase.inc  ..\..\common\jvcld16.inc
+copy ..\..\common\jvclbase.inc  ..\..\common\jvcld16_x64.inc
+copy ..\..\common\jvclbase.inc  ..\..\common\jvcld6.inc
+copy ..\..\common\jvclbase.inc  ..\..\common\jvcld7.inc
+copy ..\..\common\jvclbase.inc  ..\..\common\jvcld9.inc
