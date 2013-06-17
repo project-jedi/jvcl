ddfdcdf663de2da0724471bf6ef0c414605ef1b7
jfudickar <jens.fudickar@oratool.de>
2013-06-17 23:52:18 +0200

    New Component JvXMBrowser Added

--- jvcl/design/JvControlsReg.pas
+++ jvcl/design/JvControlsReg.pas
@@ -58,7 +58,7 @@ uses
   JvDsgnEditors, JvScrollMaxEditor, JvBehaviorLabelEditor, JvGroupHeaderEditor,
   JvControlActions, JvControlActnResForm,
   JvFooterEditor, JvSpeedbarForm, JvTransparentButtonEditors, JvRollOutEditor,
-  JvVersionControlActions;
+  JvVersionControlActions, JvXMLBrowser;
 
 {$R JvControlsReg.dcr}
 
@@ -86,7 +86,7 @@ begin
     TJvContentScroller, TJvPageScroller, TJvScrollingWindow, TJvScrollText]);
   RegisterComponents(RsPaletteSliderSplitter, [TJvSplitter, TJvxSplitter,
     TJvSyncSplitter, TJvNetscapeSplitter]);
-  RegisterComponents(RsPaletteVisual, [TJvClock, TJvCharMap, TJvZoom, TJvDice]);
+  RegisterComponents(RsPaletteVisual, [TJvClock, TJvCharMap, TJvZoom, TJvDice, TJvXMLBrowserControl]);
   RegisterComponents(RsPaletteNonVisual, [TJvHint, TJvControlActionList]);
 
   RegisterPropertyEditor(TypeInfo(TCaption), TJvHTLabel, 'Caption', TJvHintProperty);
