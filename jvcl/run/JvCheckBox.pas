f61235f3e64e60a79af12618888c4ddf72136cc7
Arioch <the_Arioch@nm.ru>
2013-09-01 16:41:05 +0400

    fix type compatibility

--- jvcl/run/JvCheckBox.pas
+++ jvcl/run/JvCheckBox.pas
@@ -523,24 +523,27 @@ type
 procedure TJvCheckBox.SetFocus;
 var
   I: Integer;
-  FocusLinkedControl: TControl;
+  FocusLinkedControl: TJvLinkedControl;
+  FocusTargetControl: TControl;
 begin
   inherited SetFocus;
 
-  // we want to skip our own focus, either to our children or to 
-  // a first accepting focus sibling, 
+  // we want to skip our own focus, either to our children or to
+  // a first accepting focus sibling,
   // depending on the direction that the user asked for
   if GetKeyState(VK_SHIFT) >= 0 then
   begin
     for I := 0 to LinkedControls.Count - 1 do begin
-    FocusLinkedControl := LinkedControls[I];
-    if (loForceFocus in FocusLinkedControl.Options)
-       and (FocusLinkedControl.Control is TWinControl) then
-       with TWinControl(FocusLinkedControl.Control) do
-            if CanFocus then begin
-               SetFocus;
-               Break; // found the new focus owner
-            end;
+      FocusLinkedControl := LinkedControls[I];
+      if (loForceFocus in FocusLinkedControl.Options) then begin
+         FocusTargetControl := FocusLinkedControl.Control;
+         if Assigned(FocusTargetControl) and (FocusTargetControl is TWinControl) then
+         with TWinControl(FocusTargetControl) do
+              if CanFocus then begin
+                 SetFocus;
+                 Break; // found the new focus owner
+              end;
+      end;
     end;
   end
   else
