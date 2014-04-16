32ea107d9ef32d4b40d731b6b8f9dd67ace411c4
Andreas Hausladen <Andreas.Hausladen@gmail.com>
2014-04-16 23:56:15 +0200

    Generate HPP files when building InnoSetup installer

--- jvcl/install/ISS/BuildSetup.bat
+++ jvcl/install/ISS/BuildSetup.bat
@@ -58,10 +58,10 @@ del /Q /S "%JVCLBUILTDIR%\*.*" 2>NUL >NUL
 SET JvclLib=%JVCLBUILTDIR%\lib\win32
 
 cd /d "%JVCLROOT%"
-msbuild make.proj "/p:Platform=win32" "/p:HppOutDir=%JVCLBUILTDIR%\hpp" "/p:DcuOutDir=%JVCLBUILTDIR%\lib\win32" "/p:BplOutDir=%JVCLBUILTDIR%\bpl" "/p:JclLibDir=%JCLBUILTDIR%\lib\win32" "/p:JclIncDir=%JCLROOT%\source\include"
+msbuild make.proj "/p:Platform=win32" "/p:ForceBCBCompile=true" "/p:HppOutDir=%JVCLBUILTDIR%\hpp" "/p:DcuOutDir=%JVCLBUILTDIR%\lib\win32" "/p:BplOutDir=%JVCLBUILTDIR%\bpl" "/p:JclLibDir=%JCLBUILTDIR%\lib\win32" "/p:JclIncDir=%JCLROOT%\source\include"
 if ERRORLEVEL 1 goto Failed
 if not exist "%BDS%\bin\dcc64.exe" goto NoWin64
-msbuild make.proj "/p:Platform=win64" "/p:HppOutDir=%JVCLBUILTDIR%\hpp64" "/p:DcuOutDir=%JVCLBUILTDIR%\lib\win64" "/p:BplOutDir=%JVCLBUILTDIR%\bpl\Win64" "/p:JclLibDir=%JCLBUILTDIR%\lib\win64" "/p:JclIncDir=%JCLROOT%\source\include"
+msbuild make.proj "/p:Platform=win64" "/p:ForceBCBCompile=true" "/p:HppOutDir=%JVCLBUILTDIR%\hpp64" "/p:DcuOutDir=%JVCLBUILTDIR%\lib\win64" "/p:BplOutDir=%JVCLBUILTDIR%\bpl\Win64" "/p:JclLibDir=%JCLBUILTDIR%\lib\win64" "/p:JclIncDir=%JCLROOT%\source\include"
 if ERRORLEVEL 1 goto Failed
 :: For 64bit we have to install both win32 and lib\win64
 SET JvclLib=%JVCLBUILTDIR%\lib
