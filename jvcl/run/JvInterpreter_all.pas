ead7d7e4ea155046b684af025eebbeb8a6e8febf
Andreas Hausladen <Andreas.Hausladen@gmail.com>
2015-09-27 16:36:41 +0200

    The BDE unit JvInterpreter_DBTables isn't included in the package if the compiler is 22.0 (XE8) or newer (independent of USE_BDE), so don't access is. Mantis #6451

--- jvcl/run/JvInterpreter_all.pas
+++ jvcl/run/JvInterpreter_all.pas
@@ -56,9 +56,11 @@ uses
   JvInterpreter_Forms, JvInterpreter_Menus, JvInterpreter_Grids,
   {$IFNDEF DelphiPersonalEdition}
   JvInterpreter_Db,
-  {$IFDEF USE_BDE}
+  {$IFNDEF COMPILER22_UP}
+    {$IFDEF USE_BDE}
   JvInterpreter_DBTables,
-  {$ENDIF USE_BDE}
+    {$ENDIF USE_BDE}
+  {$ENDIF ~COMPILER22_UP}
   JvInterpreter_DbCtrls,
   JvInterpreter_DbGrids,
   {$IFDEF JVCL_UseQuickReport}
@@ -90,9 +92,11 @@ begin
 
   {$IFNDEF DelphiPersonalEdition}
   JvInterpreter_Db.RegisterJvInterpreterAdapter(GlobalJvInterpreterAdapter);
-  {$IFDEF USE_BDE}
+  {$IFNDEF COMPILER22_UP}
+    {$IFDEF USE_BDE}
   JvInterpreter_DBTables.RegisterJvInterpreterAdapter(GlobalJvInterpreterAdapter);
-  {$ENDIF USE_BDE}
+    {$ENDIF USE_BDE}
+  {$ENDIF ~COMPILER22_UP}
   JvInterpreter_DbCtrls.RegisterJvInterpreterAdapter(GlobalJvInterpreterAdapter);
   JvInterpreter_DbGrids.RegisterJvInterpreterAdapter(GlobalJvInterpreterAdapter);
   {$IFDEF JVCL_UseQuickReport}
