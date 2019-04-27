0547ab21d27d821aa6faec00376741899cace730
mhumm <markus.humm@gmail.com>
2019-04-27 14:40:05 +0200

    Use TScreen.MenuFont instead of Canvas.Font in TJvMainMenu and TJvPopupMenu

--- jvcl/run/JvMenus.pas
+++ jvcl/run/JvMenus.pas
@@ -1255,7 +1255,7 @@ begin
         SaveIndex := SaveDC(hDC);
         try
           Canvas.Handle := hDC;
-          SetDefaultMenuFont(Canvas.Font);
+          SetDefaultMenuFont(TScreen.MenuFont);
           Canvas.Font.Color := clMenuText;
           Canvas.Brush.Color := clMenu;
           if mdDefault in State then
@@ -1836,7 +1836,7 @@ begin
         SaveIndex := SaveDC(hDC);
         try
           Canvas.Handle := hDC;
-          SetDefaultMenuFont(Canvas.Font);
+          SetDefaultMenuFont(TScreen.MenuFont);
           Canvas.Font.Color := clMenuText;
           Canvas.Brush.Color := clMenu;
           if mdDefault in State then
