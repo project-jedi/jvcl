e6576b8fa5ee0823a0cd98ceb47626bae9963fc1
Vicente Monroig <vmonroig@digitaldisseny.com>
2023-11-09 13:25:37 +0100

    Code comments for #0 search in attachment parameter

--- jvcl/run/JvMail.pas
+++ jvcl/run/JvMail.pas
@@ -402,6 +402,7 @@ procedure TJvMail.CreateMapiMessage;
       SetLength(FAttachArray, Attachment.Count);
       for I := 0 to Attachment.Count - 1 do
       begin
+        // Look for #0 splitting parameter in physical filename (lpszPathName) and virtual name (lpszFileName)
         NullPos := Pos(#0, Attachment[I]);
         if NullPos > 0 then
         begin
@@ -410,6 +411,7 @@ procedure TJvMail.CreateMapiMessage;
         end
         else
         begin
+          // Use real filename in attachment if no #0 is found
           PathName := Attachment[i];
           FileName := ExtractFileName(PathName);
         end;
