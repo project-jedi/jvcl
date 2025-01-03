0f92b16929a3b33342fe16d1beaa4fb3260aff12
emersonline86 <38477098+emersonline86@users.noreply.github.com>
2025-01-03 10:00:06 -0300

    Update Compile.pas
    Calls the Exit procedure with Win64xResult as a parameter only if the compiler version is Delphi 2009 or higher.

--- jvcl/install/JVCLInstall/Compile.pas
+++ jvcl/install/JVCLInstall/Compile.pas
@@ -1224,7 +1224,11 @@ begin
           Files.Add(ExtractFileName(ChangeFileExt(Project.SourceName, '.dpk'))); // readd, dcc removes items from the Files list
           Win64xResult := Dcc(TargetConfig, Project, DccOpt, DebugUnits, Files, nil, True);
           if Win64xResult <> 0 then
+            {$IFDEF DELPHI12_UP}
+            Exit(Win64xResult);
+            {$ELSE}
             Exit;
+            {$ENDIF}
         end;
 
         FilesCompiled := True;
